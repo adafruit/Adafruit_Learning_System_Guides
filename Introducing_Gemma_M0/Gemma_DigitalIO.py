5f066df005646006acc13eb102e728c3f3850505
ladyada <limor@ladyada.net>
2017-09-29 19:29:13 -0400

    generic for any/all boards

--- Introducing_Gemma_M0/Gemma_DigitalIO.py
+++ Introducing_Gemma_M0/Gemma_DigitalIO.py
@@ -1,4 +1,4 @@
-# Gemma IO demo #1 - General Purpose I/O
+# CircuitPython IO demo #1 - General Purpose I/O
 
 from digitalio import DigitalInOut, Direction, Pull
 import board
@@ -7,7 +7,7 @@ import time
 led = DigitalInOut(board.D13)
 led.direction = Direction.OUTPUT
 
-button = DigitalInOut(board.D1)
+button = DigitalInOut(board.D2)
 button.direction = Direction.INPUT
 button.pull = Pull.UP
 
