d5628d42bafdbaf9ed53bd4c4b48fb51e9e232ae
ladyada <limor@ladyada.net>
2017-09-30 17:12:54 -0400

    fix for 2.0

--- Introducing_Gemma_M0/Gemma_I2Csi7021.py
+++ Introducing_Gemma_M0/Gemma_I2Csi7021.py
@@ -1,19 +1,25 @@
-# Gemma M0 IO Demo - I2C demo
+# I2C sensor demo
 
-from digitalio import *
-from board import *
+import board
 import busio
 import adafruit_si7021
 import time
 
-led = DigitalInOut(D13)
-led.direction = Direction.OUTPUT
+i2c = busio.I2C(board.SCL, board.SDA)
 
-i2c = busio.I2C(D2, D0)
+# lock the I2C device before we try to scan
+while not i2c.try_lock():
+    pass
 print("I2C addresses found:", [hex(i) for i in i2c.scan()])
 
+# unlock I2C now that we're done scanning.
+i2c.unlock()
+
+# Create library object on our I2C port
 si7021 = adafruit_si7021.SI7021(i2c)
 
+
+# Use library to read the data!
 while True:
     print("Temp: %0.2F *C   Humidity: %0.1F %%" % (si7021.temperature, si7021.relative_humidity))
     time.sleep(1)
