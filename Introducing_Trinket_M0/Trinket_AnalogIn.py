a5880c10217caf39b49876bfabcc9563775ba72b
ladyada <limor@ladyada.net>
2017-09-30 13:05:15 -0400

    digial names

--- Introducing_Trinket_M0/Trinket_AnalogIn.py
+++ Introducing_Trinket_M0/Trinket_AnalogIn.py
@@ -4,7 +4,6 @@ from analogio import AnalogIn
 import board
 import time
 
-
 analog0in = AnalogIn(board.D0)
 analog1in = AnalogIn(board.D1)
 analog2in = AnalogIn(board.D2)
@@ -15,7 +14,7 @@ def getVoltage(pin):
     return (pin.value * 3.3) / 65536
 
 while True:
-    print("A0: %0.2f \t A1: %0.2f \t A2: %0.2f \t A3: %0.2f \t A4: %0.2f" %
+    print("D0: %0.2f \t D1: %0.2f \t D2: %0.2f \t D3: %0.2f \t D4: %0.2f" %
           (getVoltage(analog0in),
            getVoltage(analog1in),
            getVoltage(analog2in),
