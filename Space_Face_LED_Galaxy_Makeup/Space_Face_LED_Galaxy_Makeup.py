d244e615e5f3cdff64c73388665b30f6546e9f47
Mikey Sklar <sklarm@sonofsiri.local>
2017-10-06 17:28:39 -0600

    blind port, untested, placeholder

---
+++ Space_Face_LED_Galaxy_Makeup/Space_Face_LED_Galaxy_Makeup.py
@@ -0,0 +1,20 @@
+from digitalio import DigitalInOut, Direction
+import board 
+import neopixel
+import time
+
+pixpin = board.D1
+numpix = 5
+
+led = DigitalInOut(board.D13)
+led.direction = Direction.OUTPUT
+
+strip = neopixel.NeoPixel(pixpin, numpix, brightness=1, auto_write=True)
+
+def colorWipe(color, wait):
+    for j in range(len(strip)):
+	strip[j] = (color)
+	time.sleep(wait)
+
+while True:
+    colorWipe( (50, 0, 50), .1 ) # Purple
