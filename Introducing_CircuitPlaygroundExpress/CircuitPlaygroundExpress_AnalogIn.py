862891afb5f64c4d73efe04132b12e9a4abde212
John Edgar Park <jp@jpixl.net>
2017-10-06 12:44:35 -0700

    Create CircuitPlaygroundExpress_AnalogIn.py
    Analog in voltage read using photocell.

---
+++ Introducing_CircuitPlaygroundExpress/CircuitPlaygroundExpress_AnalogIn.py
@@ -0,0 +1,21 @@
+# CircuitPlaygroundExpress_AnalogIn
+# reads the on-board light sensor's analog voltage level
+# and prints the results
+
+from digitalio import DigitalInOut, Direction
+from analogio import AnalogIn
+import board
+import time
+
+led = DigitalInOut(board.D13)
+led.direction = Direction.OUTPUT
+
+analogin = AnalogIn(board.LIGHT)
+
+
+def getVoltage(pin): #helper
+    return (pin.value * 3.3) / 65536
+
+while True:
+    print("Analog Voltage: %f" % getVoltage(analogin))
+    time.sleep(0.1)
