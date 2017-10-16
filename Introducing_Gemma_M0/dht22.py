e41b0a83063df395dc77fcb9860605ee1c35ce54
ladyada <limor@ladyada.net>
2017-10-16 17:12:21 -0400

    tested dht example

---
+++ Introducing_Gemma_M0/dht22.py
@@ -0,0 +1,18 @@
+import board
+import time
+import adafruit_dht
+
+
+dht  = adafruit_dht.DHT22(board.D2)
+
+while True:
+    try:
+        temperature = dht.temperature
+        humidity = dht.humidity
+        # Print what we got to the REPL
+        print("Temp: {:.1f} *C \t Humidity: {}% ".format(temperature, humidity))
+    except RuntimeError as e:
+        # Reading doesn't always work! Just print error and we'll try again
+        print("Reading from DHT failure: ",e.args)
+
+    time.sleep(1)
