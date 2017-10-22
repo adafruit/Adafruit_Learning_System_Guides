8fc802883b6946fe34c5b42210755ffcf8e3c89b
Mikey Sklar <sklarm@sonofsiri.local>
2017-10-22 14:13:40 -0600

    Ported to Circuit Python tested on Gemma M0

---
+++ Interior_Purse_Light/Interior_Purse_Light.ino
@@ -0,0 +1,30 @@
+// constants won't change. They're used here to 
+// set pin numbers:
+const int buttonPin = 1;     // the number of the pushbutton pin
+const int ledPin =  2;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);      
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT_PULLUP);     
+}
+
+void loop(){
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {     
+    // turn LED on:    
+    digitalWrite(ledPin, HIGH);  
+  } 
+  else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW); 
+  }
+}
---
+++ Interior_Purse_Light/Interior_Purse_Light.py
@@ -0,0 +1,19 @@
+from digitalio import DigitalInOut, Direction, Pull
+import board
+import time
+
+button = DigitalInOut(board.D1)
+button.direction = Direction.INPUT
+button.pull = Pull.UP
+
+led = DigitalInOut(board.D2)
+led.direction = Direction.OUTPUT
+
+while True:
+    if button.value:
+		led.value = True 	# check if the pushbutton is pressed. 
+ 
+    else:
+		led.value = False	# turn LED off
+
+    time.sleep(0.01) # debounce delay
