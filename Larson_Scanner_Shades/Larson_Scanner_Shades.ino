0b43bd6fe007d726fd6ff9c2c9b4998768fa5696
Mikey Sklar <sklarm@sonofsiri.local>
2017-11-18 18:00:21 -0700

    Ported to CircuitPython, Tested on Gemma M0 and Trinket M0

---
+++ Larson_Scanner_Shades/Larson_Scanner_Shades.ino
@@ -0,0 +1,41 @@
+#include <Adafruit_NeoPixel.h>
+
+#define N_LEDS 22
+#define PIN     4
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+}
+
+int pos = 0, dir = 1; // Position, direction of "eye"
+
+void loop() {
+  int j;
+
+  // Draw 5 pixels centered on pos.  setPixelColor() will clip any
+  // pixels off the ends of the strip, we don't need to watch for that.
+  strip.setPixelColor(pos - 2, 0x100000); // Dark red
+  strip.setPixelColor(pos - 1, 0x800000); // Medium red
+  strip.setPixelColor(pos    , 0xFF3000); // Center pixel is brightest
+  strip.setPixelColor(pos + 1, 0x800000); // Medium red
+  strip.setPixelColor(pos + 2, 0x100000); // Dark red
+
+  strip.show();
+  delay(30);
+
+  // Rather than being sneaky and erasing just the tail pixel,
+  // it's easier to erase it all and draw a new one next time.
+  for(j=-2; j<= 2; j++) strip.setPixelColor(pos+j, 0);
+
+  // Bounce off ends of strip
+  pos += dir;
+  if(pos < 0) {
+    pos = 1;
+    dir = -dir;
+  } else if(pos >= strip.numPixels()) {
+    pos = strip.numPixels() - 2;
+    dir = -dir;
+  }
+}
---
+++ Larson_Scanner_Shades/README.md
@@ -0,0 +1,4 @@
+# Larson_Scanner_Shades
+
+Code to accompany this tutorial:
+https://learn.adafruit.com/larson-scanner-shades
