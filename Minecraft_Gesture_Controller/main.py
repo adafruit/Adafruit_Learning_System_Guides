46fb59fa946407854c3f60d499e57e1eb833cf29
ladyada <limor@ladyada.net>
2018-02-19 00:50:22 -0500

    stylistic edits & comments

--- Minecraft_Gesture_Controller/main.py
+++ Minecraft_Gesture_Controller/main.py
@@ -12,25 +12,32 @@
 ##########################
 ##        Libraries     ##
 ##########################
-import touchio
-import board
-import busio
-import time
+
+#Libraries for accelerometer
+import adafruit_lis3dh
 #Libraries for HID Keyboard & Mouse
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.mouse import Mouse
-#Libraries for accelerometer
-import adafruit_lis3dh
+# General purpose libraries
+import touchio
+import board
+import busio
+import time
 
 #######################
 #     CPX Setup       #
 #######################
-pin1 = board.A1
-pin2 = board.A2
-pin3 = board.A3
-pin4 = board.A4
+touch_a1 = touchio.TouchIn(board.A1)
+touch_a1.threshold = 2000
+touch_a2 = touchio.TouchIn(board.A2)
+touch_a2.threshold = 2000
+touch_a3 = touchio.TouchIn(board.A3)
+touch_a3.threshold = 2000
+touch_a4 = touchio.TouchIn(board.A4)
+touch_a4.threshold = 2000
+
 
 ############################
 # Keyboard & Mouse Setup   #
@@ -54,6 +61,8 @@ lis3dh.range = adafruit_lis3dh.RANGE_8_G
 ###########################
 ## Controller Functions  ##
 ###########################
+
+# A helper to 'remap' an input range to an output range
 def Map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min)*(out_max - out_min) / (in_max - in_min) + out_min)
 
@@ -61,7 +70,7 @@ def Move(upDown_axis, isBackward):
     axis_new = abs(upDown_axis)
     #If you are touching A4, walk backwards, else walk forwards
     if isBackward:
-        print('backwards') #Debugging
+        print("backwards") #Debugging
         if (axis_new > 1.2): #walk threshold
             if(axis_new > 2.5): #run threshold
                 kbd.press(Keycode.LEFT_CONTROL, Keycode.S)
@@ -72,7 +81,7 @@ def Move(upDown_axis, isBackward):
                 time.sleep(0.1)
             kbd.release_all()
     else:
-         if (axis_new > 1.2): #walk threshold
+        if (axis_new > 1.2): #walk threshold
             if(axis_new > 2.5): #run threshold
                 kbd.press(Keycode.LEFT_CONTROL)
                 time.sleep(0.1)
@@ -106,77 +115,69 @@ def Give(upDown_axis, frontBack_axis):
     frontBack_new = abs(frontBack_axis)
     if abs(upDown_axis) < 1:
         if(frontBack_new > 2):
-            print('give')
+            print("give")
             mouse.click(Mouse.RIGHT_BUTTON)
             mouse.release_all()
 
 def Attack():
-    print('attack')
+    """Attack! By clicking the left mouse button"""
+    print("attack")
     mouse.click(Mouse.LEFT_BUTTON)
     time.sleep(0.1)
     mouse.release_all()
 
 def Inventory():
-    print('inventory') #Debugging -- view in serial monitor
+    """Open up inventory, press the E keyboard key"""
+    print("inventory") #Debugging -- view in serial monitor
     kbd.press(Keycode.E)
     time.sleep(0.01)
     kbd.release_all()
 
 def ESC():
-    print('ESC') #Debugging -- view in serial monitor
+    """Escape by pressing the ESCape key"""
+    print("ESC") #Debugging -- view in serial monitor
     kbd.press(Keycode.ESCAPE)
     time.sleep(0.01)
     kbd.release_all()
 
-def touch(pin):  
-    touch = touchio.TouchIn(pin)
-    touch.threshold = 2000
-    rawValue = touch.raw_value
-    if touch.value:
-        touchTrig = True
-    else:
-        touchTrig = False
-    return touchTrig
-    touch.deinit()
-
 def readAxes():
-    x_axis, y_axis, z_axis = lis3dh.acceleration
-    x = x_axis / 9.806
-    y = y_axis / 9.806
-    z = z_axis / 9.806
+    """Convert acceleration from m/s^2 to G, with a delay"""
+    x, y, z = lis3dh.acceleration
     time.sleep(0.01)
-    return x, y, z
+    return ( x/9.806, y/9.806, z/9.806 )  # 9.806 m/s^2 per G
+
 
 ###########################
 ##     Main Function     ## 
 ###########################
-def __main__():
-    while True:
-    # Read accelerometer values (in m / s ^ 2).  Returns a 3-tuple of x, y, x axis
-        x, y, z = readAxes()
-    #Read finger pads and act accordingly
-        if touch(pin1):
-            Attack()
-        if touch(pin2):
-            Inventory()
-        if touch(pin3):
-            ESC()
-        isBackward = touch(pin4)
-        lookUp = touch(pin4)
-    #Run through the motions! .. literally :)
-        Move(y, isBackward)
-        Turn(y, x, z, lookUp)
-        Jump(y)
-        Give(y,x)
+while True:
+    # Read accelerometer values (in G).  Returns a 3-tuple of x, y, x axis
+    x, y, z = readAxes()
+    
+    # Read finger pads and act accordingly
+    if touch_a1.value:
+        Attack()
 
+    if touch_a2.value:
+        Inventory()
+
+    if touch_a3.value:
+        ESC()
+
+    isBackward = touch_a4.value
+    lookUp = touch_a4.value
+
+    # Run through the motions! .. literally :)
+    Move(y, isBackward)
+    Turn(y, x, z, lookUp)
+    Jump(y)
+    Give(y, x)
+    
     # Small delay to keep things responsive but give time for interrupt processing.
-        time.sleep(0.01)
-
-## Debugging Ahead!! ##
-#Use the following 2 lines to figure out which axis is upDown, frontBack, or LeftRight
-#   and also for debugging!
+    time.sleep(0.01)
+    
+    ## Debugging Ahead!! ##
+    # Use the following 2 lines to figure out which axis is upDown, frontBack, or LeftRight
+    # and also for debugging!
     #print('x = {}G, y = {}G, z = {}G'.format(x, y, z))
     #time.sleep(0.3)
-
-
-__main__()
