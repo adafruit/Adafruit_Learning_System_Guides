62e0db413e84322fc3453e960f2427bf56a33ab7
Mikey Sklar <sklarm@sonofsiri.local>
2018-03-01 19:59:36 -0700

    ported to CircuitPython

---
+++ Adafruit_LED_Sequins/Adafruit_LED_Sequins.ino
@@ -0,0 +1,37 @@
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+int counter = 0;       // counter to keep track of cycles
+
+// the setup routine runs once when you press reset:
+void setup()  { 
+  // declare pins to be an outputs:
+  pinMode(0, OUTPUT);
+  pinMode(2, OUTPUT);
+} 
+
+// the loop routine runs over and over again forever:
+void loop()  { 
+  // set the brightness of the analog-connected LEDs:
+  analogWrite(0, brightness);
+  
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade: 
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount; 
+    counter++;
+  }     
+  // wait for 15 milliseconds to see the dimming effect    
+  delay(15); 
+
+// turns on the other LEDs every four times through the fade by 
+// checking the modulo of the counter.
+// the modulo function gives you the remainder of 
+// the division of two numbers:
+  if (counter % 4 == 0) {
+    digitalWrite(2, HIGH);
+  } else {
+   digitalWrite(2, LOW);
+  }  
+}
---
+++ Adafruit_LED_Sequins/README.md
@@ -0,0 +1,4 @@
+# Adafruit_LED_Sequins
+
+Code to accompany this tutorial:
+https://learn.adafruit.com/adafruit-led-sequins/
