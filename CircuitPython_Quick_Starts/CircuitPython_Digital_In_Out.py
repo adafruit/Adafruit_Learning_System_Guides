51dd204e9829fe902962b7d91f0b6ed4316ea547
Kattni <kattni@adafruit.com>
2018-03-02 18:46:52 -0500

    Updated Pull, removed unnecessary lines

--- CircuitPython_Quick_Starts/CircuitPython_Digital_In_Out.py
+++ CircuitPython_Quick_Starts/CircuitPython_Digital_In_Out.py
@@ -11,12 +11,11 @@ switch = DigitalInOut(board.D2)  # For Gemma M0, Trinket M0, Metro M0 Express, I
 # switch = DigitalInOut(board.D5)  # For Feather M0 Express
 # switch = DigitalInOut(board.D7)  # For Circuit Playground Express
 switch.direction = Direction.INPUT
-switch.pull = Pull.DOWN  # For Gemma M0, Trinket M0, Metro M0 Exp, ItsyBitsy M0 Exp, Feather M0 Exp
-# switch.pull = Pull.UP  # For Circuit Playground Express
+switch.pull = Pull.UP
 
 
 while True:
-    # We could also just do "led.value = not switch.value" !
+    # We could also just do "led.value = not switch.value"!
     if switch.value:
         led.value = False
     else:
