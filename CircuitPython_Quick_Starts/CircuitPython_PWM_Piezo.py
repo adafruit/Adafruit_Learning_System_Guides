cd5c1492e694b968737452db81dffdae9bf0b5b7
Kattni <kattni@adafruit.com>
2018-03-08 20:01:51 -0500

    PWM piezo example

---
+++ CircuitPython_Quick_Starts/CircuitPython_PWM_Piezo.py
@@ -0,0 +1,14 @@
+import pulseio
+import time
+import board
+
+piezo = pulseio.PWMOut(board.A2, duty_cycle=0, frequency=440, variable_frequency=True)
+
+while True:
+    for f in (262, 294, 330, 349, 392, 440, 494, 523):
+        piezo.frequency = f
+        piezo.duty_cycle = 65536 // 2  # On 50%
+        time.sleep(0.25)  # On for 1/4 second
+        piezo.duty_cycle = 0   # Off
+        time.sleep(0.05)  # Pause between notes
+    time.sleep(0.5)
