281ebeabe7ede9bfb63ce3198173b0c67dd5dcf5
Kattni <kattni@adafruit.com>
2018-03-11 22:48:37 -0400

    linting

--- CircuitPython_Quick_Starts/CircuitPython_Servo.py
+++ CircuitPython_Quick_Starts/CircuitPython_Servo.py
@@ -8,6 +8,6 @@ while True:
     for angle in range(0, 180, 5):  # 0-180 degrees, 5 degrees at a time
         servo.angle = angle
         time.sleep(0.05)
-    for angle in range(180, 0, -5): # 180-0 degrees, 5 degrees at a time
+    for angle in range(180, 0, -5):  # 180-0 degrees, 5 degrees at a time
         servo.angle = angle
         time.sleep(0.05)
