d7db35842f952b41f588703e4d5aa404826da6ac
Kattni <kattni@adafruit.com>
2018-03-11 22:32:18 -0400

    Servo example

---
+++ CircuitPython_Quick_Starts/CircuitPython_Servo.py
@@ -0,0 +1,13 @@
+import time
+import simpleio
+import board
+
+servo = simpleio.Servo(board.A2)
+
+while True:
+    for angle in range(0, 180, 5):  # 0-180 degrees, 5 degrees at a time
+        servo.angle = angle
+        time.sleep(0.05)
+    for angle in range(180, 0, -5): # 180-0 degrees, 5 degrees at a time
+        servo.angle = angle
+        time.sleep(0.05)
