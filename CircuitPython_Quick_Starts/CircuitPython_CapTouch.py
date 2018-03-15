083872647a6f9fbba0868dede2751dd340df1e4d
Kattni <kattni@adafruit.com>
2018-03-14 22:58:17 -0400

    update to captouch for CPX

--- CircuitPython_Quick_Starts/CircuitPython_CapTouch.py
+++ CircuitPython_Quick_Starts/CircuitPython_CapTouch.py
@@ -2,9 +2,11 @@ import time
 import board
 import touchio
 
-touch_A0 = touchio.TouchIn(board.A0)  # Will not work on Circuit Playground Express!
+touch_A0 = touchio.TouchIn(board.A0)  # Will not work on Circuit Playground Express! Comment it out!
+# touch_A1 = touchio.TouchIn(board.A1)  # Uncomment for using with CPX.
 
 while True:
-    if touch_A0.value:
-        print("Touched A0!")
+    # if touch_A1.value:  # Uncomment for using with CPX.
+    if touch_A0.value:  # Will not work on CPX! Comment this out!
+        print("Touched!")
     time.sleep(0.05)
