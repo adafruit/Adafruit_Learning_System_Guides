ada4bbcc74692f602cdfa0abb4ed7eb72577a94c
Kattni <kattni@adafruit.com>
2018-03-19 22:23:34 -0400

    Set pixel number to 8

--- CircuitPython_Quick_Starts/CircuitPython_NeoPixel_RGBW.py
+++ CircuitPython_Quick_Starts/CircuitPython_NeoPixel_RGBW.py
@@ -5,7 +5,7 @@ import board
 import neopixel
 
 pixel_pin = board.A1
-num_pixels = 16
+num_pixels = 8
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels,
                            brightness=0.3, auto_write=False, pixel_order=(1, 0, 2, 3))
