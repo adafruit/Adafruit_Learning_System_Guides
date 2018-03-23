708d2f20b5e73371d5d114ad3bd6f8db63d58353
Kattni <kattni@adafruit.com>
2018-03-23 19:14:05 -0400

    Updated pins for all boards

--- CircuitPython_Essentials/CircuitPython_Logger_Boot.py
+++ CircuitPython_Essentials/CircuitPython_Logger_Boot.py
@@ -2,10 +2,12 @@ import digitalio
 import board
 import storage
 
-switch = digitalio.DigitalInOut(board.D0)
+# For Gemma M0, Trinket M0, Metro M0 Express, ItsyBitsy M0 Express
+switch = digitalio.DigitalInOut(board.D2)
+# switch = digitalio.DigitalInOut(board.D5)  # For Feather M0 Express
+# switch = digitalio.DigitalInOut(board.D7)  # For Circuit Playground Express
 switch.direction = digitalio.Direction.INPUT
 switch.pull = digitalio.Pull.UP
 
-# If the D0 is connected to ground with a wire
-# CircuitPython can write to the drive
+# If the switch pin is connected to ground CircuitPython can write to the drive
 storage.remount("/", switch.value)
