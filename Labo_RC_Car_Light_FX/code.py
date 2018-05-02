7a62606e660cc67c63fa1a8f4ea17dcc1c8beef1
Collin Cunningham <me@collinmel.com>
2018-05-01 21:20:22 -0400

    variable renaming

--- Labo_RC_Car_Light_FX/code.py
+++ Labo_RC_Car_Light_FX/code.py
@@ -7,14 +7,14 @@ pixels.fill(0)
 
 while True:
     
-    x_float, y_float, z_float = cpx.acceleration  # read acceleromter
+    x, y, z = cpx.acceleration  # read acceleromter
     r, g, b = 0, 0, 0
     
-    if abs(x_float) > 4.0:
+    if abs(x) > 4.0:
         r = 255
-    if abs(y_float) > 2.0:
+    if abs(y) > 2.0:
         g = 255
-    if z_float > -6.0 or z_float < -12.0:
+    if z > -6.0 or z < -12.0:
        b = 255 
     
     pixels.fill((r,g,b))
