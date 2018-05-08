636a6f96463e5c748e33eb773337cb945e396e1b
Kattni Rembor <kattni@adafruit.com>
2018-05-08 16:05:32 -0400

    Refactored variable names

--- Gemma_LightTouch/red_green_blue.py
+++ Gemma_LightTouch/red_green_blue.py
@@ -4,21 +4,21 @@ import touchio
 import adafruit_dotstar
 import board
 
-DOTSTAR = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
-TOUCH_A0 = touchio.TouchIn(board.A0)
-TOUCH_A1 = touchio.TouchIn(board.A1)
-TOUCH_A2 = touchio.TouchIn(board.A2)
+dotstar = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
+touch_A0 = touchio.TouchIn(board.A0)
+touch_A1 = touchio.TouchIn(board.A1)
+touch_A2 = touchio.TouchIn(board.A2)
 
 r = g = b = 0
 
 while True:
-    if TOUCH_A0.value:
+    if touch_A0.value:
         r = (r + 1) % 256
-    if TOUCH_A1.value:
+    if touch_A1.value:
         g = (g + 1) % 256
-    if TOUCH_A2.value:
+    if touch_A2.value:
         b = (b + 1) % 256
 
-    DOTSTAR[0] = (r, g, b)
+    dotstar[0] = (r, g, b)
     print((r, g, b))
     time.sleep(0.01)
