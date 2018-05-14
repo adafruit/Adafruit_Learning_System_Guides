e209493de0aa5098e45049412a13a39b2c80a4a6
Craig Richardson <craigargh@gmail.com>
2018-05-14 21:59:35 +0100

    Fix PEP8 compliance

--- EPROM_Emulator/debouncer.py
+++ EPROM_Emulator/debouncer.py
@@ -40,8 +40,10 @@ class Debouncer(object):
     def __init__(self, pin, mode=None, interval=0.010):
         """Make am instance.
            :param int pin: the pin (from board) to debounce
-           :param int mode: digitalio.Pull.UP or .DOWN (default is no pull up/down)
-           :param int interval: bounce threshold in seconds (default is 0.010, i.e. 10 milliseconds)
+           :param int mode: digitalio.Pull.UP or .DOWN (default is no
+           pull up/down)
+           :param int interval: bounce threshold in seconds (default is 0.010,
+           i.e. 10 milliseconds)
         """
         self.state = 0x00
         self.pin = digitalio.DigitalInOut(pin)
@@ -70,7 +72,8 @@ class Debouncer(object):
         return (self.state & bits) != 0
 
     def update(self):
-        """Update the debouncer state. Must be called before using any of the properties below"""
+        """Update the debouncer state. Must be called before using any of
+        the properties below"""
         self.__unset_state(Debouncer.CHANGED_STATE)
         current_state = self.pin.value
         if current_state != self.__get_state(Debouncer.UNSTABLE_STATE):
@@ -78,7 +81,8 @@ class Debouncer(object):
             self.__toggle_state(Debouncer.UNSTABLE_STATE)
         else:
             if time.monotonic() - self.previous_time >= self.interval:
-                if current_state != self.__get_state(Debouncer.DEBOUNCED_STATE):
+                debounced_state = self.__get_state(Debouncer.DEBOUNCED_STATE)
+                if current_state != debounced_state:
                     self.previous_time = time.monotonic()
                     self.__toggle_state(Debouncer.DEBOUNCED_STATE)
                     self.__set_state(Debouncer.CHANGED_STATE)
@@ -90,10 +94,14 @@ class Debouncer(object):
 
     @property
     def rose(self):
-        """Return whether the debounced input went from low to high at the most recent update."""
-        return self.__get_state(self.DEBOUNCED_STATE) and self.__get_state(self.CHANGED_STATE)
+        """Return whether the debounced input went from low to high at
+        the most recent update."""
+        return self.__get_state(self.DEBOUNCED_STATE) \
+            and self.__get_state(self.CHANGED_STATE)
 
     @property
     def fell(self):
-        """Return whether the debounced input went from high to low at the most recent update."""
-        return (not self.__get_state(self.DEBOUNCED_STATE)) and self.__get_state(self.CHANGED_STATE)
+        """Return whether the debounced input went from high to low at
+        the most recent update."""
+        return (not self.__get_state(self.DEBOUNCED_STATE)) \
+            and self.__get_state(self.CHANGED_STATE)
