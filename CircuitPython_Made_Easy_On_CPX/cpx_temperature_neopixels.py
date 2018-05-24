4f00b479c81eda698e560e676188a12c86eb24b6
Kattni Rembor <kattni@adafruit.com>
2018-05-24 17:35:41 -0400

    Updated and added new code

--- CircuitPython_Made_Easy_On_CPX/cpx_temperature_neopixels.py
+++ CircuitPython_Made_Easy_On_CPX/cpx_temperature_neopixels.py
@@ -5,10 +5,14 @@ import simpleio
 cpx.pixels.auto_write = False
 cpx.pixels.brightness = 0.3
 
+# Set these based on your ambient temperature for best results!
+minimum_temp = 24
+maximum_temp = 30
+
 while True:
-    # light value remapped to pixel position
-    peak = simpleio.map_range(cpx.light, 0, 320, 0, 10)
-    print(cpx.light)
+    # temperature value remapped to pixel position
+    peak = simpleio.map_range(cpx.temperature, minimum_temp, maximum_temp, 0, 10)
+    print(cpx.temperature)
     print(int(peak))
 
     for i in range(0, 10, 1):
