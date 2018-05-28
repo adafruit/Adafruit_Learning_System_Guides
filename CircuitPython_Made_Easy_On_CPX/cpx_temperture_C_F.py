ee27dc9f7c11e11598305202f4d9f2f83309da5f
Kattni Rembor <kattni@adafruit.com>
2018-05-28 14:36:15 -0400

    Created separate file for temp in F

--- CircuitPython_Made_Easy_On_CPX/cpx_temperture_C_F.py
+++ CircuitPython_Made_Easy_On_CPX/cpx_temperture_C_F.py
@@ -3,4 +3,5 @@ from adafruit_circuitplayground.express import cpx
 
 while True:
     print("Temperature C:", cpx.temperature)
+    print("Temperature F:", cpx.temperature * 1.8 + 32)
     time.sleep(1)
