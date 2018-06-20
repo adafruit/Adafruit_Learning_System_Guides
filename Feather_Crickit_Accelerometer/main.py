605d421c1b77115f28e7475847dcae2c2e5a662b
Dave Astels <dastels@daveastels.com>
2018-06-20 11:00:48 -0400

    Add FeatherWing Crickit accelerometer demo code

---
+++ Feather_Crickit_Accelerometer/main.py
@@ -0,0 +1,41 @@
+# See https://en.wikipedia.org/wiki/PID_controller
+
+import busio
+import board
+import time
+from adafruit_seesaw.seesaw import Seesaw
+from adafruit_seesaw.pwmout import PWMOut
+from adafruit_motor import servo
+import adafruit_lsm9ds0
+
+# Setup hardware
+i2c = busio.I2C(board.SCL, board.SDA)
+
+sensor = adafruit_lsm9ds0.LSM9DS0_I2C(i2c)
+seesaw = Seesaw(i2c)
+ 
+# Create servo objects
+pwm1 = PWMOut(seesaw, 17)
+pwm1.frequency = 50
+servo1 = servo.Servo(pwm1, min_pulse=500, max_pulse=2500)
+
+# Center the servo
+servo1.angle = 90 
+
+while True:
+    # Read the accel
+    x, y, z = sensor.accelerometer
+
+    # Clip the value
+    if y < -10:
+        y = -10
+    if y > 10:
+        y = 10
+        
+    # print(((y / 10) + 1) * 90)
+
+    # Set the angle
+    servo1.angle = ((-y / 10) + 1) * 90
+
+    time.sleep(0.1)
+
