d766a325f71afa6f860d4447ef55761bfebdb2e8
Dan Halbert <halbert@halwitz.org>
2018-06-25 10:49:55 -0400

    fix indentation

--- seesaw/seesaw_blink.py
+++ seesaw/seesaw_blink.py
@@ -9,9 +9,8 @@ ss = Seesaw(myI2C)
 
 ss.pin_mode(15, ss.OUTPUT);
 
-
 while True:
-	ss.digital_write(15, True)   # turn the LED on (True is the voltage level)
-	time.sleep(1)                # wait for a second
-	ss.digital_write(15, False)  # turn the LED off by making the voltage LOW
-        time.sleep(1)
+    ss.digital_write(15, True)   # turn the LED on (True is the voltage level)
+    time.sleep(1)                # wait for a second
+    ss.digital_write(15, False)  # turn the LED off by making the voltage LOW
+    time.sleep(1)
