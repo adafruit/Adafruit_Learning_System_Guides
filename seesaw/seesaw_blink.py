bf250bb237d23927522605eae9eacf2ea32ed327
Dan Halbert <halbert@halwitz.org>
2018-06-25 11:06:24 -0400

    pylint

--- seesaw/seesaw_blink.py
+++ seesaw/seesaw_blink.py
@@ -1,13 +1,14 @@
-from board import *
-import busio
-from adafruit_seesaw.seesaw import Seesaw
 import time
 
-myI2C = busio.I2C(SCL, SDA)
+import board
+import busio
+from adafruit_seesaw.seesaw import Seesaw
+
+myI2C = busio.I2C(board.SCL, board.SDA)
 
 ss = Seesaw(myI2C)
 
-ss.pin_mode(15, ss.OUTPUT);
+ss.pin_mode(15, ss.OUTPUT)
 
 while True:
     ss.digital_write(15, True)   # turn the LED on (True is the voltage level)
