d39560116c8f091734e4596d75e90e2ae00d8935
Mikey Sklar <mikesklar@gmail.com>
2018-07-06 12:27:32 -0600

    syntax port for no remote

--- Techno_Tiki_RGB_LED_Torch/Techno_Tiki_No_Remote_Control.py
+++ Techno_Tiki_RGB_LED_Torch/Techno_Tiki_No_Remote_Control.py
@@ -28,28 +28,23 @@ color_animation =   ([255, 0, 0], [255, 36, 0], [255, 72, 0], [255, 109, 0],
 strip = neopixel.NeoPixel(pixpin, pixel_count, brightness=1, auto_write=False)
 
 while True:  # Loop forever...
-    for i range(pixel_count):
+
     # Main loop will update all the pixels based on the animation.
+    for i range(pixel_count):
 
-    # Animation 0, solid color pulse of all pixels.
-    if animation == 0:
-        current_Step = (millis() / speed)%(colorSteps*2-2);
+        # Animation 0, solid color pulse of all pixels.
+        if animation == 0:
+            current_step = (time.monotonic() / speed) % (color_steps * 2 - 2)
+            if (current_step >= color_steps):
+                current_step = color_steps - (current_step - (colorSteps - 2))
 
-    # Animation 1, moving color pulse.  Use position to change brightness.
-    elif animation == 1:
+        # Animation 1, moving color pulse.  Use position to change brightness.
+        elif animation == 1:
+            current_step = (time.monotonic() / speed + i) % (color_steps * 2 - 2);
+            if (current_step >= color_steps):
+                current_step = color_steps - (current_step - (colorSteps - 2))
 
-    for i in range(pixel_count):
-        color = color_animation
-#        color = fancy.palette_lookup(color_animation, offset + i / pixel_count)
-#        color = fancy.gamma_adjust(color, brightness=brightness)
-        strip[i] = color.pack()
+        strip[i] = color_animation[current_step]
+
+    # Show the updated pixels.
     strip.show()
-
-    if fadeup:
-        offset += steps
-        if offset >= 1:
-            fadeup = False
-    else:
-        offset -= steps
-        if offset <= 0:
-            fadeup = True
