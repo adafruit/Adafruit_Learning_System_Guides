862600b107f0cdc532f024825c521d021adba8f8
Mike Barela <mydigitalhome@gmail.com>
2018-07-16 12:39:16 -0400

    Create Crickit-Onboard-NeoPixel.py
    Change the color of the onboard single NeoPixel on any Crickit board

---
+++ Make_It_Glow_With_Crickit/Crickit-Onboard-NeoPixel.py
@@ -0,0 +1,12 @@
+# Using Crickit's onboard NeoPixel 
+# See http://www.color-hex.com/ for more colors and find your fav!
+from adafruit_crickit import crickit
+from adafruit_seesaw.neopixel import NeoPixel
+
+crickit_status_pixel = NeoPixel(crickit.seesaw, 27, 1)  # one NeoPixel pin 27
+
+# Fill them with our favorite color "#0099FF light blue" -> 0x0099FF
+crickit_status_pixel.fill(0x0099FF)
+
+while True:
+    pass
