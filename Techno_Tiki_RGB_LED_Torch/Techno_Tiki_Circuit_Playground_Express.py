2597e57bec7bc7396dad64e8c79a09a6ddcce9c4
Mikey Sklar <mikesklar@gmail.com>
2018-07-17 13:57:21 -0600

    fixed IR exceptions, button logic is correct, removed prints, ready for pylint

--- Techno_Tiki_RGB_LED_Torch/Techno_Tiki_Circuit_Playground_Express.py
+++ Techno_Tiki_RGB_LED_Torch/Techno_Tiki_Circuit_Playground_Express.py
@@ -12,7 +12,7 @@ import neopixel
 import pulseio
 import time
 
-brightness = .1		# 0-1, higher number is brighter
+brightness = 1		# 0-1, higher number is brighter
 
 # Adafruit IR Remote Codes:
 # Button       Code         Button  Code
@@ -61,7 +61,7 @@ power_on = volume_up			# Button that turns on the pixels.  Must be pressed twice
 # for each step within the animation.  Each value is a 24-bit RGB color.  By looping through
 # the columns of a row the colors of pixels will animate.
 color_steps = 8         # Number of steps in the animation.
-color_count = 14		# number of columns/steps
+color_count = 8 		# number of columns/steps
 
 # Build lookup table/palette for the color animations so they aren't computed at runtime.
 color_palette = [
@@ -76,15 +76,15 @@ color_palette = [
 ([0, 255, 255], [0, 218, 255], [0, 182, 255], [0, 145, 255], [0, 109, 255], [0, 72, 255], [0, 36, 255], [0, 0, 255]), # cyan blue
 ([0, 0, 255], [36, 0, 255], [72, 0, 255], [109, 0, 255], [145, 0, 255], [182, 0, 255], [218, 0, 255], [255, 0, 255]), # blue magenta
 ([255, 0, 255], [255, 0, 218], [255, 0, 182], [255, 0, 145], [255, 0, 109], [255, 0, 72], [255, 0, 36], [255, 0, 0]), # magenta red
+]
 
 # Other combos
-([255, 0, 0], [218, 36, 0], [182, 72, 0], [145, 109, 0],  [109, 145, 0], [72, 182, 0], [36, 218, 0], [0, 255, 0]), # red green
-([255, 255, 0], [218, 255, 36], [182, 255, 72], [145, 255, 109], [109, 255, 145], [72, 255, 182], [36, 255, 218], [0, 255, 255]), # yellow cyan
-([0, 255, 0], [0, 218, 36], [0, 182, 72], [0, 145, 109],  [0, 109, 145], [0, 72, 182], [0, 36, 218], [0, 0, 255]), # green blue
-([0, 255, 255], [36, 218, 255], [72, 182, 255], [109, 145, 255],  [145, 109, 255], [182, 72, 255], [218, 36, 255], [255, 0, 255]), # cyan magenta
-([0, 0, 255], [36, 0, 218], [72, 0, 182], [109, 0, 145], [145, 0, 109], [182, 0, 72], [218, 0, 36], [255, 0, 0]), # blue red
-([255, 0, 255], [255, 36, 218], [255, 72, 182], [255, 109, 145], [255, 145, 109], [255, 182, 72], [255, 218, 36], [255, 255, 0]), # magenta yellow
-]
+#([255, 0, 0], [218, 36, 0], [182, 72, 0], [145, 109, 0],  [109, 145, 0], [72, 182, 0], [36, 218, 0], [0, 255, 0]), # red green
+#([255, 255, 0], [218, 255, 36], [182, 255, 72], [145, 255, 109], [109, 255, 145], [72, 255, 182], [36, 255, 218], [0, 255, 255]), # yellow cyan
+#([0, 255, 0], [0, 218, 36], [0, 182, 72], [0, 145, 109],  [0, 109, 145], [0, 72, 182], [0, 36, 218], [0, 0, 255]), # green blue
+#([0, 255, 255], [36, 218, 255], [72, 182, 255], [109, 145, 255],  [145, 109, 255], [182, 72, 255], [218, 36, 255], [255, 0, 255]), # cyan magenta
+#([0, 0, 255], [36, 0, 218], [72, 0, 182], [109, 0, 145], [145, 0, 109], [182, 0, 72], [218, 0, 36], [255, 0, 0]), # blue red
+#([255, 0, 255], [255, 36, 218], [255, 72, 182], [255, 109, 145], [255, 145, 109], [255, 182, 72], [255, 218, 36], [255, 255, 0]), # magenta yellow
 
 # Solid colors fading to dark.
 #([255, 0, 0], [223, 0, 0], [191, 0, 0], [159, 0, 0], [127, 0, 0], [95, 0, 0], [63, 0, 0], [31, 0, 0]), # red
@@ -96,7 +96,6 @@ color_palette = [
 #([139, 0, 255], [121, 0, 223], [104, 0, 191], [86, 0, 159], [69, 0, 127], [52, 0, 95], [34, 0, 63], [17, 0, 31]), # violet
 #([255, 255, 255], [223, 223, 223], [191, 191, 191], [159, 159, 159], [127, 127, 127], [95, 95, 95], [63, 63, 63], [31, 31, 31]), # white
 #([255, 0, 0], [255, 153, 0], [255, 255, 0], [0, 255, 0], [0, 0, 255], [75, 0, 130], [139, 0, 255], [255, 255, 255]), # rainbow colors
-#]
 
 # List of animations speeds (in seconds).  This is how long an animation spends before
 # changing to the next step.  Higher values are slower.
@@ -134,12 +133,15 @@ def read_nec():
 	pulses = decoder.read_pulses(pulsein, max_pulse=5000) 
 	command = None
 
-	print(len(pulses))
-
-	if (len(pulses) >= ir_code_min and len(pulses) <= ir_code_max):
-		code = decoder.decode_bits(pulses)
-		if len(code) > 3:
-			command = code[2]
+	try:
+		if (len(pulses) >= ir_code_min and len(pulses) <= ir_code_max):
+			code = decoder.decode_bits(pulses)
+			if len(code) > 3:
+				command = code[2]
+	except adafruit_irremote.IRNECRepeatException:  # Catches the repeat signal
+		pass
+	except adafruit_irremote.IRDecodeException:  # Failed to decode
+		pass
 
 	return(command)
 
@@ -147,7 +149,10 @@ def handle_remote():
 	global color_index, animation_index, speed_index, last_ir_code
 
 	ir_code = read_nec()
-	print(ir_code)
+
+	if ir_code == None:
+		time.sleep(.1)
+		return
 
 	if ir_code == color_change:
 		color_index = (color_index + 1) % color_count
@@ -183,4 +188,3 @@ while True:  # Loop forever...
 
 	# Show the updated pixels.
 	strip.show()
-	time.sleep(.25)
