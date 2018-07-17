c03c82963dce90b2b9d07393447b79705fe40ee7
Dave Astels <dastels@daveastels.com>
2018-07-16 23:36:39 -0400

    Changes to reflect recent changes to pylint rules

--- CircuitPython_101/basic_data_structures/song_book/debouncer.py
+++ CircuitPython_101/basic_data_structures/song_book/debouncer.py
@@ -28,7 +28,7 @@ Debounce an input pin.
 import time
 import digitalio
 
-class Debouncer(object):
+class Debouncer:
     """Debounce an input pin"""
 
     DEBOUNCED_STATE = 0x01
@@ -45,7 +45,7 @@ class Debouncer(object):
         self.state = 0x00
         self.pin = digitalio.DigitalInOut(pin)
         self.pin.direction = digitalio.Direction.INPUT
-        if mode != None:
+        if mode is not None:
             self.pin.pull = mode
         if self.pin.value:
             self.__set_state(Debouncer.DEBOUNCED_STATE | Debouncer.UNSTABLE_STATE)
