78d57af9769fc372a28d87aa58d8c6381ec51ba5
Dave Astels <dastels@daveastels.com>
2018-07-18 13:56:51 -0400

    Revert "Fix header comment"
    This reverts commit e8adbb9fc83929efb077470ab3dc21e9c54a4e70.

--- Crickit_AdafruitIO/dashboard_control/dashboard_control.ino
+++ Crickit_AdafruitIO/dashboard_control/dashboard_control.ino
@@ -1,6 +1,11 @@
-// Crckit + Adafruit IO Guide code
+// Adafruit IO Publish & Subscribe Example
 //
-// Copyright (c) 2018 Dave Astels
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
 // Licensed under the MIT license.
 //
 // All text above must be included in any redistribution.
