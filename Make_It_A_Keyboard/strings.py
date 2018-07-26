280df0bb1f188ffbc5a8a7e8e1fe07c5f395298d
Mike Barela <mydigitalhome@gmail.com>
2018-07-26 09:47:19 -0400

    Create strings.py
    first commit

---
+++ Make_It_A_Keyboard/strings.py
@@ -0,0 +1,13 @@
+from adafruit_circuitplayground.express import cpx
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+kbd = Keyboard()
+layout = KeyboardLayoutUS(kbd)
+
+while True:
+    if cpx.button_a:
+        # Type 'abc' followed by Enter (a newline).
+        layout.write('Jane Doe\n')
+        while cpx.button_a:
+            pass
