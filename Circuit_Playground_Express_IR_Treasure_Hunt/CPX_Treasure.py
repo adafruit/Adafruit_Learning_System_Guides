d76748d0bcbed8dc250357374781b757b12ce0c0
caternuson <methuzla@speakeasy.net>
2018-07-26 19:28:55 -0700

    one treasure to rule them all...

--- Circuit_Playground_Express_IR_Treasure_Hunt/CPX_Treasure.py
+++ Circuit_Playground_Express_IR_Treasure_Hunt/CPX_Treasure.py
@@ -5,8 +5,8 @@ import adafruit_irremote
 import neopixel
 
 # Configure treasure information
-TREASURE_ID = 2
-TRANSMIT_DELAY = 15
+TREASURE_ID = 1      # CHANGE THIS NUMBER TO 2, 3, ETC, A UNIQUE NUMBER FOR EACH TREASURE!
+TRANSMIT_DELAY = 15  # change this as desired to affect game dynamics, or just leave as is
 
 # Create NeoPixel object to indicate status
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
@@ -20,6 +20,7 @@ encoder = adafruit_irremote.GenericTransmit(header=[9500, 4500],
                                             one=[550, 550],
                                             zero=[550, 1700],
                                             trail=0)
+
 while True:
     pixels.fill(0xFF0000)
     encoder.transmit(pulseout, [TREASURE_ID]*4)
