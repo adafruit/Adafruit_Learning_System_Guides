60a9b75d67cd3dcb979722f2dcf04c7eb03fa723
Mike Barela <mydigitalhome@gmail.com>
2018-07-31 16:44:54 -0400

    Create cpx-cap-touch.py
    first run for cap touch

---
+++ Make_It_Sense/cpx-cap-touch.py
@@ -0,0 +1,20 @@
+from adafruit_circuitplayground.express import cpx
+
+# NeoPixel blank out
+cpx.pixels.fill( (0, 0, 0) )
+
+# Set sensitivity of all touch pads
+cpx.adjust_touch_threshold(200)
+
+# Check for touch, if so light the nearby NeoPixel green
+while True:
+    if cpx.touch_A4:
+        cpx.pixels[0] = (0, 30, 0)
+    if cpx.touch_A5:
+        cpx.pixels[1] = (0, 30, 0)
+    if cpx.touch_A6:
+        cpx.pixels[3] = (0, 30, 0)
+    if cpx.touch_A7:
+        cpx.pixels[4] = (0, 30, 0)
+    if cpx.button_a:  # blank lights on Button A
+        cpx.pixels.fill( (0, 0, 0) )
