3b900f20c5322cef36fbd30364e8aee03f44b718
Mike Barela <mydigitalhome@gmail.com>
2018-08-08 16:33:53 -0400

    Create serial-read-write.py
    Initial code for Circuit Playground Express Serial Communications guide

---
+++ CPX_Serial/serial-read-write.py
@@ -0,0 +1,18 @@
+import time
+import board
+import busio
+from adafruit_circuitplayground.express import cpx
+ 
+uart = busio.UART(board.TX, board.RX, baudrate=115200)
+ 
+while True:
+    
+    data = uart.read(1)  # read a byte
+    
+    if data is not None:  # Data was received
+    
+        output = "%0.1f\t%d\t%0.1f\r\n" % (time.monotonic(), 
+                                    cpx.light, cpx.temperature)
+        uart.write(output)         # Print to serial 
+    
+        time.sleep(1.0)
