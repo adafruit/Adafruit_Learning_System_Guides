376951bded78c7c978ffb5bdc7c4752b23640fc3
Mikey Sklar <mikesklar@gmail.com>
2018-09-01 15:15:59 -0600

    verified ino is good, using simpleio for notes, starting on m0 port

---
+++ Trinket_Question_Block_Sound_Jewelry/Trinket_Question_Block_Sound_Jewelry.py
@@ -0,0 +1,9 @@
+import time
+import board
+import simpleio
+
+while True:
+    for f in (262, 294, 330, 349, 392, 440, 494, 523):
+        simpleio.tone(board.D2, f, 0.25)  # on for 1/4 second
+        time.sleep(0.05)  # pause between notes
+    time.sleep(0.5)
