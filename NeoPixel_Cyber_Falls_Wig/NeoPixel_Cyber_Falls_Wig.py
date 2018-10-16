4b464810681355b8e9369018546dc80939503cdb
Mikey Sklar <mikesklar@gmail.com>
2018-10-16 16:00:33 -0600

    works with list of neopixel strips, code is stable

--- NeoPixel_Cyber_Falls_Wig/NeoPixel_Cyber_Falls_Wig.py
+++ NeoPixel_Cyber_Falls_Wig/NeoPixel_Cyber_Falls_Wig.py
@@ -4,27 +4,29 @@ import neopixel
 import time
 from digitalio import DigitalInOut, Direction, Pull
 
-led_pin = board.D0  # which pin your pixels are connected to
-num_leds = 16       # how many LEDs you have
-brightness = .1     # 0-1, higher number is brighter
-saturation = 255 # 0-255, 0 is pure white, 255 is fully saturated color
-blend = True  # color blending between palette indices
-simultaneous_leds = 8
-duration_on = 0.04     # 40ms
+num_leds = 10       # number of LEDs per strip
+saturation = 255    # 0-255, 0 is pure white, 255 is fully saturated color
+blend = True        # color blending between palette indices
+brightness = 0.1
+concurrent = 2
+on_time = 0.04      # 40ms
 
 # initialize list with all pixels off
 palette = [0] * num_leds
 
-# Declare a NeoPixel object on led_pin with num_leds as pixels
-# No auto-write.
-# Set brightness to max.
-# We will be using FancyLED's brightness control.
-strip = neopixel.NeoPixel(led_pin, num_leds, brightness=1)
+# NeoPixel objects with led_pin and number of LEDs
+drop0 = neopixel.NeoPixel(board.D0, num_leds)
+drop1 = neopixel.NeoPixel(board.D1, num_leds)
+drop2 = neopixel.NeoPixel(board.D2, num_leds)
+drop3 = neopixel.NeoPixel(board.D3, num_leds)
+drop4 = neopixel.NeoPixel(board.D4, num_leds)
+
+# list of strips
+drop_list = [drop0, drop1, drop2, drop3, drop4]
 
 # FancyLED allows for assigning a color palette using these formats:
 # see FastLED - colorpalettes.cpp
-
-while True:
+def led_drops(strip):
 
     palette = [fancy.CRGB(150, 255, 150),          # lighter green
                 fancy.CRGB(0, 255, 0)]        # full green
@@ -34,14 +36,18 @@ while True:
         color = fancy.gamma_adjust(color, brightness=brightness)
         strip[i] = color.pack()
 
-        if  simultaneous_leds <= i >= (num_leds - simultaneous_leds):
-            strip[i - simultaneous_leds] = (0,0,0)
+        if  i >= concurrent:
+            strip[i - concurrent] = (0,0,0)
 
         if i >= num_leds - 1:
-            for j in range(simultaneous_leds,-1,-1):
+            for j in range(concurrent,-1,-1):
                 strip[i-j] = (0,0,0)
-                time.sleep(duration_on)
+                time.sleep(on_time)
 
-        time.sleep(duration_on)
+        time.sleep(on_time)
 
-    time.sleep(1)
+    
+while True:
+
+    for strip in drop_list:
+        led_drops(strip)
