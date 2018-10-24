cd6f67938ada5467de9ef49877040b54a3800bb6
Mikey Sklar <mikesklar@gmail.com>
2018-10-23 18:42:14 -0600

    code runs clean, pylint is clear of warnings

--- Laser_Dog_Goggles/Laser_Dog_Goggles.py
+++ Laser_Dog_Goggles/Laser_Dog_Goggles.py
@@ -1,5 +1,5 @@
-# Trinket Gemma Servo Control
-# for Adafruit M0 boards
+# Laser Dog Goggles
+# https://learn.adafruit.com/laser-dog-goggles
 
 import time
 import board
@@ -10,7 +10,7 @@ from adafruit_motor import servo
 pwm = pulseio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
 my_servo = servo.Servo(pwm)
 speed = .04     # 40ms lower value means faster movement
-max_turn = 180  # 150 degrees
+max_turn = 180  # rotation range 180 degree, half a circle
 
 while True:
     # move stepper max_turn degrees clockwise
