0da4bd79e716f866b09c5310303cf304fd312464
Dave Astels <dastels@daveastels.com>
2018-10-26 17:37:29 -0400

    Tweak and refactor

--- BalanceBot/code.py
+++ BalanceBot/code.py
@@ -14,11 +14,11 @@ All text above must be included in any redistribution.
 """
 
 
-import busio
-import board
 import time
 import array
 import math
+import busio
+import board
 from adafruit_seesaw.seesaw import Seesaw
 from adafruit_seesaw.pwmout import PWMOut
 from adafruit_motor import servo
@@ -59,6 +59,15 @@ servo1.throttle = SERVO1_ZERO_ADJUST
 servo2.throttle = SERVO2_ZERO_ADJUST
 
 
+def adjust(original, op, value):
+    if op == b'=':
+        return value
+    elif op == b'+':
+        return original + value
+    else:
+        return original - value
+
+
 # A line was received via the UART, process it
 
 def process_command(cmd, kp, ki, kd):
@@ -73,7 +82,7 @@ def process_command(cmd, kp, ki, kd):
         """Inner function to write the constants to the UART"""
         uart.write("KP: {0: 0.3f}  KI: {1: 0.3f}  KD: {2: 0.3f}\r\n".format(kp, ki, kd))
 
-    if cmd[0] == 63:
+    if cmd[0] == b'?':
         report(kp, ki, kd)
     elif cmd[0] not in [b'p', b'i', b'd']:
         uart.write("Bad parameter\r\n")
@@ -92,26 +101,11 @@ def process_command(cmd, kp, ki, kd):
             return (kp, ki, kd)
 
         if var == b'p':
-            if op == b'=':
-                kp = value
-            elif op == b'+':
-                kp += value
-            else:
-                kp -= value
+            kp = adjust(kp, op, value)
         elif var == b'i':
-            if op == b'=':
-                ki = value
-            elif op == b'+':
-                ki += value
-            else:
-                ki -= value
+            ki = adjust(ki, op, value)
         else:
-            if op == b'=':
-                kd = value
-            elif op == b'+':
-                kd += value
-            else:
-                kd -= value
+            kd = adjust(kd, op, value)
         report(kp, ki, kd)
     else:
         uart.write("Bad command\r\n")
@@ -123,91 +117,85 @@ def limit(x):
     """Limit the argument to the range -1.0 to 1.0"""
     return max([-1.0, min([1.0, x])])
 
-def run():
-    kp = 2.000
-    ki = 1.000
-    kd = 0.050
-    iterm = 0.0
-    dterm = 0.0
+kp = 2.000
+ki = 1.000
+kd = 0.050
+iterm = 0.0
+dterm = 0.0
 
-    extreme_z_count = 0
-    error = 0.0
-    previous_error = 0.0
-    previous_time = 0.0
-    output = 0.0
-    cmd_buffer = array.array("B", [0] * 8)
-    cmd_buffer_index = 0
+extreme_z_count = 0
+error = 0.0
+previous_error = 0.0
+previous_time = 0.0
+output = 0.0
+cmd_buffer = array.array("B", [0] * 8)
+cmd_buffer_index = 0
 
-    while True:
-        loop_start_time = time.monotonic()
+while True:
+    loop_start_time = time.monotonic()
 
-        # Process UART input and process complete lines
-        if uart.in_waiting > 0:
-            ch = uart.read(1)
-            if cmd_buffer_index > 7:
-                cmd_buffer_index = 0
-                uart.write("command too long.. ignored\r\n")
-            else:
-                uart.write(ch)
-                if ch == b'\r':
-                    if cmd_buffer_index > 0:
-                        kp, ki, kd = process_command(cmd_buffer[0:cmd_buffer_index], kp, ki, kd)
-                    cmd_buffer_index = 0
-                else:
-                    cmd_buffer[cmd_buffer_index] = ch[0]
-                    cmd_buffer_index += 1
-            continue
-
-
-        _, _, z = sensor.accelerometer
-        # print("z: {0: 0.3f}, adj: {1: 0.3f}".format(z, z + ACCEL_ZERO_ADJUST))
-
-        # Check if the bot fell over: a high Z value for a second.
-        # If so stop the servos and blink the tilt LED
-        # When it's righted, continue
-        if math.fabs(z) >= 6.0:
-            extreme_z_count += 1
-            if extreme_z_count > 50:
-                servo1.throttle = 0.0
-                servo2.throttle = 0.0
-                while math.fabs(z) >= 6.0:
-                    led.value = True
-                    time.sleep(0.2)
-                    led.value = False
-                    time.sleep(0.2)
-                    _, _, z = sensor.accelerometer
-                    # print("z: {0: 03f}".format(z))
-                extreme_z_count = 0
-                previous_error = 0.0
-                iterm = 0.0
-                previous_time = loop_start_time
-                error = 0.0
-                continue
+    # Process UART input and process complete lines
+    if uart.in_waiting > 0:
+        ch = uart.read(1)
+        if cmd_buffer_index > 7:
+            cmd_buffer_index = 0
+            uart.write("command too long.. ignored\r\n")
         else:
+            uart.write(ch)
+            if ch == b'\r':
+                if cmd_buffer_index > 0:
+                    kp, ki, kd = process_command(cmd_buffer[0:cmd_buffer_index], kp, ki, kd)
+                cmd_buffer_index = 0
+            else:
+                cmd_buffer[cmd_buffer_index] = ch[0]
+                cmd_buffer_index += 1
+        continue
+
+
+    _, _, z = sensor.accelerometer
+    # print("z: {0: 0.3f}, adj: {1: 0.3f}".format(z, z + ACCEL_ZERO_ADJUST))
+
+    # Check if the bot fell over: a high Z value for a second.
+    # If so stop the servos and blink the tilt LED
+    # When it's righted, continue
+    if math.fabs(z) >= 6.0:
+        extreme_z_count += 1
+        if extreme_z_count > 50:
+            servo1.throttle = 0.0
+            servo2.throttle = 0.0
+            while math.fabs(z) >= 6.0:
+                led.value = True
+                time.sleep(0.2)
+                led.value = False
+                time.sleep(0.2)
+                _, _, z = sensor.accelerometer
+                # print("z: {0: 03f}".format(z))
             extreme_z_count = 0
+            previous_error = 0.0
+            iterm = 0.0
+            previous_time = loop_start_time
+            error = 0.0
+            continue
+    else:
+        extreme_z_count = 0
 
-        # process the error
-        error = (z + ACCEL_ZERO_ADJUST) / 10.0
+    # process the error
+    error = (z + ACCEL_ZERO_ADJUST) / 10.0
 
-        delta_time = loop_start_time - previous_time
-        delta_error = error - previous_error
+    delta_time = loop_start_time - previous_time
+    delta_error = error - previous_error
 
-        if previous_time > 0.0:
-            iterm += error * delta_time
-            dterm = 0.0
-            if delta_time > 0:
-                dterm = delta_error / delta_time
+    if previous_time > 0.0:
+        iterm += error * delta_time
+        dterm = 0.0
+        if delta_time > 0:
+            dterm = delta_error / delta_time
 
-            output = limit((kp * error) + (ki * iterm) + (kd * dterm))
-            servo1.throttle = limit(output + SERVO1_ZERO_ADJUST)
-            servo2.throttle = limit((-1 * output) + SERVO2_ZERO_ADJUST)
+        output = limit((kp * error) + (ki * iterm) + (kd * dterm))
+        servo1.throttle = limit(output + SERVO1_ZERO_ADJUST)
+        servo2.throttle = limit((-1 * output) + SERVO2_ZERO_ADJUST)
 
-        previous_error = error
-        previous_time = loop_start_time
+    previous_error = error
+    previous_time = loop_start_time
 
-
-        # print("error: {0: 0.3f}, iterm: {1: 0.3f}, dterm: {2: 0.3f},  output: {3: 0.3f}".format(error, iterm, dterm, output))
-
-        time.sleep(SAMPLE_INTERVAL - (time.monotonic() - loop_start_time))
-
-run()
+    time.sleep(SAMPLE_INTERVAL - (time.monotonic() - loop_start_time))
