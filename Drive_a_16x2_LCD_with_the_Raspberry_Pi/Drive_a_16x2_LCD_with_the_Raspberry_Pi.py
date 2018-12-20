22ff889afa749d1367dcb87ed0dc9403438e73bb
Mikey Sklar <mikesklar@gmail.com>
2018-12-20 15:04:12 -0700

    pylint clean for updted 16x2 CP RPI code

--- Drive_a_16x2_LCD_with_the_Raspberry_Pi/Drive_a_16x2_LCD_with_the_Raspberry_Pi.py
+++ Drive_a_16x2_LCD_with_the_Raspberry_Pi/Drive_a_16x2_LCD_with_the_Raspberry_Pi.py
@@ -1,11 +1,9 @@
-import time
+from subprocess import Popen, PIPE
+from time import sleep
+from datetime import datetime
 import board
 import digitalio
 import adafruit_character_lcd.character_lcd as characterlcd
-import re
-from subprocess import *
-from time import sleep, strftime
-from datetime import datetime
 
 # Modify this if you have a different sized character LCD
 lcd_columns = 16
@@ -25,35 +23,46 @@ lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
                                       lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 # looking for an active Ethernet or WiFi device
-find_interface = "ip addr show | grep \"state UP\" | cut -d: -f2"
+def find_interface():
+    find_device = "ip addr show"
+    interface_parse = run_cmd(find_device)
+    for line in interface_parse.splitlines():
+        if "state UP" in line:
+            dev_name = line.split(':')[1]
+    return dev_name
 
-# find a live IP on the first LIVE network device
-def parse_ip(device):
-        find_ip = "ip addr show %s" % interface
-        ip_parse = run_cmd(find_ip)
-        for line in ip_parse.splitlines():
-            if "inet " in line:
-                ip = line.split(' ')[5]
-                ip = ip.split('/')[0]
-                return ip
+# find an active IP on the first LIVE network device
+def parse_ip():
+    find_ip = "ip addr show %s" % interface
+    ip_parse = run_cmd(find_ip)
+    for line in ip_parse.splitlines():
+        if "inet " in line:
+            ip = line.split(' ')[5]
+            ip = ip.split('/')[0]
+    return ip
 
+# run unix shell command, return as ASCII
 def run_cmd(cmd):
-        p = Popen(cmd, shell=True, stdout=PIPE)
-        output = p.communicate()[0]
-        return output.decode('ascii')
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output.decode('ascii')
 
 # wipe LCD screen before we start
 lcd.clear()
 
+# before we start the main loop - detect active network device and ip address
+interface = find_interface()
+ip_address = parse_ip()
+
 while True:
-        interface = run_cmd(find_interface)
 
-        # date and time (line 1)
-        lcd_line_1 = datetime.now().strftime('%b %d  %H:%M:%S\n') 
+    # date and time
+    lcd_line_1 = datetime.now().strftime('%b %d  %H:%M:%S\n')
 
-        # current ip address (line 2)
-        lcd_line_2 = "IP " + parse_ip(interface)
+    # current ip address
+    lcd_line_2 = "IP " + ip_address
 
-        lcd.message = lcd_line_1 + lcd_line_2
+    # combine both lines into one update to the display
+    lcd.message = lcd_line_1 + lcd_line_2
 
-        sleep(2)
+    sleep(2)
