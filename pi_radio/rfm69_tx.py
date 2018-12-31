4800bd327f1188e5f7ce5eba87ebc0f37524801b
brentru <brent@adafruit.com>
2018-12-31 15:17:24 -0500

    add rfm69 tx code, pylint

--- pi_radio/rfm69_tx.py
+++ pi_radio/rfm69_tx.py
@@ -1,13 +1,13 @@
-# Simple transmitter for RFM9x
+# Simple transmitter for RFM69
 import time
 import board
 import busio
 import digitalio
-import adafruit_rfm9x
+import adafruit_rfm69
 
 # Define pins connected to the chip, use these if wiring up the breakout according to the guide:
-CS = digitalio.DigitalInOut(board.D6)
-RESET = digitalio.DigitalInOut(board.D5)
+CS = digitalio.DigitalInOut(board.D5)
+RESET = digitalio.DigitalInOut(board.D6)
 
 # Define the onboard LED
 LED = digitalio.DigitalInOut(board.D13)
@@ -17,22 +17,31 @@ LED.direction = digitalio.Direction.OUTPUT
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
 # Initialze RFM radio
-rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, 915.0)
 
-# You can however adjust the transmit power (in dB).  The default is 13 dB but
-# high power radios like the RFM95 can go up to 23 dB:
-rfm9x.tx_power = 23
+# Optionally set an encryption key (16 byte AES key). MUST match both
+# on the transmitter and receiver (or be set to None to disable/the default).
+rfm69.encryption_key = b'\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08'
 
 # Packet counter, incremented per tx
 packetNum = 0
+
+rfm69.send(bytes('Hello world!\r\n',"utf-8"))
+print('Sent hello world message!')
+
+# Wait to receive packets.  Note that this library can't receive data at a fast
+# rate, in fact it can only receive and process one 60 byte packet at a time.
+# This means you should only use this for low bandwidth scenarios, like sending
+# and receiving a single message at a time.
+print('Waiting for packets...')
 while True:
-    rfm9x.send(bytes("Hello Pi #","utf-8"))
+    rfm69.send(bytes("Hello Pi #","utf-8"))
     packetNum+=1
     print(packetNum)
-    rfm9x.send(bytes(packetNum))
+    rfm69.send(bytes(packetNum))
 
     # Listen for a new packet
-    packet = rfm9x.receive()
+    packet = rfm69.receive()
     if packet is None:
         print('No reply, is there a transmitter around?')
         LED.value = False
@@ -47,10 +56,5 @@ while True:
         # sending side is sending ASCII data before you try to decode!
         packet_text = str(packet, 'ascii')
         print('Received (ASCII): {0}'.format(packet_text))
-        # Also read the RSSI (signal strength) of the last received message and
-        # print it.
-        rssi = rfm9x.rssi
-        print('Received signal strength: {0} dB'.format(rssi))
     # Wait one second between Tx/Rx
     time.sleep(1)
-    
