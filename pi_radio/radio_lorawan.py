3f90b010f5b956763e625bbc8aafe0b874b7c632
brentru <brent@adafruit.com>
2018-12-31 10:20:23 -0500

    add LoRaWAN example

---
+++ pi_radio/radio_lorawan.py
@@ -0,0 +1,78 @@
+"""
+Example for using the RFM9x Radio with Raspberry Pi and LoRaWAN
+
+Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
+Author: Brent Rubell for Adafruit Industries
+"""
+import time
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+# Import thte SSD1306 module.
+import adafruit_ssd1306
+# Import Adafruit TinyLoRa
+from adafruit_tinylora.adafruit_tinylora import TTN, TinyLoRa
+
+# Button A
+btnA = DigitalInOut(board.D26)
+btnA.direction = Direction.INPUT
+btnA.pull = Pull.UP
+
+# Button B
+btnB = DigitalInOut(board.D19)
+btnB.direction = Direction.INPUT
+btnB.pull = Pull.UP
+
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# 128x32 OLED Display
+display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3c)
+# Clear the display.
+display.fill(0)
+display.show()
+width = display.width
+height = display.height
+
+# TinyLoRa Configuration
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = DigitalInOut(board.D18)
+irq = DigitalInOut(board.D25)
+# TTN Device Address, 4 Bytes, MSB
+devaddr = bytearray([0x00, 0x00, 0x00, 0x00])
+# TTN Network Key, 16 Bytes, MSB
+nwkey = bytearray([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+# TTN Application Key, 16 Bytess, MSB
+app = bytearray([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+# Initialize ThingsNetwork configuration
+ttn_config = TTN(devaddr, nwkey, app, country='US')
+# Initialize lora object
+lora = TinyLoRa(spi, cs, irq, ttn_config)
+# Data to send to TTN
+data = bytearray(b"\x01\x02\x03\x04")
+
+while True:
+    packet = None
+    # draw a box to clear the image
+    display.fill(0)
+    display.text('RasPi LoRaWAN', 35, 0, 1)
+
+    if not btnA.value:
+        # Send Packet
+        lora.send_data(data, len(data), lora.frame_counter)
+        display.fill(0)
+        display.text('Sent Packet to TTN!', 0, 25, 1)
+        display.show()
+        time.sleep(0.1)
+    if not btnB.value:
+        # Display pkt data
+        display.fill(0)
+        display.text('PKT Data:', 35, 0, 1)
+        display.text(data.decode(), 0, 25, 1)
+        display.show()
+        time.sleep(0.1)
+
+    display.show()
+    time.sleep(.1)
