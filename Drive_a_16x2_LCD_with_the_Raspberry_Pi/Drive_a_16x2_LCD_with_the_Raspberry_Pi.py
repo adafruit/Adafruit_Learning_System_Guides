9402a02d8e2be1ac51b3aec79a6e5c88c872592e
mikeysklar <mikeysklar@gmail.com>
2019-01-04 15:59:35 -0700

    changed GPIO pins used on RPI to be compatible with rev. 1, 2 and B+

--- Drive_a_16x2_LCD_with_the_Raspberry_Pi/Drive_a_16x2_LCD_with_the_Raspberry_Pi.py
+++ Drive_a_16x2_LCD_with_the_Raspberry_Pi/Drive_a_16x2_LCD_with_the_Raspberry_Pi.py
@@ -9,18 +9,17 @@ import adafruit_character_lcd.character_lcd as characterlcd
 lcd_columns = 16
 lcd_rows = 2
 
-# Raspberry Pi Pin Config:
-lcd_rs = digitalio.DigitalInOut(board.D26)
-lcd_en = digitalio.DigitalInOut(board.D19)
-lcd_d7 = digitalio.DigitalInOut(board.D27)
-lcd_d6 = digitalio.DigitalInOut(board.D22)
-lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_rs = digitalio.DigitalInOut(board.D22)
+lcd_en = digitalio.DigitalInOut(board.D17)
 lcd_d4 = digitalio.DigitalInOut(board.D25)
-lcd_backlight = digitalio.DigitalInOut(board.D4)
+lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_d6 = digitalio.DigitalInOut(board.D23)
+lcd_d7 = digitalio.DigitalInOut(board.D18)
+
 
 # Initialise the lcd class
 lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
-                                      lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+                                      lcd_d7, lcd_columns, lcd_rows)
 
 # looking for an active Ethernet or WiFi device
 def find_interface():
