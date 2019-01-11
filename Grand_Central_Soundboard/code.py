3d9a9cca0b681750b64a3184bf4b4f52090ea614
Mike Barela <mydigitalhome@gmail.com>
2019-01-11 13:27:48 -0500

    linty

--- Grand_Central_Soundboard/code.py
+++ Grand_Central_Soundboard/code.py
@@ -4,16 +4,16 @@ import digitalio
 import audioio
 import board
 import adafruit_matrixkeypad
- 
+
 # Membrane 3x4 matrix keypad - https://www.adafruit.com/product/419
 cols = [digitalio.DigitalInOut(x) for x in (board.D3, board.D2, board.D1)]
 rows = [digitalio.DigitalInOut(x) for x in (board.D7, board.D6, board.D5, board.D4)]
- 
+
 keys = ((1, 2, 3),
         (4, 5, 6),
         (7, 8, 9),
         ('*', 0, '#'))
- 
+
 keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
 
 wavefiles = [file for file in os.listdir("/sounds/")
@@ -32,7 +32,7 @@ def play_file(filename):
     f = open(filename, "rb")
     wav = audioio.WaveFile(f)
     gc_audio.play(wav)
- 
+
 while True:
     keys = keypad.pressed_keys
     if keys:
