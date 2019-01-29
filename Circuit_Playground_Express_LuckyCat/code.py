ed7eef714ae3ab4c89693167c06cd2ac018f644a
Mike Barela <mydigitalhome@gmail.com>
2019-01-29 17:36:11 -0500

    remove two spaces

--- Circuit_Playground_Express_LuckyCat/code.py
+++ Circuit_Playground_Express_LuckyCat/code.py
@@ -5,10 +5,10 @@ import time
 import board
 import pulseio
 from adafruit_motor import servo
- 
+
 # create a PWMOut object on Pin A1.
 pwm = pulseio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
- 
+
 # Create a servo object, my_servo
 my_servo = servo.Servo(pwm)
 
