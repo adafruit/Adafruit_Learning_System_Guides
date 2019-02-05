cdaf7424348a2ff4fd6d36646b7243779809c3b1
Mike Barela <mydigitalhome@gmail.com>
2019-02-05 17:30:34 -0500

    update per Limor suggestions

--- Circuit_Playground_Express_LuckyCat/code.py
+++ Circuit_Playground_Express_LuckyCat/code.py
@@ -6,18 +6,18 @@ import board
 import pulseio
 from adafruit_motor import servo
 
-# create a PWMOut object on Pin A1.
-pwm = pulseio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
+# create a PWMOut object on Pin A1
+pwm = pulseio.PWMOut(board.A1, frequency=50)
 
 # Create a servo object, my_servo
 my_servo = servo.Servo(pwm)
 
 while True:
-    for angle in range(50):  # 0 to 49 degrees in 1 deg steps
+    for angle in range(50):         # 0 to 49 degrees in 1 deg steps
         my_servo.angle = angle
-        time.sleep(0.0045)   # Tiny delay each steps
-    time.sleep(0.25)        # More time at end of arm down
-    for angle in range(50):  # 49 to 0 degrees in 1 deg steps
-        my_servo.angle = 49 - angle
-        time.sleep(0.0045)
-    time.sleep(0.25)        # More time when arm up
+        time.sleep(0.005)           # Tiny delay each steps
+    time.sleep(0.25)                # More time at end of arm down
+    for angle in range(50, 0, -1):  # 50 to 0 degrees in 1 deg steps
+        my_servo.angle = angle
+        time.sleep(0.005)
+    time.sleep(0.25)                # More time when arm up
