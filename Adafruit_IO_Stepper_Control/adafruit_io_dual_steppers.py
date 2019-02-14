e8dcf837d6cdfbb293819c309f5b586d732c1a02
brentru <robots199Wme.com>
2019-02-14 11:36:09 -0500

    refactor loop

--- Adafruit_IO_Stepper_Control/adafruit_io_dual_steppers.py
+++ Adafruit_IO_Stepper_Control/adafruit_io_dual_steppers.py
@@ -40,7 +40,7 @@ ADAFRUIT_IO_USERNAME = ''
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 # Delay between checking for new stepper slider value on Adafruit IO, in seconds
-ADAFRUIT_IO_DELAY = 15
+ADAFRUIT_IO_DELAY = 1
 
 # Stepper 1 Adafruit IO Feeds
 feed_step_1_steps = aio.feeds('stepper1steps')
@@ -52,6 +52,7 @@ feed_step_2_direction = aio.feeds('stepper2direction')
 feed_step_2_step_size = aio.feeds('stepper2stepsize')
 # Shutdown Feed
 feed_stepper_shutdown = aio.feeds('steppershutdown')
+feed_steppers_status = aio.feeds('stepperstart')
 
 # create a default object, no changes to I2C address or frequency
 kit = MotorKit()
@@ -86,69 +87,65 @@ def stepper_worker(stepper, numsteps, direction, stepper_name, style, show_steps
         aio.send(feed_step_2_steps.key, 0)
 
 while True:
-    # Retrieve Data from Adafruit IO
-
-    # Stepper Shutdown
-    is_shutdown = aio.receive(feed_stepper_shutdown.key)
-    if is_shutdown.value == 1:
-        print('* SHUTTING OFF BOTH STEPPER MOTORS *')
-        turnOffMotors()
+    try:
+        stepper_start = aio.receive(feed_steppers_status.key)
+        print('Stepper Button Pressed? ', stepper_start.value)
+    except Adafruit_IO.errors.ThrottlingError:
+        print('Exceeded the limit of Adafruit IO requests, delaying 30 seconds...')
+        time.sleep(30)
 
     # Stepper 1
-    if not st1.isAlive(): # if thread is killed
-        print('Retrieving Stepper 1 Config from IO...')
+    if not st1.isAlive() and int(stepper_start.value):
+        print('Stepper 1 Configuration (from IO)')
+        stepper_1_direction = aio.receive(feed_step_1_direction.key)
+        stepper_1_step_size = aio.receive(feed_step_1_step_size.key)
+        time.sleep(1)
         stepper_1_steps = aio.receive(feed_step_1_steps.key)
         stepper_1_steps = int(stepper_1_steps.value)
-        if stepper_1_steps is not 0: # check against if the feed value is 0
-            stepper_1_direction = aio.receive(feed_step_1_direction.key)
-            stepper_1_step_size = aio.receive(feed_step_1_step_size.key)
-            print('Stepper 1 Configuration (from IO)')
-            print("%d steps" % stepper_1_steps)
-            print('Step Size: ', stepper_1_step_size.value)
-            print('Stepper Direction: ', stepper_1_direction.value)
-            stepper_name = "Stepper 1"
-            # Set Stepper Direction
-            if stepper_1_direction.value == 'Forward':
-                move_dir = STEPPER.FORWARD
-                print("forward")
-            elif stepper_1_direction.value == 'Backward':
-                move_dir = STEPPER.BACKWARD
-                print("backward")
-            # Stepper 1 Thread
-            st1 = threading.Thread(target=stepper_worker, args=(kit.stepper1,
-                                                                stepper_1_steps,
-                                                                move_dir,
-                                                                stepper_name,
-                                                                stepstyles[STEPPER.SINGLE],))
-            st1.start()
-    
+        print("%d steps" % stepper_1_steps)
+        print('Step Size: ', stepper_1_step_size.value)
+        print('Stepper Direction: ', stepper_1_direction.value)
+        stepper_name = "Stepper 1"
+        # Set Stepper Direction
+        if stepper_1_direction.value == 'Forward':
+            move_dir = STEPPER.FORWARD
+            print("forward")
+        elif stepper_1_direction.value == 'Backward':
+            move_dir = STEPPER.BACKWARD
+            print("backward")
+        # Stepper 1 Thread
+        st1 = threading.Thread(target=stepper_worker, args=(kit.stepper1,
+                                                            stepper_1_steps,
+                                                            move_dir,
+                                                            stepper_name,
+                                                            stepstyles[STEPPER.SINGLE],))
+        st1.start()
+
     # Stepper 2
-    if not st2.isAlive(): # if thread is killed
-        print('Retrieving Stepper 2 Config from IO...')
+    if not st2.isAlive() and int(stepper_start.value):
         stepper_2_steps = aio.receive(feed_step_2_steps.key)
         stepper_2_steps = int(stepper_2_steps.value)
-        if stepper_2_steps is not 0: # check against if the feed value is 0
-            stepper_2_direction = aio.receive(feed_step_2_direction.key)
-            stepper_2_step_size = aio.receive(feed_step_2_step_size.key)
-            print('Stepper 2 Configuration (from IO)')
-            print("%d steps" % stepper_2_steps)
-            print('Step Size: ', stepper_2_step_size.value)
-            print('Stepper Direction: ', stepper_2_direction.value)
-            stepper_name = "Stepper 2"
-            # Set Stepper Direction
-            if stepper_2_direction.value == 'Forward':
-                move_dir = STEPPER.FORWARD
-                print("forward")
-            elif stepper_2_direction.value == 'Backward':
-                move_dir = STEPPER.BACKWARD
-                print("backward")
-            # Stepper 2 Thread
-            st2 = threading.Thread(target=stepper_worker, args=(kit.stepper2,
-                                                                stepper_2_steps,
-                                                                move_dir,
-                                                                stepper_name,
-                                                                stepstyles[STEPPER.SINGLE],))
-            st2.start()
+        stepper_2_direction = aio.receive(feed_step_2_direction.key)
+        stepper_2_step_size = aio.receive(feed_step_2_step_size.key)
+        print('Stepper 2 Configuration (from IO)')
+        print("%d steps" % stepper_2_steps)
+        print('Step Size: ', stepper_2_step_size.value)
+        print('Stepper Direction: ', stepper_2_direction.value)
+        stepper_name = "Stepper 2"
+        # Set Stepper Direction
+        if stepper_2_direction.value == 'Forward':
+            move_dir = STEPPER.FORWARD
+            print("forward")
+        elif stepper_2_direction.value == 'Backward':
+            move_dir = STEPPER.BACKWARD
+            print("backward")
+        # Stepper 2 Thread
+        st2 = threading.Thread(target=stepper_worker, args=(kit.stepper2,
+                                                            stepper_2_steps,
+                                                            move_dir,
+                                                            stepper_name,
+                                                            stepstyles[STEPPER.SINGLE],))
+        st2.start()
 
     # poll for new data every 30sec to avoid Adafruit IO Timeouts
     print('delaying checking Adafruit IO for %d seconds...' % ADAFRUIT_IO_DELAY)
