18da2eff4989cbc8c22226981d20edc9b5dc8d48
Mikey Sklar <mikesklar@gmail.com>
2019-03-01 14:58:30 -0700

    existing code runs well with python v2 manual invocation

---
+++ Basic_Resistor_Sensor_Reading_on_Raspberry_Pi/Basic_Resistor_Sensor_Reading_on_Raspberry_Pi.py
@@ -0,0 +1,24 @@
+# Example for RC timing reading for Raspberry Pi
+# Must be used with GPIO 0.3.1a or later - earlier verions
+# are not fast enough!
+
+import RPi.GPIO as GPIO, time, os      
+
+DEBUG = 1
+GPIO.setmode(GPIO.BCM)
+
+def RCtime (RCpin):
+        reading = 0
+        GPIO.setup(RCpin, GPIO.OUT)
+        GPIO.output(RCpin, GPIO.LOW)
+        time.sleep(0.1)
+
+        GPIO.setup(RCpin, GPIO.IN)
+        # This takes about 1 millisecond per loop cycle
+        while (GPIO.input(RCpin) == GPIO.LOW):
+                reading += 1
+        return reading
+
+while True:                                     
+        print RCtime(18)     # Read RC timing using pin #18
+
