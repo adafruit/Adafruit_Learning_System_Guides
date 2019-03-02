29b903680cf32e9544ce4f287faf551b8021e6df
Mikey Sklar <mikesklar@gmail.com>
2019-03-01 18:28:49 -0700

    linted and runs smoothly

--- Basic_Resistor_Sensor_Reading_on_Raspberry_Pi/Basic_Resistor_Sensor_Reading_on_Raspberry_Pi.py
+++ Basic_Resistor_Sensor_Reading_on_Raspberry_Pi/Basic_Resistor_Sensor_Reading_on_Raspberry_Pi.py
@@ -1,30 +1,29 @@
 # Example for RC timing reading for Raspberry Pi
 # using CircuitPython Libraries
 
-import os
 import time
 import board
 from digitalio import DigitalInOut, Direction
 
 def RCtime (RCpin):
-        reading = 0
-        
-        # setup pin as output and direction low value
-        rc = DigitalInOut(RCpin)
-        rc.direction = Direction.OUTPUT
-        rc.value = False
+    reading = 0
 
-        time.sleep(0.1)
+    # setup pin as output and direction low value
+    rc = DigitalInOut(RCpin)
+    rc.direction = Direction.OUTPUT
+    rc.value = False
 
-        # setup pin as input and wait for low value
-        rc = DigitalInOut(RCpin)
-        rc.direction = Direction.INPUT
+    time.sleep(0.1)
 
-        # This takes about 1 millisecond per loop cycle
-        while ( rc.value == False ):
-                reading += 1
-        return reading
+    # setup pin as input and wait for low value
+    rc = DigitalInOut(RCpin)
+    rc.direction = Direction.INPUT
 
-while True:                                     
-        result = RCtime(board.D18)     # Read RC timing using pin #18
-        print(result)
+    # This takes about 1 millisecond per loop cycle
+    while rc.value is False:
+        reading += 1
+    return reading
+
+while True:
+    result = RCtime(board.D18)     # Read RC timing using pin #18
+    print(result)
