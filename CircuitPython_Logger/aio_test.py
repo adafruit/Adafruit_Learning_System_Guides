8a48c9d89756acf3012e2f055757d3f770938f91
Dave Astels <dastels@daveastels.com>
2019-03-13 23:29:49 -0400

    Add AdafruitIO handler

---
+++ CircuitPython_Logger/aio_test.py
@@ -0,0 +1,21 @@
+import time
+import random
+from aio_handler import AIOHandler
+import adafruit_logging as logging
+
+l = logging.getLogger('aio')
+l.addHandler(AIOHandler('test'))
+
+while True:
+    t = random.randint(1, 5)
+    if t == 1:
+        l.debug("debug message: %d", random.randint(0, 1000))
+    elif t == 2:
+        l.info("debug message: %d", random.randint(0, 1000))
+    elif t == 3:
+        l.warning("warning message: %d", random.randint(0, 1000))
+    elif t == 4:
+        l.error("error message: %d", random.randint(0, 1000))
+    elif t == 5:
+        l.critical("critical message: %d", random.randint(0, 1000))
+    time.sleep(5.0 + (random.random() * 5.0))
