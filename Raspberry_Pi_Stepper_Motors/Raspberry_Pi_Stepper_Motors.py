024abd8a9c98b0ee103e361e378c9442c97e96e1
Mikey Sklar <mikesklar@gmail.com>
2019-03-22 13:08:16 -0600

    linted clean, tested 293D and 2803A runs smooth, python3 compatible

--- Raspberry_Pi_Stepper_Motors/Raspberry_Pi_Stepper_Motors.py
+++ Raspberry_Pi_Stepper_Motors/Raspberry_Pi_Stepper_Motors.py
@@ -1,6 +1,5 @@
+import time
 import board
-import time 
-from adafruit_motor import stepper 
 import digitalio
 
 enable_pin = digitalio.DigitalInOut(board.D18)
@@ -15,37 +14,43 @@ coil_A_2_pin.direction = digitalio.Direction.OUTPUT
 coil_B_1_pin.direction = digitalio.Direction.OUTPUT
 coil_B_2_pin.direction = digitalio.Direction.OUTPUT
 
-def forward(delay, steps):  
-  for i in range(0, steps):
-    setStep(1, 0, 1, 0)
-    time.sleep(delay)
-    setStep(0, 1, 1, 0)
-    time.sleep(delay)
-    setStep(0, 1, 0, 1)
-    time.sleep(delay)
-    setStep(1, 0, 0, 1)
-    time.sleep(delay)
+enable_pin.value = True
 
-def backwards(delay, steps):  
-  for i in range(0, steps):
-    setStep(1, 0, 0, 1)
-    time.sleep(delay)
-    setStep(0, 1, 0, 1)
-    time.sleep(delay)
-    setStep(0, 1, 1, 0)
-    time.sleep(delay)
-    setStep(1, 0, 1, 0)
-    time.sleep(delay)
+def forward(delay, steps):
+    i = 0
+    while i in range(0, steps):
+        setStep(1, 0, 1, 0)
+        time.sleep(delay)
+        setStep(0, 1, 1, 0)
+        time.sleep(delay)
+        setStep(0, 1, 0, 1)
+        time.sleep(delay)
+        setStep(1, 0, 0, 1)
+        time.sleep(delay)
+        i += 1
+
+def backwards(delay, steps):
+    i = 0
+    while i in range(0, steps):
+        setStep(1, 0, 0, 1)
+        time.sleep(delay)
+        setStep(0, 1, 0, 1)
+        time.sleep(delay)
+        setStep(0, 1, 1, 0)
+        time.sleep(delay)
+        setStep(1, 0, 1, 0)
+        time.sleep(delay)
+        i += 1
 
 def setStep(w1, w2, w3, w4):
-  coil_A_1_pin.value = w1
-  coil_A_2_pin.value = w2
-  coil_B_1_pin.value = w3
-  coil_B_2_pin.value = w4
+    coil_A_1_pin.value = w1
+    coil_A_2_pin.value = w2
+    coil_B_1_pin.value = w3
+    coil_B_2_pin.value = w4
 
 while True:
-  delay = input("Delay between steps (milliseconds)?")
-  steps = input("How many steps forward? ")
-  forward(int(delay) / 1000.0, int(steps))
-  steps = input("How many steps backwards? ")
-  backwards(int(delay) / 1000.0, int(steps))
+    user_delay = input("Delay between steps (milliseconds)?")
+    user_steps = input("How many steps forward? ")
+    forward(int(user_delay) / 1000.0, int(user_steps))
+    user_steps = input("How many steps backwards? ")
+    backwards(int(user_delay) / 1000.0, int(user_steps))
