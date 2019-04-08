eb350f9620297a3d3f9ea1e6acfea0145d847488
Noe Ruiz <noe@adafruit.com>
2019-04-08 11:38:37 -0400

    Kattni credit to keyblade code
    Adding credit to Kattni in keyblade code

--- PropMaker_KeyBlade/code.py
+++ PropMaker_KeyBlade/code.py
@@ -1,3 +1,14 @@
+"""
+PyPortal based alarm clock.
+Adafruit invests time and resources providing this open source code.
+Please support Adafruit and open source hardware by purchasing
+products from Adafruit!
+Written by Dave Astels for Adafruit Industries
+Copyright (c) 2019 Adafruit Industries
+Licensed under the MIT license.
+All text above must be included in any redistribution.
+"""
+
 import time
 import digitalio
 import audioio
