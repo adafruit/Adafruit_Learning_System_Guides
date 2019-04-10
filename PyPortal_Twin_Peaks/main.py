f8a6dabaebae1bed0d520c322467c396bd723ad3
Mike Barela <mydigitalhome@gmail.com>
2019-04-10 14:48:01 -0400

    remove spaces and unused import

--- PyPortal_Twin_Peaks/main.py
+++ PyPortal_Twin_Peaks/main.py
@@ -3,7 +3,6 @@
 # Tutorial: https://learn.adafruit.com/twin-peaks-light-reactive-pyportal-picture-frame
 import time
 import board
-import displayio
 from analogio import AnalogIn
 from adafruit_pyportal import PyPortal
 
@@ -18,16 +17,16 @@ woodsman = (cwd+"/woodsman.bmp")
 gottaLight = (cwd+"/gottaLight.wav")
 
 pyportal = PyPortal(default_bg=laura)
- 
+
 def getVoltage(pin):  # helper
     return (pin.value * 3.3) / 65536
 
 while True:
-        
-        if getVoltage(analogin) > 0.175:
-            pyportal.set_background(laura)
-            time.sleep(1)
-        else:
-            pyportal.set_background(woodsman)
-            pyportal.play_file(gottaLight)
-            time.sleep(1)
+
+    if getVoltage(analogin) > 0.175:
+        pyportal.set_background(laura)
+        time.sleep(1)
+    else:
+        pyportal.set_background(woodsman)
+        pyportal.play_file(gottaLight)
+        time.sleep(1)
