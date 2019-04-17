43f120b88a012a68ea0ab28b1ed5107173af3cbe
Mikey Sklar <mikesklar@gmail.com>
2019-04-17 17:40:24 -0600

    Single_Output.py, runs clean, linted, CP and python3 ready

--- Capacitive_Touch_Sensors_on_the_Raspberry_Pi/Single_Output.py
+++ Capacitive_Touch_Sensors_on_the_Raspberry_Pi/Single_Output.py
@@ -1,18 +1,18 @@
 import time
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
+import board
+from digitalio import DigitalInOut, Direction
 
-padPin = 23
-GPIO.setup(padPin, GPIO.IN)
+pad_pin = board.D23
 
+pad = DigitalInOut(pad_pin)
+pad.direction = Direction.INPUT
 
-alreadyPressed = False
+already_pressed = False
 
 while True:
-    padPressed =  GPIO.input(padPin)
 
-    if padPressed and not alreadyPressed:
-        print "pressed"
-    
-    alreadyPressed = padPressed
+    if pad.value and not already_pressed:
+        print("pressed")
+
+    already_pressed = pad.value
     time.sleep(0.1)
