10e65551434267ef176a5a0b2e24f680ee7cd717
Noe Ruiz <noe@adafruit.com>
2019-04-23 19:47:22 -0400

    CPX soundbox
    Adding code for CPX soundbox

---
+++ CPX_Sound_Box/code.py
@@ -0,0 +1,50 @@
+import board
+from digitalio import DigitalInOut, Direction, Pull
+import audioio
+import neopixel
+import time
+
+filename = "electrons.wav"
+
+# What pad our button is connected to:
+button = DigitalInOut(board.A4)
+button.direction = Direction.INPUT
+button.pull = Pull.UP
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=1)
+
+# NeoPixel Animation
+def simpleCircle(wait):
+    PURPLE = (255, 0, 255)
+    BLACK = (0, 0, 0)
+    CYAN = (0, 255, 255)
+    ORANGE = (255, 255, 0)
+
+    for i in range(len(pixels)):
+        pixels[i] = PURPLE
+        time.sleep(wait)
+    for i in range(len(pixels)):
+        pixels[i] = CYAN
+        time.sleep(wait)
+    for i in range(len(pixels)):
+        pixels[i] = ORANGE
+        time.sleep(wait)
+    for i in range(len(pixels)):
+        pixels[i] = BLACK
+        time.sleep(wait)
+
+# Audio Stuff
+def play_file(filename):
+    print("Playing File" + filename)
+    wave_file = open(filename, "rb")
+    with audioio.WaveFile(wave_file) as wave:
+        with audioio.AudioOut(board.A0) as audio:
+            audio.play(wave)
+            while audio.playing:
+                pass
+                simpleCircle(.02)
+    print("finished")
+
+while True:
+    if not button.value:
+        play_file(filename)
