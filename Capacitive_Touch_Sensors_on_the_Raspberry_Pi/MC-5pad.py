22943c66c49091d8cfa72991d9948d500e342b43
Mikey Sklar <mikesklar@gmail.com>
2019-04-23 19:30:25 -0600

    MC-5pad.py ported to CP, python3 tested and linted

--- Capacitive_Touch_Sensors_on_the_Raspberry_Pi/MC-5pad.py
+++ Capacitive_Touch_Sensors_on_the_Raspberry_Pi/MC-5pad.py
@@ -1,48 +1,54 @@
-import RPi.GPIO as GPIO
 import time
+import board
 import mcpi.minecraft as minecraft
+from digitalio import DigitalInOut, Direction
 mc = minecraft.Minecraft.create()
 
-GPIO.setmode(GPIO.BCM)
+# set the GPIO input pins
+pad0_pin = board.D22
+pad1_pin = board.D21
+pad2_pin = board.D17
+pad3_pin = board.D24
+pad4_pin = board.D23
 
-#set the GPIO input pins
-pad0 = 22
-pad1 = 27
-pad2 = 17
-pad3 = 24
-pad4 = 23
+pad0 = DigitalInOut(pad0_pin)
+pad1 = DigitalInOut(pad1_pin)
+pad2 = DigitalInOut(pad2_pin)
+pad3 = DigitalInOut(pad3_pin)
+pad4 = DigitalInOut(pad4_pin)
 
-GPIO.setup(pad0, GPIO.IN)
-GPIO.setup(pad1, GPIO.IN)
-GPIO.setup(pad2, GPIO.IN)
-GPIO.setup(pad3, GPIO.IN)
-GPIO.setup(pad4, GPIO.IN)
+pad0.direction = Direction.INPUT
+pad1.direction = Direction.INPUT
+pad2.direction = Direction.INPUT
+pad3.direction = Direction.INPUT
+pad4.direction = Direction.INPUT
 
-pad0alreadyPressed = False
-pad3alreadyPressed = False
-pad4alreadyPressed = False
+pad0_already_pressed = False
+pad3_already_pressed = False
+pad4_already_pressed = False
 
 immutable = False
+
 tnt = 46
 water = 9
 flowers = 38
 
 while True:
-    pad0pressed = not GPIO.input(pad0)
-    pad1pressed = not GPIO.input(pad1)
-    pad2pressed = not GPIO.input(pad2)
-    pad3pressed = not GPIO.input(pad3)
-    pad4pressed = not GPIO.input(pad4)
+    pad0_pressed = not pad0.value
+    pad1_pressed = not pad1.value
+    pad2_pressed = not pad2.value
+    pad3_pressed = not pad3.value
+    pad4_pressed = not pad4.value
 
-    if pad0pressed and not pad0alreadyPressed:
+    if pad0_pressed and not pad0_already_pressed:
         #teleport
         x = 0
         y = 0
         z = 0
         mc.player.setPos(x, y, z)
-    pad0alreadyPressed = pad0pressed
+    pad0_already_pressed = pad0_pressed
 
-    if pad1pressed:
+    if pad1_pressed:
         #Flowers
         pos = mc.player.getPos()
         x = pos.x
@@ -50,7 +56,7 @@ while True:
         z = pos.z
         mc.setBlock(x, y, z, flowers)
 
-    if pad2pressed:
+    if pad2_pressed:
         #TNT
         pos = mc.player.getPos()
         x = pos.x
@@ -58,19 +64,19 @@ while True:
         z = pos.z
         mc.setBlock(x, y, z, tnt, 1)
 
-    if pad3pressed and not pad3alreadyPressed:
+    if pad3_pressed and not pad3_already_pressed:
         #Chat message: Are in water?
         pos = mc.player.getPos()
         block = mc.getBlock(pos.x, pos.y, pos.z)
         inWater = block == water
         mc.postToChat("In water: " + str(inWater))
-    pad3alreadyPressed = pad3pressed
+    pad3_already_pressed = pad3_pressed
 
-    if pad4pressed and not pad4alreadyPressed:
+    if pad4_pressed and not pad4_already_pressed:
         #Immutable
         immutable = not immutable
         mc.setting("world_immutable", immutable)
         mc.postToChat("Immutable: " + str(immutable))
-    pad4alreadyPressed = pad4pressed
+    pad4_already_pressed = pad4_pressed
 
     time.sleep(0.1)
