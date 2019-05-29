4200479ec764d12417897dab80c18798ad7fe1d3
Isaac Wellish <isaac.wellish@gmail.com>
2019-05-29 16:02:49 -0400

    pylint changes

--- PyPortal_Pocket_Museum/pocket_museum.py
+++ PyPortal_Pocket_Museum/pocket_museum.py
@@ -8,7 +8,6 @@ from adafruit_pyportal import PyPortal
 # There's a few different places we look for data in the photo of the day
 IMAGE_LOCATION = ["primaryImage"]
 TITLE_LOCATION = ["title"]
-ARTIST_LOCATION = ["artistDisplayName"]
 
 # the current working directory (where this file is)
 cwd = ("/"+__file__).rsplit('/', 1)[0]
@@ -16,9 +15,9 @@ pyportal = PyPortal(json_path=(IMAGE_LOCATION, TITLE_LOCATION),
                     status_neopixel=board.NEOPIXEL,
                     default_bg=cwd+"/pocket_museum_background.bmp",
                     text_font=cwd+"/fonts/Arial-12.bdf",
-                    text_position=((320, 240), (5, 220)),
+                    text_position=((320, 240), (5, 220)), # need better way of hiding link text
                     text_color=(0xFFFFFF, 0xFFFFFF),
-                    text_maxlen=(1, 50), # cut off characters
+                    text_maxlen=(1, 50), # need better way of hiding link text
                     image_resize=(320, 240),
                     image_position=(0, 0),
                     debug = True)
@@ -29,22 +28,23 @@ pyportal.preload_font() # speed things up by preloading font
 while True:
 
     print("Searching for new work!")
-
     #random work is selected
     RANDOM_ART = str(randint(1, 470000))
 
+    # error handling
     try:
         try:
             # set image json path to change pictures when screen touched
-            pyportal._url=("https://collectionapi.metmuseum.org/public/collection/v1/objects/"+RANDOM_ART)
+            pyportal._url=\
+            ("https://collectionapi.metmuseum.org/public/collection/v1/objects/"+RANDOM_ART)
             pyportal._image_json_path = (IMAGE_LOCATION)
             value = pyportal.fetch()
-            print("The image is here: ", value[0])
-
+            # print("The image is here: ", value[0])
+            # if primary image does not exist, try another work
             if value[0] == '':
                 continue
                 print("No image found")
-
+        # if Key error (conent-length) occurs, try another work
         except KeyError:
             print('Key Error')
             continue
@@ -53,6 +53,9 @@ while True:
         print("Some error occured, retrying! -", e)
         continue
 
-    # if they touch the screen, fetch immediately!
-    if pyportal.touchscreen.touch_point:
-        continue
+    stamp = time.monotonic()
+    # wait 5 minutes before getting again
+    while (time.monotonic() - stamp) < (5*60):
+        # or, if they touch the screen, fetch immediately!
+        if pyportal.touchscreen.touch_point:
+            break
