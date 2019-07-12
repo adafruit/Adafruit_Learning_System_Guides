fe7b4925bdfdfa9df03c2db881130acc63cd2ae6
Isaac Wellish <isaac.wellish@gmail.com>
2019-07-12 18:47:47 -0400

    displayio formatting

--- PyPortal_Trivia_Time/code.py
+++ PyPortal_Trivia_Time/code.py
@@ -11,6 +11,10 @@ import random
 import board
 from adafruit_pyportal import PyPortal
 from digitalio import DigitalInOut, Direction, Pull
+from adafruit_display_text import label
+from adafruit_bitmap_font import bitmap_font
+import adafruit_imageload
+import displayio
 
 # initialize harware
 led = DigitalInOut(board.L) # For debugging
@@ -21,6 +25,7 @@ button1.direction = Direction.INPUT
 button2.direction = Direction.INPUT
 button1.pull = Pull.UP
 button2.pull = Pull.UP
+display = board.DISPLAY
 
 # Set up where we'll be fetching data from
 DATA_SOURCE = "https://opentdb.com/api.php?amount=1&type=multiple"
@@ -31,6 +36,11 @@ WA_LOCATION3 = ['results', 0, 'incorrect_answers', 2]
 CA_LOCATION = ['results', 0, 'correct_answer']
 CAPTION = 'opentdb.com'
 answerChoices = ("A","B","C","D")
+qf_text = ''
+a1f_text = ''
+a2f_text = ''
+a3f_text = ''
+a4f_text = ''
 
 # determine the current working directory
 # needed so we know where to find files
@@ -74,48 +84,77 @@ def unescape(s):
     s = s.replace("&amp;", "&")
     return s
 
-# Initialize the pyportal object and let us know what data to fetch and where
-# to display it
+def unescapeList(l):
+    for i in range(len(l)):
+        l[i] = l[i].replace("&quot;", "\"")
+        l[i] = l[i].replace("&#039;", "\'")
+        l[i] = l[i].replace("&amp;", "&")
+    return l
+
+def bmpImageLoad():
+    with open(cwd+"/trivia.bmp", "rb") as bitmap_file:
+
+        # Setup the file as the bitmap data source
+        bitmap = displayio.OnDiskBitmap(bitmap_file)
+
+        # Create a TileGrid to hold the bitmap
+        tile_grid = displayio.TileGrid(bitmap, pixel_shader=displayio.ColorConverter())
+
+        # Create a Group to hold the TileGrid
+        group = displayio.Group()
+
+        # Add the TileGrid to the Group
+        group.append(tile_grid)
+
+        # Add the Group to the Display
+        display.show(group)
+
+    # Loop forever so you can enjoy your image
+    while True:
+        pass
 pyportal = PyPortal(url=DATA_SOURCE,
                     status_neopixel=board.NEOPIXEL,
                     default_bg=cwd+"/trivia_title.bmp",
-                    text_font=cwd+"/fonts/Arial-ItalicMT-17.bdf",
-                    text_position=((25, 70),  # question location
-                                   (35, 135), # answer location 1
-                                   (35, 155), # answer location 2
-                                   (35, 175), # answer location 3
-                                   (35, 195)), # answer location 4
-                    text_color=(0x8080FF,
-                                0xFFFFFF,
-                                0xFFFFFF,
-                                0xFFFFFF,
-                                0xFFFFFF),
-                    text_wrap=(35, # characters to wrap for text
-                               0,
-                               0,
-                               0,
-                               0),
-                    text_maxlen=(140, 30, 30, 30, 30), # max text size
                     caption_font=cwd+"/fonts/Arial-ItalicMT-17.bdf")
+
+
 print("loading...") # print to repl while waiting for font to load
 pyportal.preload_font() # speed things up by preloading font
+trivia_font = bitmap_font.load_font("/fonts/Arial-ItalicMT-17.bdf")
+# 320 x 240
+loading_position = (100,100)
+q_position = (25, 70)
+a1_position = (25, 135)
+a2_position = (25, 155)
+a3_position = (25, 175)
+a4_position = (25, 195)
+loading_color = 0x8080FF
+q_color = 0x8080FF
+a_color = 0xFFFFFF
+
 
 while True:
     # Load new question when screen is touched
     if pyportal.touchscreen.touch_point:
         pyportal.set_background(cwd+"/trivia.bmp")
-        pyportal.set_text("Loading question...")
+        #pyportal.set_text("Loading question...")
+        loading_text = "loading question..."
+        loading_text_area = label.Label(trivia_font, text=loading_text, color=loading_color)
+        loading_text_area.x = loading_position[0]
+        loading_text_area.y = loading_position[1]
+        pyportal.splash.append(loading_text_area)
+
         pyportal.set_caption(CAPTION,(200, 218), 0x808080)
         answerList = [WA_LOCATION1, WA_LOCATION2, WA_LOCATION3, CA_LOCATION]
         # For catching potential index error when shuffling question
         try:
-            shuffle(answerList)
+            shuffle(answerList) # Shuffle answers
         except IndexError:
             print("Index Error")
-            pyportal.set_text("Tap again for next question.")
+            #pyportal.set_text("Tap again for next question.")
             continue
         try:
-            # set the JSON path here
+            # set the JSON path here, now that answers are shuffled
             # pylint: disable=protected-access
             pyportal._json_path=(Q_LOCATION,
                                  answerList[0],
@@ -124,6 +163,53 @@ while True:
                                  answerList[3],)
             value = pyportal.fetch()
             print("Response is", value)
+
+            #pyportal.set_text("Loading question...")
+
+            # "clean" results - still don't know how to put in constructor
+            valueClean = unescapeList(value)
+            print("Response is", value)
+
+            # Formatting with displayio
+            q_text = unescape(value[0])
+            q_text_formatted = pyportal.wrap_nicely(q_text, 35)
+            for i in range (len(q_text_formatted)):
+                qf_text = qf_text + q_text_formatted[i] +"\n"
+            q_text = qf_text
+            q_text_area = label.Label(trivia_font, text=q_text, color=q_color, line_spacing = 1)
+            q_text_area.x = q_position[0]
+            q_text_area.y = q_position[1]
+            a1_text = unescape(value[1])
+            a1_text_area = label.Label(trivia_font, text="A. "+a1_text, color=a_color, line_spacing = 1.5)
+            a1_text_area.x = a1_position[0]
+            a1_text_area.y = a1_position[1]
+            a2_text = unescape(value[2])
+            a2_text_area = label.Label(trivia_font, text="B. "+a2_text, color=a_color, line_spacing = 1.5)
+            a2_text_area.x = a2_position[0]
+            a2_text_area.y = a2_position[1]
+            a3_text = unescape(value[3])
+            a3_text_area = label.Label(trivia_font, text="C. "+a3_text, color=a_color, line_spacing = 1.5)
+            a3_text_area.x = a3_position[0]
+            a3_text_area.y = a3_position[1]
+            a4_text = unescape(value[4])
+            a4_text_area = label.Label(trivia_font, text="D. "+a4_text, color=a_color, line_spacing = 1.5)
+            a4_text_area.x = a4_position[0]
+            a4_text_area.y = a4_position[1]
+
+            pyportal.splash.pop() # take off loading...
+            pyportal.splash.pop() # take off loading...
+            pyportal.splash.append(q_text_area)
+            pyportal.splash.append(a1_text_area)
+            pyportal.splash.append(a2_text_area)
+            pyportal.splash.append(a3_text_area)
+            pyportal.splash.append(a4_text_area)
+
+            #display.show(q_text_area)
+
+
+
+
+
         except RuntimeError as e:
             print("Some error occured, retrying! -", e)
         # 30 seconds with question
@@ -138,4 +224,4 @@ while True:
                 correctAnswerChoice = answerChoices[k]
                 break
         answerRevealText="Correct Answer: "+str(correctAnswerChoice)+"\n(Tap for next question.)"
-        pyportal.set_text(answerRevealText)
+        #pyportal.set_text(answerRevealText)
