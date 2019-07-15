7adc2147495c65550a9072718f324fb30615f341
Dave Astels <dastels@daveastels.com>
2019-07-15 14:52:41 -0400

    Use literal bytestring

--- Twitter_API/binascii.py
+++ Twitter_API/binascii.py
@@ -4,26 +4,9 @@ Copied and tweaked from micropython-lib
 
 PAD = '='
 
-#pylint:disable=bad-whitespace,invalid-name,bad-continuation
-table_a2b_base64 = bytes([
-    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-    255,255,255,255, 255,255,255,255, 255,255,255, 62, 255,255,255, 63,
-     52, 53, 54, 55,  56, 57, 58, 59,  60, 61,255,255, 255,255,255,255,
-    255,  0,  1,  2,   3,  4,  5,  6,   7,  8,  9, 10,  11, 12, 13, 14,
-     15, 16, 17, 18,  19, 20, 21, 22,  23, 24, 25,255, 255,255,255,255,
-    255, 26, 27, 28,  29, 30, 31, 32,  33, 34, 35, 36,  37, 38, 39, 40,
-     41, 42, 43, 44,  45, 46, 47, 48,  49, 50, 51,255, 255,255,255,255,
-    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-    255,255,255,255, 255,255,255,255, 255,255,255,255, 255,255,255,255,
-])
-#pylint:enable=bad-whitespace,bad-continuation
+#pylint:disable=invalid-name,line-too-long
+table_a2b_base64 = b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff>\xff\xff\xff?456789:;<=\xff\xff\xff\xff\xff\xff\xff\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\xff\xff\xff\xff\xff\xff\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./0123\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+#pylint:enable=line-too-long
 
 assert len(table_a2b_base64) == 256
 
