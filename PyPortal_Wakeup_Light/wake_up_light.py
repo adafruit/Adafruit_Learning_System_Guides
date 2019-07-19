54c22ff5837decfd6e5a27236ce11929cb5c2749
Isaac Wellish <isaac.wellish@gmail.com>
2019-07-19 17:09:22 -0400

    pyportal time revisions

--- PyPortal_Wakeup_Light/wake_up_light.py
+++ PyPortal_Wakeup_Light/wake_up_light.py
@@ -3,101 +3,81 @@ This example uses a PyPortal and rgbw leds for a simple "wake up" light.
 The strip starts to brighten 30 minutes before set wake up time.
 This program assumes a neopixel strip is attached to D4 on the Adafruit PyPortal.
 """
-
 import time
 import board
-import neopixel
-import busio
-from digitalio import DigitalInOut
-from analogio import AnalogIn
-import displayio
-from adafruit_display_text.label import Label
+from adafruit_pyportal import PyPortal
 from adafruit_bitmap_font import bitmap_font
-from adafruit_esp32spi import adafruit_esp32spi, adafruit_esp32spi_wifimanager
-from adafruit_io.adafruit_io import RESTClient, AdafruitIO_RequestError
+from adafruit_display_text.Label import Label
 
-cwd = ("/"+__file__).rsplit('/', 1)[0] # the current working directory (where this file is)
+# Set to True for '12 hour + AM/PM' time, set to false for 24 hour time
+AM_PM = True
 
-# rate at which to refresh the pyportal screen, in seconds
-PYPORTAL_REFRESH = 15
+# determine the current working directory
+# needed so we know where to find files
+cwd = ("/"+__file__).rsplit('/', 1)[0]
+# Initialize the pyportal object and let us know what data to fetch and where
+# to display it
 
-# Get wifi details and more from a secrets.py file
-try:
-    from secrets import secrets
-except ImportError:
-    print("WiFi secrets are kept in secrets.py, please add them there!")
-    raise
+pyportal = PyPortal(status_neopixel=board.NEOPIXEL,
+                    default_bg=0x000000)
 
-# PyPortal ESP32 Setup
-esp32_cs = DigitalInOut(board.ESP_CS)
-esp32_ready = DigitalInOut(board.ESP_BUSY)
-esp32_reset = DigitalInOut(board.ESP_RESET)
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
-status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
-wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
-
-# Set your Adafruit IO Username and Key in secrets.py
-# (visit io.adafruit.com if you need to create an account,
-# or if you need your Adafruit IO key.)
-try:
-    ADAFRUIT_IO_USER = secrets['aio_username']
-    ADAFRUIT_IO_KEY = secrets['aio_key']
-except KeyError:
-    raise KeyError('To use this code, you need to include your Adafruit IO username \
-and password in a secrets.py file on the CIRCUITPY drive.')
-
-# Fonts within /fonts folder
-info_font = cwd+"/fonts/Nunito-Black-17.bdf"
-time_font = cwd+"/fonts/Nunito-Light-75.bdf"
+big_font = bitmap_font.load_font(cwd+"/fonts/Nunito-Light-75.bdf")
+big_font.load_glyphs(b'0123456789:AP') # pre-load glyphs for fast printing
+print('loading fonts...')
+info_font = bitmap_font.load_font(cwd+"/fonts/Nunito-Black-17.bdf")
+info_font.load_glyphs(b'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-,.:/ ')
 
 # Type in time to get up
 input_wake_up_time = "6:30"
 input_wake_up_time_text = "Wake up at: " + input_wake_up_time + "am"
 #light_on_time_text = "Light starting at: " # - 30 minutes? how?
 
-# create text object group
-text_group = displayio.Group(max_size=6)
-text_group.append(text_group)
+time_color = 0xFFFFFF
+time_position = (0,130)
+time_textarea = Label(big_font, max_glyphs=15, color=time_color,
+                                x=time_position[0], y=time_position[1])
 
-print('loading fonts...')
-glyphs = b'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-,.:/ '
+wakeup_time_color = 0xFFFFFF
+wakeup_time_position = (15,200)
+wakeup_time_textarea = Label(info_font, max_glyphs=30, color=wakeup_time_color,
+                                x=wakeup_time_position[0], y=wakeup_time_position[1])
 
-# Font for time
-time_font = bitmap_font.load_font(time_font)
-time_font.load_glyphs(glyphs)
+pyportal.splash.append(time_textarea)
+wakeup_time_textarea.text = input_wake_up_time_text
+pyportal.splash.append(wakeup_time_textarea)
 
-# Font for other info
-info_font = bitmap_font.load_font(info_font)
-info_font.load_glyphs(glyphs)
-
-# Time
-time_text = Label(time_font, max_glyphs=40)
-time_text.x = 65
-time_text.y = 120
-text_group.append(time_text)
-
-# Wake up time
-wakeup_time_text = Label(info_font, text= input_wake_up_time_text)
-wakeup_time_text.x = 15
-wakeup_time_text.y = 200
-text_group.append(wakeup_time_text)
-
-board.DISPLAY.show(time_text)
-# board.DISPLAY.show(text_group) # when running this line, the pyportal crashes
-
-# Create an instance of the Adafruit IO REST client
-io = RESTClient(ADAFRUIT_IO_USER, ADAFRUIT_IO_KEY, wifi)
+refresh_time = None
 
 while True:
-    try: # WiFi Connection
-        # Get and display date and time from Adafruit IO
-        print('Getting time from Adafruit IO...')
-        datetime = io.receive_time()
-        print('displaying time...')
-        time_text.text = '%02d:%02d'%(datetime[3],datetime[4])
-    except (ValueError, RuntimeError) as e: # WiFi Connection Failure
-        print("Failed to get data, retrying\n", e)
-        wifi.reset()
-        continue
-    time.sleep(PYPORTAL_REFRESH)
+    # only query the online time once per hour (and on first run)
+    if (not refresh_time) or (time.monotonic() - refresh_time) > 3600:
+        try:
+            print("Getting time from internet!")
+            pyportal.get_local_time()
+            refresh_time = time.monotonic()
+        except RuntimeError as e:
+            print("Some error occured, retrying! -", e)
+            continue
+    print(time.monotonic())
+    now = time.localtime()
+    hour, minute = now[3:5]
+    print(now)
+    print("Current time: %02d:%02d" % (hour, minute))
+
+    # display the time in a nice big font
+    format_str = "%d:%02d"
+    if AM_PM:
+        if hour >= 12:
+            hour -= 12
+            format_str = format_str+"P"
+        else:
+            format_str = format_str+"A"
+        if hour == 0:
+            hour = 12
+    if hour < 10:
+        format_str = "   "+format_str
+    time_str = format_str % (hour, minute)
+    time_textarea.text = time_str
+
+    # update every 15 seconds
+    time.sleep(15)
