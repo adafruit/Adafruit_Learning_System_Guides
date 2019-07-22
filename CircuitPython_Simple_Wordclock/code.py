52f527ff8fcf545994b565400da633bc4161aafc
Elmwood Electronics <craig@elmwoodelectronics.ca>
2019-07-22 16:32:37 -0400

    Update code.py
    Noticed that midnight was not handled properly for cases like "Five to Midnight" or "Twenty after Midnight".  Was seeing noon instead of midnight.

--- CircuitPython_Simple_Wordclock/code.py
+++ CircuitPython_Simple_Wordclock/code.py
@@ -89,6 +89,9 @@ def writetime(the_hr, the_min):
         the_hr = the_hr + 1  # for the TO case
         value = value | TO
     # set hour
+    if the_hr == 24:   # special case for Midnight
+        value = value | MIDIGHT
+        return value
     if the_hr > 12:
         the_hr = the_hr - 12  # Convert 24 hour format to 12 hour
     if the_hr == 1:
@@ -113,8 +116,6 @@ def writetime(the_hr, the_min):
         value = value | TEN
     if the_hr == 11:
         value = value | ELEVEN
-    if the_hr == 0:
-        value = value | MIDNIGHT
     if the_hr == 12:
         value = value | NOON
     return value
