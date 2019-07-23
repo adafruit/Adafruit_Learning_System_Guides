dc5375c39aff20280fce8fb6d6459366bcaedf50
Isaac Wellish <isaac.wellish@gmail.com>
2019-07-23 19:08:46 -0400

    edit comment

--- PyPortal_Wakeup_Light/wake_up_light.py
+++ PyPortal_Wakeup_Light/wake_up_light.py
@@ -1,7 +1,7 @@
 """
 This example uses a PyPortal and rgbw leds for a simple "wake up" light.
 The strip starts to brighten 30 minutes before set wake up time.
-This program assumes a neopixel strip is attached to D4 on the Adafruit PyPortal.
+This program assumes a neopixel strip is attached to D3 on the Adafruit PyPortal.
 """
 import time
 import board
