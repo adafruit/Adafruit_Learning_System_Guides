c0499af4a64cb46ed2532df4a05cd5206d0748a7
Isaac Wellish <isaac.wellish@gmail.com>
2019-08-08 18:37:58 -0400

    record caps funciton added

--- PyRuler_Simon_Game/pyruler_simon.py
+++ PyRuler_Simon_Game/pyruler_simon.py
@@ -4,6 +4,11 @@ from digitalio import DigitalInOut, Direction
 import time
 import random
 import touchio
+import adafruit_dotstar
+
+#pixels = adafruit_dotstar.DotStar(APA102_MOSI, APA102_SCK, 1, brightness=0.1, auto_write=False)
+#pixels.fill(0,255,0)
+#pixels.show()
 
 led = DigitalInOut(board.D13)
 led.direction = Direction.OUTPUT
@@ -24,7 +29,7 @@ for led in leds:
 
 cap_touches = [False, False, False, False]
 
-def read_caps():
+def read_caps(timeout=30):
     t0_count = 0
     t0 = touches[0]
     t0.direction = Direction.OUTPUT
@@ -40,6 +45,14 @@ def read_caps():
     cap_touches[2] = touches[2].raw_value > 3000
     cap_touches[3] = touches[3].raw_value > 3000
     return cap_touches
+
+def record_caps():
+    caps = read_caps()
+    val = 0
+    for i,c in enumerate(caps):
+        if c:
+            val = i
+    return val
 
 def light_cap(cap, duration=0.5):
     # turn the LED for the selected cap on
@@ -57,12 +70,32 @@ def play_sequence(sequence):
     for cap in sequence:
         light_cap(cap, duration)
 
+def read_sequence(sequence):
+    for cap in sequence:
+        if record_caps() != cap:
+            # the player made a mistake!
+            return False
+        light_cap(cap, 0.5)
+    return True
+
+def play_error():
+    # make dot star red
+    time.sleep(1)
 
 def play_game():
     sequence = []
     while True:
+        time.sleep(1)
         sequence.append(random.randint(0, 3))
         play_sequence(sequence)
+        print("Next sequence unlocked!")
+
+        if not read_sequence(sequence):
+            # game over
+            play_error()
+            print("gameover")
+            break
+
         time.sleep(1)
 
 while True:
