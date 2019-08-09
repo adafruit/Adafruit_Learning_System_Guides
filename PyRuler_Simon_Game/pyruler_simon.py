b3663a32962253a4068929cf5f04438c0985dafb
Isaac Wellish <isaac.wellish@gmail.com>
2019-08-09 14:02:30 -0400

    add dotstar and led UI animations

--- PyRuler_Simon_Game/pyruler_simon.py
+++ PyRuler_Simon_Game/pyruler_simon.py
@@ -6,9 +6,14 @@ import random
 import touchio
 import adafruit_dotstar
 
-#pixels = adafruit_dotstar.DotStar(APA102_MOSI, APA102_SCK, 1, brightness=0.1, auto_write=False)
-#pixels.fill(0,255,0)
-#pixels.show()
+num_pixels = 1
+pixels = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, num_pixels, brightness=0.1, auto_write=False)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
+pixels.fill(blue)
+pixels.show()
 
 led = DigitalInOut(board.D13)
 led.direction = Direction.OUTPUT
@@ -21,13 +26,37 @@ leds = []
 for p in (board.LED4, board.LED5, board.LED6, board.LED7):
     led = DigitalInOut(p)
     led.direction = Direction.OUTPUT
-    led.value = True
-    time.sleep(0.25)
     leds.append(led)
-for led in leds:
-    led.value = False
 
 cap_touches = [False, False, False, False]
+
+def intro_game():
+    for p in range(len(leds)):
+        leds[p].value = True
+        time.sleep(0.25)
+    for led in range(len(leds)):
+        leds[led].value = False
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    if pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    pos -= 170
+    return (pos * 3, 0, 255 - pos * 3)
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            rc_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(rc_index & 255)
+        pixels.show()
+        time.sleep(wait)
 
 def read_caps(timeout=30):
     t0_count = 0
@@ -52,6 +81,7 @@ def record_caps():
     for i,c in enumerate(caps):
         if c:
             val = i
+            print("true!")
     return val
 
 def light_cap(cap, duration=0.5):
@@ -71,6 +101,9 @@ def play_sequence(sequence):
         light_cap(cap, duration)
 
 def read_sequence(sequence):
+    pixels.fill(green)
+    pixels.show()
+    time.sleep(1)
     for cap in sequence:
         if record_caps() != cap:
             # the player made a mistake!
@@ -80,22 +113,29 @@ def read_sequence(sequence):
 
 def play_error():
     # make dot star red
-    time.sleep(1)
+    pixels.fill(red)
+    pixels.show()
+    time.sleep(3)
 
 def play_game():
+    intro_game() # led light sequence at beginning of each game
     sequence = []
     while True:
+        pixels.fill(blue)
+        pixels.show()
         time.sleep(1)
         sequence.append(random.randint(0, 3))
         play_sequence(sequence)
-        print("Next sequence unlocked!")
-
         if not read_sequence(sequence):
             # game over
             play_error()
             print("gameover")
             break
-
+        else:
+            print("Next sequence unlocked!")
+            rainbow_cycle(0) # Dot star animation after each correct sequence
+        pixels.fill(0)
+        pixels.show()
         time.sleep(1)
 
 while True:
