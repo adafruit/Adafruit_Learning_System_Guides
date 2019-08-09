0f44780dece0e2c723ad7d87b80aa31e99b7b88e
Dave Astels <dastels@daveastels.com>
2019-08-09 17:49:49 -0400

    Start file

---
+++ PyPortal_UV_Index/code.py
@@ -0,0 +1,13 @@
+"""
+PyPortal UV Index display
+
+Adafruit invests time and resources providing this open source code.
+Please support Adafruit and open source hardware by purchasing
+products from Adafruit!
+
+Written by Dave Astels for Adafruit Industries
+Copyright (c) 2019 Adafruit Industries
+Licensed under the MIT license.
+
+All text above must be included in any redistribution.
+"""
