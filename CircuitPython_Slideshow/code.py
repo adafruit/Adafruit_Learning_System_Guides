b4f2c24f95d32a3435be7702cf8328edf674c9b8
Mike Barela <mydigitalhome@gmail.com>
2019-08-09 16:13:21 -0400

    Update code.py

--- CircuitPython_Slideshow/code.py
+++ CircuitPython_Slideshow/code.py
@@ -4,10 +4,10 @@ from adafruit_slideshow import PlayBackOrder, SlideShow
 
 # Create the slideshow object that plays through once alphabetically.
 slideshow = SlideShow(board.DISPLAY,
-	folder="/images",
-	loop=True,
-	order=PlayBackOrder.ALPHABETICAL,
-	dwell=60)
+                      folder="/images",
+                      loop=True,
+                      order=PlayBackOrder.ALPHABETICAL,
+                      dwell=60)
 
 while slideshow.update():
     pass
