e7df472eb9db03f06c8398d282a66db0929d2529
Mike Barela <mydigitalhome@gmail.com>
2019-08-09 15:51:32 -0400

    Update code.py

--- CircuitPython_Slideshow/code.py
+++ CircuitPython_Slideshow/code.py
@@ -1,14 +1,13 @@
 # CircuitPython Slideshow - uses the adafruit_slideshow.mpy library
 import board
 from adafruit_slideshow import PlayBackOrder, SlideShow
-import pulseio
 
 # Create the slideshow object that plays through once alphabetically.
 slideshow = SlideShow(board.DISPLAY,
-		      folder="/images",
-		      loop=True,
-		      order=PlayBackOrder.ALPHABETICAL,
-		      dwell=60)
+	folder="/images",
+	loop=True,
+	order=PlayBackOrder.ALPHABETICAL,
+	dwell=60)
 
 while slideshow.update():
     pass
