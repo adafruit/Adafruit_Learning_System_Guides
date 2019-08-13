110e6f8f13595bcb99c2689bd1cb9b5b78a69d50
Dave Astels <dastels@daveastels.com>
2019-08-12 22:37:27 -0400

    Get ready for publishing

--- PyPortal_UV_Index/code.py
+++ PyPortal_UV_Index/code.py
@@ -52,6 +52,16 @@ def halt_and_catch_fire(message, *args):
 
 url = 'https://enviro.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/{0}/JSON'.format(secrets['zip'])
 
+def extract_hour(date_time):
+    split_date_time = date_time.split()
+    hour = split_date_time[1]
+    suffix = split_date_time[2]
+    if hour[0] == '0':
+        hour = hour[1]
+    return '\n'.join([hour, suffix])
+
+def extract_date(date_time):
+    return ' '.join(date_time.split('/')[0:2])
 
 # Initialize the pyportal object and let us know what data to fetch and where
 # to display it
@@ -62,33 +72,43 @@ pyportal = PyPortal(url=url,
                     text_position=(20, 60),
                     text_color=0xFFFFFF,
                     text_wrap=35,
-                    caption_text='UV Index',
-                    caption_font=CAPTION_FONT_FILE,
-                    caption_position=(5, 20),
-                    caption_color=0x000000)
+                    caption_font=CAPTION_FONT_FILE)
 
-def extract_hour(date_time):
-    return '\n'.join(date_time.split()[1:3])
-
-data = [{'hour': extract_hour(d['DATE_TIME']), 'value': d['UV_VALUE']} for d in json.loads(pyportal.fetch()) if d['UV_VALUE'] > 0]
-
-number_of_readings = len(data)
-
-whitespace = (number_of_readings - 1) * SPACE_BETWEEN_BARS + 2 * MARGIN
-bar_width = (320 - whitespace) // number_of_readings
-
-max_reading = max([d['value'] for d in data])
-
-canvas = displayio.Group(max_size=24)
+canvas = displayio.Group(max_size=36)
 pyportal.splash.append(canvas)
 hour_font = bitmap_font.load_font(HOUR_FONT_FILE)
 
-for i, reading in enumerate(data):
-    bar_height = (MAX_BAR_HEIGHT * reading['value']) // max_reading
-    x = MARGIN + i * (bar_width + SPACE_BETWEEN_BARS)
-    canvas.append(Rect(x, 200 - bar_height, bar_width, bar_height, fill=COLORS[reading['value']]))
-    text = Label( hour_font, x=x, y=220, text=reading['hour'], color=0x000000, line_spacing=0.8)
-    canvas.append(text)
-
 while True:
-    pass
+    raw_data = json.loads(pyportal.fetch())
+    data = [{'hour': extract_hour(d['DATE_TIME']), 'value': int(d['UV_VALUE'])}
+                for d in raw_data
+                if d['UV_VALUE'] > 0]
+    the_day = raw_data[0]['DATE_TIME']
+    pyportal.set_caption('UV Index for {0}'.format(extract_date(the_day)),
+                         (80, 20),
+                         0x000000)
+    number_of_readings = len(data)
+    whitespace = (number_of_readings - 1) * SPACE_BETWEEN_BARS + 2 * MARGIN
+    bar_width = (320 - whitespace) // number_of_readings
+    max_reading = max([d['value'] for d in data])
+
+    while len(canvas) > 0:
+        canvas.pop()
+
+    for i, reading in enumerate(data):
+        bar_height = (MAX_BAR_HEIGHT * reading['value']) // max_reading
+        x = int(MARGIN + i * (bar_width + SPACE_BETWEEN_BARS))
+        canvas.append(Rect(x, 200 - bar_height,
+                           bar_width, bar_height,
+                           fill=COLORS[reading['value']]))
+        canvas.append(Label(hour_font,
+                            x=x+3, y=220,
+                            text=reading['hour'],
+                            color=0x000000,
+                            line_spacing=0.6))
+        canvas.append(Label(hour_font,
+                            x=x+(bar_width//2)-4, y=208-bar_height,
+                            text=str(reading['value']),
+                            color=0x000000))
+
+    time.sleep(3600)                  #refresh hourly
