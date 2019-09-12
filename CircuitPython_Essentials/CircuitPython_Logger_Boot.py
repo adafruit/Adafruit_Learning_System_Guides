fbb43d7959051590b4d458497abfe6ca30fa3d3f
Kattni Rembor <kattni@adafruit.com>
2019-09-12 13:11:12 -0400

    Adding Circuit Playground Bluefruit

--- CircuitPython_Essentials/CircuitPython_Logger_Boot.py
+++ CircuitPython_Essentials/CircuitPython_Logger_Boot.py
@@ -4,8 +4,13 @@ import storage
 
 # For Gemma M0, Trinket M0, Metro M0/M4 Express, ItsyBitsy M0/M4 Express
 switch = digitalio.DigitalInOut(board.D2)
-# switch = digitalio.DigitalInOut(board.D5)  # For Feather M0/M4 Express
-# switch = digitalio.DigitalInOut(board.D7)  # For Circuit Playground Express
+
+# For Feather M0/M4 Express
+# switch = digitalio.DigitalInOut(board.D5)
+
+# For Circuit Playground Express, Circuit Playground Bluefruit
+# switch = digitalio.DigitalInOut(board.D7)
+
 switch.direction = digitalio.Direction.INPUT
 switch.pull = digitalio.Pull.UP
 
