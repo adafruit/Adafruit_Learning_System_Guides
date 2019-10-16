e6f3f3b0a8cb1cb5dc00c8b7e14eb0f4c67aa281
Dan Cogliano <dan@zcontent.net>
2019-10-16 12:39:59 -0400

    Initial commit

---
+++ PyPortal_EZ_Make_Oven/codecalibrate.py
@@ -0,0 +1,73 @@
+import time
+import json
+import board
+import busio
+import array
+import math
+import audioio
+import displayio
+import digitalio
+import terminalio
+from adafruit_pyportal import PyPortal
+from adafruit_bitmap_font import bitmap_font
+from adafruit_display_text import label
+from adafruit_button import Button
+import adafruit_touchscreen
+from adafruit_bus_device.i2c_device import I2CDevice
+from adafruit_mcp9600 import MCP9600
+
+SENSOR_ADDR = 0X60
+
+i2c = busio.I2C(board.SCL, board.SDA,frequency=200000)
+sensor = MCP9600(i2c,SENSOR_ADDR,"K")
+
+oven = digitalio.DigitalInOut(board.D4)
+oven.direction = digitalio.Direction.OUTPUT
+
+def oven_control(enable=False):
+    #board.D4
+    oven.value = enable
+
+check_temp = 100
+print("This program will determine calibration settings for your oven ")
+print("to use with the EZ Make Oven Controller.\n\n")
+print("Calibration will start in 10 seconds...")
+time.sleep(10)
+print("Starting...")
+print("Calibrating oven temperature to ",check_temp)
+finish = False
+oven_control(True)
+maxloop=300
+counter = 0
+while not finish:
+    time.sleep(1)
+    counter += 1
+    current_temp = sensor.temperature
+    print(current_temp)
+    if current_temp >= check_temp:
+        finish = True
+        oven_control(False)
+    if counter >= maxloop:
+        raise Exception("Oven not working or bad sensor")
+
+print("checking oven lag time and temperature")
+finish = False
+start_time = time.monotonic()
+start_temp = sensor.temperature
+last_temp = start_temp
+
+while not finish:
+    time.sleep(1)
+    current_temp = sensor.temperature
+    print(current_temp)
+    if current_temp <= last_temp:
+        finish = True
+    last_temp = current_temp
+
+lag_temp = last_temp - check_temp
+lag_time = int(time.monotonic() - start_time)
+
+print("** Calibration Results **")
+print("Modify config.json with these values for your oven:")
+print("calibrate_temp:", lag_temp)
+print("calibrate_seconds:",lag_time)
