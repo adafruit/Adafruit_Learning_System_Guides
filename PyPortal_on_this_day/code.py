0d6394cdb6957cdb97c3262a5bb94cf4e19dca1c
Isaac Wellish <isaac.wellish@gmail.com>
2019-11-26 16:31:11 -0500

    add pics and update local data source

--- PyPortal_on_this_day/code.py
+++ PyPortal_on_this_day/code.py
@@ -1,39 +1,36 @@
 import random
 import board
-import adafruit_pyportal
+import time
+from adafruit_pyportal import PyPortal
 
 cwd = ("/"+__file__).rsplit('/', 1)[0] # the current working directory (where this file is)
 
 DAY = ["Day of the year"]
 PERSON = ["Person"]
 NOTABLE = ["Notable for"]
-YEAR = ["year"]
+YEAR = ["Year"]
 ACCOMPLISH = ["Accomplishment"]
 WEB = ["Web Reference"]
 
-DATA = Fake_Requests(cwd+"/jan1_history_json.py")
+DATA = cwd+"/local.txt"
 
 # create pyportal object w no data source (we'll feed it text later)
 pyportal = PyPortal(url = DATA,
                     json_path = (DAY, PERSON, NOTABLE, YEAR, ACCOMPLISH, WEB),
                     status_neopixel = board.NEOPIXEL,
-                    default_bg = None,
+                    default_bg = cwd + "/mary_jan1.bmp",
                     text_font = cwd+"fonts/Arial-ItalicMT-17.bdf",
-                    text_position=((5, 10), (5, 50), (5, 90),(5, 130), (5, 170), (5, 210)),
+                    text_position=((10, 70), (10, 100), (10, 130),(60, 160), (105, 190), (10, 220)),
                     text_color=(0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF),
                     text_maxlen=(50, 50, 50, 50, 50, 50), # cut off characters
                    )
 
-pyportal.set_text("loading ...") # display while user waits
-pyportal.preload_font() # speed things up by preloading font
-pyportal.set_text("What happened today in history?") # show title
-
 while True:
-    if pyportal.touchscreen.touch_point:
-        while True:
-            response = None
-            try:
-                response = pyportal.fetch()
-                print("Response is", response)
-            except RuntimeError as e:
-                print("Some error occured, retrying! -", e)
+    response = None
+    try:
+        response = pyportal.fetch()
+        print("Response is", response)
+    except RuntimeError as e:
+        print("Some error occured, retrying! -", e)
+
+    time.sleep(60)
