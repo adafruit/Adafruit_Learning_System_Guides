359bf75e4fffd70cc98d819e0b33955183d69a30
brentru <robots199@me.com>
2020-01-02 17:01:39 -0500

    remove old code, add code with buttons, still needs to dyn. generate buttons

--- PyPortal_TOTP_Friend/code.py
+++ PyPortal_TOTP_Friend/code.py
@@ -11,12 +11,16 @@ import neopixel
 import terminalio
 from adafruit_binascii import hexlify, unhexlify
 from adafruit_bitmap_font import bitmap_font
+from adafruit_button import Button
 from adafruit_display_shapes.circle import Circle
 from adafruit_display_shapes.roundrect import RoundRect
 from adafruit_display_text.label import Label
 from adafruit_esp32spi import adafruit_esp32spi
 from adafruit_ntp import NTP
 from adafruit_pyportal import PyPortal
+
+# Background/Images
+BACKGROUND = 0x059ACE
 
 # Initialize PyPortal Display
 display = board.DISPLAY
@@ -149,24 +153,41 @@ display.show(root_group)
 
 # TODO: Add press-able icon group
 
-# Text DisplayIO
-text_group = displayio.Group(max_size=100)
+BACKGROUND = BACKGROUND if isinstance(BACKGROUND, int) else 0x000000
+bg_bitmap = displayio.Bitmap(display.width, display.height, 1)
+bg_palette = displayio.Palette(1)
+bg_palette[0] = BACKGROUND
+background = displayio.TileGrid(bg_bitmap, pixel_shader=bg_palette)
 
-# TOTP Key Label, scaled 5x
+# Text DisplayIO
+splash = displayio.Group(max_size=100)
+
+splash.append(background)
+
+
 key_group = displayio.Group(scale=5)
 # We'll use a default text placeholder for this label
 label_key = Label(font, text="000 000")
-label_key.x = (display.width // 2) // 15
-label_key.y = 5
+label_key.x = (display.width // 2) // 13
+label_key.y = 15
 key_group.append(label_key)
-text_group.append(key_group)
 
-# Create a label for the status
+label_title = Label(font, max_glyphs=14)
+label_title.x = (display.width // 2) // 10
+label_title.y = 5
+key_group.append(label_title)
+
+splash.append(key_group)
+
+# Create a label to monitor the status
 label_status = Label(font, max_glyphs=45)
 label_status.x = (display.width // 2) - 50
-label_status.y = 75
-text_group.append(label_status)
-display.show(text_group)
+label_status.y = 120
+splash.append(label_status)
+
+# Show the group
+display.show(splash)
+
 
 print("Connecting to AP...")
 label_status.text = "Connecting to AP..."
@@ -189,11 +210,36 @@ ntp = NTP(esp)
 # keep retrying until a valid time is returned
 while not ntp.valid_time:
     ntp.set_time()
-    label_status.text = "NTP Fetch Failed, retrying.."
     print("Failed to obtain time, retrying in 5 seconds...")
     time.sleep(5)
 
-label_status.text = "NTP Set!"
+# Clear the status label
+label_status.text = ""
+
+# Add buttons to the interface
+# TODO: Generate them like in https://learn.adafruit.com/pyportal-philips-hue-lighting-controller/code-walkthrough
+# TODO: Make these dynamically based on what is store in secrets.py 
+# TODO: Add icons to buttons instead of text
+
+BUTTON_WIDTH = 60
+BUTTON_HEIGHT = 60
+BUTTON_MARGIN = 20
+buttons = []
+
+button_0 =  Button(name='Gmail',x=0, y=130,
+                  width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
+                  label="Gmail", label_font=font, style=Button.ROUNDRECT,  fill_color=0xFF0000)
+buttons.append(button_0)
+
+
+button_1 =  Button(name='Discord',x=70, y=130,
+                  width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
+                  label="Discord", label_font=font, style=Button.ROUNDRECT,  fill_color=0x9900FF)
+buttons.append(button_1)
+
+for b in buttons:
+    splash.append(b.group)
+
 
 # Get the current time in seconds since Jan 1, 1970
 t = time.time()
@@ -208,15 +254,26 @@ countdown = ON_SECONDS  # how long to stay on if not in always_on mode
 while ALWAYS_ON or (countdown > 0):
     # Calculate current time based on NTP + monotonic
     unix_time = t - mono_time + int(time.monotonic())
-    # print("Unix time: ", unix_time)
-    for name, secret in secrets['totp_keys']:
-        otp = generate_otp(unix_time // 30, secret)
-        # TODO: This needs to get cleaned up into a one-liner
-        otp = str(otp)
-        formatted_otp = "{} {}".format(otp[0:3],otp[3:6])
-        label_key.text = formatted_otp
-        label_status.text = name
-        print(name + " OTP output: ", otp)  # serial debugging output
+    p = ts.touch_point
+    if p:
+        print(p)
+        for i, b in enumerate(buttons):
+            if b.contains(p):
+                print("Button %d pressed" % i)
+                b.selected = True
+                for name, secret in secrets['totp_keys']:
+                    print(b.name)
+                    if b.name == name:
+                        print('button selected: ', name)
+                        otp = generate_otp(unix_time // 30, secret)
+                        # TODO: This needs to get cleaned up into a one-liner
+                        otp = str(otp)
+                        formatted_otp = "{} {}".format(otp[0:3],otp[3:6])
+                        label_key.text = formatted_otp
+                        label_title.text = name
+                        print(name + " OTP output: ", otp)  # serial debugging output
+            else:
+                b.selected = False
     # We'll update every 1/4 second, we can hash very fast so its no biggie!
     countdown -= 0.25
     time.sleep(0.25)
