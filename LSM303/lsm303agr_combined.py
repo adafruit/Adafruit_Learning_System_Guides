20dbbd222e8e8e971970188eb8c2a80e627f9629
siddacious <nospam187+github@gmail.com>
2020-01-08 14:17:33 -0800

    Update lsm303agr_combined.py

--- LSM303/lsm303agr_combined.py
+++ LSM303/lsm303agr_combined.py
@@ -2,10 +2,10 @@ from time import sleep
 import board
 import busio
 import adafruit_lsm303_accel
-import adafruit_lsm303agr_mag
+import adafruit_lis2mdl
 
 i2c = busio.I2C(board.SCL, board.SDA)
-mag = adafruit_lsm303agr_mag.LSM303AGR_Mag(i2c)
+mag = adafruit_lis2mdl.LIS2MDL(i2c)
 accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
 
 while True:
