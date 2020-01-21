0eb7d3e29b2c82719f8681777adbdc403a061473
CedarGroveStudios <jan@goolsbey.net>
2020-01-21 12:09:09 -0800

    add PyGamer Thermal Camera

---
+++ PyGamer_Thermal_Camera/thermal_cam_config.py
@@ -0,0 +1,5 @@
+# Thermal_Cam_config.py
+### Alarm and range default values in Farenheit ###
+ALARM_F     = 120
+MIN_RANGE_F =  60
+MAX_RANGE_F = 120
---
+++ PyGamer_Thermal_Camera/thermal_cam_converters.py
@@ -0,0 +1,8 @@
+# Thermal_Cam_converters.py
+# (c) 2020 Cedar Grove Studios for Adafruit Industries
+
+def celsius_to_fahrenheit(deg_c=None):  # convert C to F; round to 1 degree C
+    return round(((9 / 5) * deg_c) + 32)
+
+def fahrenheit_to_celsius(deg_f=None):  # convert F to C; round to 1 degree F
+    return round((deg_f - 32) * (5 / 9))
