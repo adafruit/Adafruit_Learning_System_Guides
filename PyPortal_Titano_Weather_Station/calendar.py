7f3601cd579a48da31accf21f5d365ef151890bc
BlitzCityDIY <blitzcitydiy@gmail.com>
2020-01-27 20:45:50 -0500

    adding titano weather station code & files
    Adding code, fonts, graphics and sounds for the PyPortal Titano Weather Station Learn Guide

---
+++ PyPortal_Titano_Weather_Station/calendar.py
@@ -0,0 +1,18 @@
+alarms = {
+    'bed' : '10:00 PM',
+    'breakfast' : '8:00 AM',
+    'lunch' : '12:00 PM',
+    'dinner' : '6:30 PM',
+    'trash' : ('Fri.', '1:00 PM')
+    }
+
+timers = {
+    'snooze_time' : 300
+    }
+
+holidays = {
+    'new years' : ('Jan. 1', 'Happy New Year!'),
+    'valentines' : ('Feb. 14', "Happy Valentine's Day! <3"),
+    'halloween' : ('Oct. 31', 'Happy Halloween!'),
+    'xmas' : ('Dec. 25', 'Merry Christmas!')
+    }
