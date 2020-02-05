9c050670da790fc265abba114a618879aead4870
siddacious <nospam187+github@gmail.com>
2020-02-05 15:01:59 -0800

    categorizing better

--- SipNPyuff/code.py
+++ SipNPyuff/code.py
@@ -5,28 +5,53 @@ import adafruit_lps35hw
 from puff_detector import PuffDetector
 from adafruit_debug_i2c import DebugI2C
 
+
+
+
+    # if pressure > 0:
+    #     if pressure_level == 1:
+    #         pressure_type = "Soft Puff"
+    #     if pressure_level == 2:
+    #         pressure_type = "Hard Puff"
+    # else:
+
+    #     if pressure_level == 1:
+    #         pressure_type = "Soft Sip"
+    #     if pressure_level == 2:
+    #         pressure_type = "Hard Sip"
 #pylint:disable:invalid-name
-def pressure_str(pressure):
-    pressure_type = ""
-    pressure_level = 0
+def catagorize_pressure(pressure):
+    """determine the strength and polarity of the pressure reading"""
+    level = 0
+    direction = 0
+    abs_pressure = abs(pressure)
 
-    if abs(pressure) > 10:
-        pressure_level = 1
-    if abs(pressure) > high_pressure:
-        pressure_level = 2
-    if pressure > 0:
-        if pressure_level == 1:
-            pressure_type = "Soft Puff"
-        if pressure_level == 2:
-            pressure_type = "Hard Puff"
-    else:
+    if abs_pressure > min_pressure:
+        level = 1
+    if abs_pressure > high_pressure:
+        level = 2
 
-        if pressure_level == 1:
-            pressure_type = "Soft Sip"
-        if pressure_level == 2:
-            pressure_type = "Hard Sip"
-    return pressure_type
+    if level != 0:
+        if pressure > 0:
+            direction = 1
+        else:
+            direction = -1
 
+    return (direction, level)
+
+def pressure_string(pressure_type):
+    dir, level =  pressure_type
+    pressure_str = ""
+    if level == 1:
+        pressure_str = "LOW"
+    elif level == 2:
+        presure_str = "HIGH"
+
+    if dir == 1:
+        pressure_str += "PUFF"
+    elif dir == -1:
+        pressure_str += "SIP"
+    return pressure_str
 
 i2c = busio.I2C(board.SCL, board.SDA)
 # i2c = DebugI2C(i2c)
@@ -34,8 +59,7 @@ lps = adafruit_lps35hw.LPS35HW(i2c, 0x5c)
 
 lps.zero_pressure()
 lps.data_rate = adafruit_lps35hw.DataRate.RATE_75_HZ
-max_postive = 0
-max_negative = 0
+
 min_pressure = 8
 high_pressure = 20
 # sip/puff is "over" on keyup/pressure direction reversal
@@ -43,30 +67,36 @@ high_pressure = 20
 
 
 
-# find reasonable band estimatesV
-print("Filter enabled:", lps.low_pass_enabled)
 lps.filter_enabled = True
 print("Filter enabled:", lps.low_pass_enabled)
 
-print("Filter Config:", lps.low_pass_config)
 lps.filter_config = True
 print("Filter Config:", lps.low_pass_config)
 
+time.sleep(1)
 prev_direction = None
 pressure_list = []
-pressure_type = ""
-prev_pressure_type = ""
-puff_start = 0
-puff_end = 0
+pressure_type = tuple()
+prev_pressure_type = tuple()
+puff_start = time.monotonic()
+puff_end = puff_start
 while True:
-    pressure = lps.pressure
-    # print((pressure,))
+    current_pressure = lps.pressure
+    # print((current_pressure,))
 
-    pressure_type = pressure_str(pressure)
+    pressure_type = catagorize_pressure(current_pressure)
+
     if pressure_type != prev_pressure_type:
         puff_end = time.monotonic()
-        print("duration:", puff_end-puff_start)
+        puff_duration = puff_end-puff_start
         puff_start = puff_end
-        print(pressure_type)
+        print("pressure type:", pressure_type)
+        if puff_duration > 0.2:
+            print("duration:", puff_duration)
+            print(current_pressure)
+            print("abs:", abs(current_pressure))
+            label = pressure_string(pressure_type)
+            print(label)
+            print("____________________")
     prev_pressure_type = pressure_type
     time.sleep(0.01)
