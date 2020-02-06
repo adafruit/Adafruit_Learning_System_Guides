5464a20ff1c569c4f7729b5558134d984bb91015
siddacious <nospam187+github@gmail.com>
2020-02-05 16:33:58 -0800

    checkpoint

--- SipNPyuff/code.py
+++ SipNPyuff/code.py
@@ -20,6 +20,7 @@ from adafruit_debug_i2c import DebugI2C
     #     if pressure_level == 2:
     #         pressure_type = "Hard Sip"
 #pylint:disable:invalid-name
+
 def catagorize_pressure(pressure):
     """determine the strength and polarity of the pressure reading"""
     level = 0
@@ -42,6 +43,7 @@ def catagorize_pressure(pressure):
 def pressure_string(pressure_type):
     dir, level =  pressure_type
     pressure_str = ""
+    print("pressure level:", level)
     if level == 1:
         pressure_str = "LOW"
     elif level == 2:
@@ -65,13 +67,11 @@ high_pressure = 20
 # sip/puff is "over" on keyup/pressure direction reversal
 # averaging code
 
-
-
 lps.filter_enabled = True
-print("Filter enabled:", lps.low_pass_enabled)
+# print("Filter enabled:", lps.low_pass_enabled)
 
 lps.filter_config = True
-print("Filter Config:", lps.low_pass_config)
+# print("Filter Config:", lps.low_pass_config)
 
 time.sleep(1)
 prev_direction = None
@@ -79,7 +79,10 @@ pressure_list = []
 pressure_type = tuple()
 prev_pressure_type = tuple()
 puff_start = time.monotonic()
+current_level = 0
+prev_level = 0
 puff_end = puff_start
+prev_duration = 0
 while True:
     current_pressure = lps.pressure
     # print((current_pressure,))
@@ -90,13 +93,28 @@ while True:
         puff_end = time.monotonic()
         puff_duration = puff_end-puff_start
         puff_start = puff_end
-        print("pressure type:", pressure_type)
+        print("\tpressure type:", pressure_type)
+        print("duration:", puff_duration)
+        direction, level = pressure_type
+        print("direction:", direction, "level:", level)
+
+        if (direction == 1) and (prev_level > level):
+            print("\tDown")
+            puff_duration += prev_duration
+            level = prev_level
+        if (direction == -1) and (prev_level < level):
+            print("\tUp")
+            puff_duration += prev_duration
+            level = prev_level
+        
+        print("direction:", direction, "level:", level)
         if puff_duration > 0.2:
-            print("duration:", puff_duration)
+            print("\tduration:", puff_duration)
             print(current_pressure)
-            print("abs:", abs(current_pressure))
             label = pressure_string(pressure_type)
             print(label)
             print("____________________")
-    prev_pressure_type = pressure_type
+        prev_pressure_type = pressure_type
+        prev_duration = puff_duration
+    prev_level = level
     time.sleep(0.01)
