04e58fd09b419d827e0d8068f50c2f29be180855
brentru <robots199@me.com>
2020-04-22 12:13:55 -0400

    add selectable bacground

--- PyPortal_Quarantine_Clock/code.py
+++ PyPortal_Quarantine_Clock/code.py
@@ -7,7 +7,7 @@ from adafruit_esp32spi import adafruit_esp32spi_socket as socket
 from adafruit_esp32spi import adafruit_esp32spi, adafruit_esp32spi_wifimanager
 import adafruit_requests as requests
 import digitalio
-import analogio
+from analogio import AnalogIn
 from adafruit_pyportal import PyPortal
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text import label
@@ -20,6 +20,16 @@ except ImportError:
 the secrets dictionary must contain 'ssid' and 'password' at a minimum""")
     raise
 
+# Colors for each label
+LABEL_DAY_COLOR = 0xFFFFFF
+LABEL_TIME_COLOR = 0x8f42f4
+
+# the current working directory (where this file is)
+cwd = ("/"+__file__).rsplit('/', 1)[0]
+background = None
+# un-comment to set background image
+# background = cwd+"/background.bmp"
+
 # Descriptions of each hour
 # https://github.com/mwfisher3/QuarantineClock/blob/master/today.html
 time_name = ["midnight-ish", "late night", "late", "super late",
@@ -29,7 +39,6 @@ time_name = ["midnight-ish", "late night", "late", "super late",
             "late afternoon","early evening","early evening","dusk-ish",
             "evening","evening","late evening","late evening"]
 
-
 esp32_cs = digitalio.DigitalInOut(board.ESP_CS)
 esp32_ready = digitalio.DigitalInOut(board.ESP_BUSY)
 esp32_reset = digitalio.DigitalInOut(board.ESP_RESET)
@@ -37,7 +46,6 @@ esp32_reset = digitalio.DigitalInOut(board.ESP_RESET)
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset, debug=False)
 requests.set_socket(socket, esp)
-
 
 def wday_to_weekday_name(tm_wday):
     """Returns the name of the weekday based on tm_wday value.
@@ -52,11 +60,15 @@ def wday_to_weekday_name(tm_wday):
     4: "Friday",
     5: "Saturday",
     6: "Sunday",}
-    return switch.get(tm_wday, "Not found")
+    return switch.get(tm_wday, "Day not found")
 
 # initialize pyportal
 pyportal = PyPortal(esp=esp,
-                    external_spi=spi)
+                    external_spi=spi,
+                    default_bg = None)
+
+# set pyportal's backlight brightness
+pyportal.set_backlight(0.7)
 
 if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
     print("ESP32 found and in idle mode")
@@ -72,19 +84,19 @@ while not esp.is_connected:
         continue
 
 # Set the font and preload letters
-font_large = bitmap_font.load_font("/fonts/Helvetica-Bold-36.bdf")
+font_large = bitmap_font.load_font("/fonts/Helvetica-Bold-44.bdf")
 font_small = bitmap_font.load_font("/fonts/Helvetica-Bold-24.bdf")
 font_large.load_glyphs(b'abcdefghjiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890- ')
 font_small.load_glyphs(b'abcdefghjiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890- ()')
 
 # Set up label for the day
-label_day = label.Label(font_large, color=0xFFFFFF, max_glyphs=200)
-label_day.x = board.DISPLAY.width // 5
-label_day.y = 70
+label_day = label.Label(font_large, color=LABEL_DAY_COLOR, max_glyphs=200)
+label_day.x = board.DISPLAY.width // 7
+label_day.y = 80
 pyportal.splash.append(label_day)
 
 # Set up label for the time
-label_time = label.Label(font_small, color=0x538ab1, max_glyphs=200)
+label_time = label.Label(font_small, color=LABEL_TIME_COLOR, max_glyphs=200)
 label_time.x = board.DISPLAY.width // 4
 label_time.y = 150
 pyportal.splash.append(label_time)
@@ -97,16 +109,14 @@ while True:
             print("Getting new time from internet...")
             pyportal.get_local_time(secrets['timezone'])
             refresh_time = time.monotonic()
+            # set the_time
+            the_time = time.localtime()
+            # Convert tm_wday to name of day
+            weekday = wday_to_weekday_name(the_time.tm_wday)
         except (ValueError, RuntimeError) as error:
             print("Failed to get data, retrying\n", e)
             wifi.reset()
             continue
-
-    # set the_time
-    the_time = time.localtime()
-
-    # Convert tm_wday to name of day
-    weekday = wday_to_weekday_name(the_time.tm_wday)
 
     # set the day label's text
     label_day.text = weekday
@@ -114,5 +124,5 @@ while True:
     # set the time label's text
     label_time.text = "({})".format(time_name[the_time.tm_hour])
 
-    # update every 30s.
-    time.sleep(30)
+    # update every minute.
+    time.sleep(60)
