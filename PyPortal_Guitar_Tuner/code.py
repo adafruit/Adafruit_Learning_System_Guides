836de1635bfb7c99155d42981d8e1b78eef8a5d0
BlitzCityDIY <blitzcitydiy@gmail.com>
2020-06-08 12:21:29 -0400

    Adding code and files for PyPortal Guitar Tuner
    Adding code, audio files (six) and bitmap graphic for PyPortal Guitar Tuner Learn Guide.
    
    When the note name is pressed on the PyPortal's touchscreen, the corresponding note is played 3 times through the speaker.

---
+++ PyPortal_Guitar_Tuner/code.py
@@ -0,0 +1,64 @@
+import time
+import board
+import displayio
+import adafruit_imageload
+from adafruit_button import Button
+from adafruit_pyportal import PyPortal
+
+display = board.DISPLAY
+
+pyportal = PyPortal()
+
+bitmap, palette = adafruit_imageload.load("/stock-pyportal.bmp",
+                                          bitmap=displayio.Bitmap,
+                                          palette=displayio.Palette)
+
+# Create a TileGrid to hold the bitmap
+tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+
+lowE = "/sounds/lowE.wav"
+A = "/sounds/A.wav"
+D = "/sounds/D.wav"
+G = "/sounds/G.wav"
+B = "/sounds/B.wav"
+highE = "/sounds/highE.wav"
+
+notes = [lowE, A, D, G, B, highE]
+
+pegs = [
+    {'label': "lowE", 'pos': (53, 0), 'size': (65, 90)},
+    {'label': "A", 'pos': (124, 0), 'size': (65, 90)},
+    {'label': "D", 'pos': (194, 0), 'size': (65, 90)},
+    {'label': "G", 'pos': (194, 150), 'size': (65, 90)},
+    {'label': "B", 'pos': (124, 150), 'size': (65, 90)},
+    {'label': "highE", 'pos': (53, 150), 'size': (65, 90)}
+    ]
+
+pyportal.splash.append(tile_grid)
+
+buttons = []
+for peg in pegs:
+    button = Button(x=peg['pos'][0], y=peg['pos'][1],
+                    width=peg['size'][0], height=peg['size'][1],
+                    style=Button.RECT,
+                    fill_color=None, outline_color=0x5C3C15,
+                    name=peg['label'])
+    pyportal.splash.append(button.group)
+    buttons.append(button)
+
+note_select = None
+
+while True:
+    touch = pyportal.touchscreen.touch_point
+    if not touch and note_select:
+        note_select = False
+    if touch:
+        for i in range(6):
+            tuning = notes[i]
+            button = buttons[i]
+            if button.contains(touch) and not note_select:
+                print("Touched", button.name)
+                note_select = True
+                for z in range(3):
+                    pyportal.play_file(tuning)
+    time.sleep(0.1)
