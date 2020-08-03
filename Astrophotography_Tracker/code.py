27febca14a4c0dbae24c8b17e03787a4c5a705f6
dherrada <dylan.herrada@adafruit.com>
2020-08-03 09:31:57 -0400

    Moved calculations in step_time.py to code.py

--- Astrophotography_Tracker/code.py
+++ Astrophotography_Tracker/code.py
@@ -2,6 +2,16 @@ import time
 import board
 import digitalio
 
+worm_ratio = 40/1
+belt_ratio = 100/60
+gear_ratio = worm_ratio * belt_ratio
+
+steps = 200 # Steps per revolution
+microsteps = 64 # Microstepping resolution
+total_steps = steps * microsteps # Total microsteps per revolution
+
+wait = 1/ ((gear_ratio * steps * microsteps) / 86400)
+
 step = digitalio.DigitalInOut(board.D6)
 direct = digitalio.DigitalInOut(board.D5)
 
@@ -14,4 +24,4 @@ while True:
     step.value = True
     time.sleep(0.001)
     step.value = False
-    time.sleep(0.10025)
+    time.sleep(wait - 0.001)
--- Astrophotography_Tracker/step_time.py
+++
@@ -1,9 +0,0 @@
-worm_ratio = 40 / 1
-belt_ratio = 100 / 60
-
-steps = 200
-microsteps = 64
-
-time = 1 / (((worm_ratio * belt_ratio) * steps * microsteps) / 86400)
-print(f"One step every: {time} seconds")
-print(f"Delay should be: {time-.001} seconds")
