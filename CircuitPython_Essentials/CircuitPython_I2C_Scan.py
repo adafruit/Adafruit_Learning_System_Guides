dccfcc10a3dc83935279638c2bb3d538ed293b0e
Hawley Waldman <hawleyw@gmail.com>
2020-08-13 07:48:55 -0400

    formatting error

--- CircuitPython_Essentials/CircuitPython_I2C_Scan.py
+++ CircuitPython_Essentials/CircuitPython_I2C_Scan.py
@@ -15,10 +15,10 @@ while not i2c.try_lock():
 
 try:
 
-while True:
-    print("I2C addresses found:", [hex(device_address)
-                                   for device_address in i2c.scan()])
-    time.sleep(2)
+    while True:
+        print("I2C addresses found:", [hex(device_address)
+            for device_address in i2c.scan()])
+        time.sleep(2)
 
 finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
     i2c.unlock()
