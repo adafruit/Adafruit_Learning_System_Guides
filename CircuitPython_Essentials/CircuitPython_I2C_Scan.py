d65e8d11f3cb0b35f51d4d54f23f364f09c7cf45
Hawley Waldman <hawleyw@gmail.com>
2020-08-13 19:25:11 -0400

    Make requested formatting changes for pull

--- CircuitPython_Essentials/CircuitPython_I2C_Scan.py
+++ CircuitPython_Essentials/CircuitPython_I2C_Scan.py
@@ -2,8 +2,8 @@
 # 
 # If you run this and it seems to hang, try manually unlocking
 # your I2C bus from the REPL with
-# ` >>> import board `
-# ` >>> board.I2C().unlock() `
+#  >>> import board 
+#  >>> board.I2C().unlock() 
 
 import time
 import board
@@ -14,10 +14,9 @@ while not i2c.try_lock():
     pass
 
 try:
-
     while True:
         print("I2C addresses found:", [hex(device_address)
-            for device_address in i2c.scan()])
+              for device_address in i2c.scan()])
         time.sleep(2)
 
 finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
