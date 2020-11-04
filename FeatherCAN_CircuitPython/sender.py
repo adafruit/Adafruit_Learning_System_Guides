a806e4b4c617bc7c510898fa368ae4f1c934ba65
Jeff Epler <jepler@gmail.com>
2020-11-04 09:31:14 -0600

    FeatherCAN: Add basic sender/listener code

---
+++ FeatherCAN_CircuitPython/sender.py
@@ -0,0 +1,35 @@
+import board
+import canio
+import digitalio
+import struct
+import time
+
+# If the CAN transceiver has a standby pin, bring it out of standby mode
+if hasattr(board, 'CAN_STANDBY'):
+    standby = digitalio.DigitalInOut(board.CAN_STANDBY)
+    standby.switch_to_output(False)
+
+# If the CAN transceiver is powered by a boost converter, turn on its supply
+if hasattr(board, 'BOOST_ENABLE'):
+    standby = digitalio.DigitalInOut(board.CAN_STANDBY)
+    standby.switch_to_output(True)
+
+can = canio.CAN(rx=board.CAN_RX, tx=board.CAN_TX, baudrate=250_000, auto_restart=True)
+
+old_bus_state = None
+count = 0
+
+while True:
+    bus_state = can.state
+    if bus_state != old_bus_state:
+        print(f"Bus state changed to {bus_state}")
+        old_bus_state = bus_state
+
+    now_ms = (time.monotonic_ns() // 1_000_000) & 0xffffffff
+    print(f"Sending message: count={count} now_ms={now_ms}")
+
+    message = canio.Message(id=0x408, data=struct.pack("<II", count, now_ms))
+    can.send(message)
+
+    time.sleep(.5)
+    count += 1
