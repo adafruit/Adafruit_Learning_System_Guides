35eb371ac27fe96f622d9b0559e63ccb8aec298b
John Park <jp@jpixl.net>
2020-11-17 20:40:47 -0800

    updated w magtag library

--- Magtag_Slideshow/magtag_slideshow.py
+++ Magtag_Slideshow/magtag_slideshow.py
@@ -2,143 +2,121 @@
 # auto plays .bmp images in /slides folder
 # press left and right buttons to go back or forward one slide
 # press down button to toggle autoplay mode
-# press up button to toggle NeoPixels
+# press up button to toggle sound
 
 import time
-import board
-from digitalio import DigitalInOut, Direction, Pull
-import displayio
-import adafruit_il0373
-from adafruit_slideshow import SlideShow, PlayBackDirection
-import neopixel
+import terminalio
+from adafruit_magtag.magtag import MagTag
+from adafruit_slideshow import PlayBackOrder, SlideShow, PlayBackDirection
 
-displayio.release_displays()
-display_bus = displayio.FourWire(board.SPI(), command=board.EPD_DC,
-                                 chip_select=board.EPD_CS,
-                                 reset=board.EPD_RESET, baudrate=1000000)
-time.sleep(1)
-display = adafruit_il0373.IL0373(
-    display_bus,
-    width=296,
-    height=128,
-    rotation=270,
-    black_bits_inverted=False,
-    color_bits_inverted=False,
-    grayscale=True,
-    refresh_time=1,
-    seconds_per_frame=1  # this overrides the default 180, just be careful
+magtag = MagTag()
+
+def blink(color, duration):
+    magtag.peripherals.neopixel_disable = False
+    magtag.peripherals.neopixels.fill(color)
+    time.sleep(duration)
+    magtag.peripherals.neopixel_disable = True
+
+RED = 0x880000
+GREEN = 0x008800
+BLUE = 0x000088
+YELLOW = 0x884400
+CYAN = 0x0088BB
+MAGENTA = 0x9900BB
+WHITE = 0x888888
+
+blink(WHITE, 0.3)
+
+# pylint: disable=no-member
+magtag.add_text(
+    text_font=terminalio.FONT,
+    text_position=(
+        5,
+        (magtag.graphics.display.height // 2) - 1,
+    ),
+    text_scale=3,
 )
-
-forward_button = DigitalInOut(board.BUTTON_D)
-back_button = DigitalInOut(board.BUTTON_A)
-pixel_button = DigitalInOut(board.BUTTON_B)
-pixel_enable = DigitalInOut(board.NEOPIXEL_POWER)
-autoplay_button = DigitalInOut(board.BUTTON_C)
-led = DigitalInOut(board.D13)
-
-forward_button.pull = Pull.UP
-back_button.pull = Pull.UP
-pixel_button.pull = Pull.UP
-autoplay_button.pull = Pull.UP
-
-led.direction = Direction.OUTPUT
-pixel_enable.direction = Direction.OUTPUT
-
-pixel_enable.value = False  # Set this False to use NeoPixels
-led.value = True
-
-RED = (0xff0000)
-GREEN = (0x00ff00)
-BLUE = (0x0000ff)
-YELLOW = (0xbb6600)
-CYAN = (0x0088bb)
-MAGENTA = (0x9900bb)
-WHITE = (0xaaaaaa)
-BLACK = (0x000000)
-
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, brightness=0.04, auto_write=False)
-
-def blink_pixels(color, seconds):
-    pixels.fill(color)
-    pixels.show()
-    time.sleep(seconds)
-    pixels.fill(BLACK)
-    pixels.show()
-
-blink_pixels(WHITE, 1)
-
-def light_pixels(color):
-    pixels.fill(color)
-    pixels.show()
-
-slideshow = SlideShow(
-    display,
-    None,
-    folder="/slides",
-    auto_advance=False,
-    dwell=0,
+magtag.set_text("MagTag Slideshow")
+time.sleep(5)
+magtag.add_text(
+    text_font=terminalio.FONT,
+    text_position=(3, 120),
+    text_scale=1,
 )
+magtag.set_text("  back        mute      pause/play     fwd", 1)
+time.sleep(8)
 
-print(slideshow.current_image_name)
-display.refresh()
-time.sleep(1)
-
-pixel_toggle = False  # holds state of the pixels
-autoplay_toggle = True
+timestamp = time.monotonic()
+sound_toggle = True  # state of sound feedback
+autoplay_toggle = True  # state of autoplay
 auto_pause = 60  # time between slides in auto mode
-timestamp = None
+
+# Create the slideshow object that plays through alphabetically.
+slideshow = SlideShow(
+    magtag.graphics.display,
+    None,
+    auto_advance=autoplay_toggle,
+    folder="/slides",
+    loop=True,
+    order=PlayBackOrder.ALPHABETICAL,
+    dwell=auto_pause,
+)
 
 while True:
-    if not timestamp or (time.monotonic() - timestamp) > auto_pause:
-        timestamp = time.monotonic()
-        if autoplay_toggle:
-            slideshow.advance()
-            print(slideshow.current_image_name)
-            display.refresh()
-
-    if not forward_button.value:  # button is pressed when it goes false
-        led.value = True
-        light_pixels(CYAN)
-        slideshow.direction = PlayBackDirection.FORWARD
-        slideshow.advance()
-        print(slideshow.current_image_name)
-        display.refresh()
-        light_pixels(BLACK)
-
-    if not back_button.value:
-        led.value = True
-        light_pixels(MAGENTA)
+    slideshow.update()
+    if magtag.peripherals.button_a_pressed:
+        if sound_toggle:
+            try:  # dealing with an ocassional bug here
+                magtag.peripherals.play_tone(220, 0.15)
+            except NameError:
+                pass
+        blink(YELLOW, 0.4)
         slideshow.direction = PlayBackDirection.BACKWARD
+        time.sleep(5)
         slideshow.advance()
-        print(slideshow.current_image_name)
-        display.refresh()
-        light_pixels(BLACK)
 
-    if not pixel_button.value:
-        led.value = True
-        if not pixel_toggle:
-            light_pixels(YELLOW)
-            pixel_toggle = True
-            time.sleep(0.35)  # debounce
+    if magtag.peripherals.button_b_pressed:
+        if not sound_toggle:
+            try:  # dealing with an ocassional bug here
+                magtag.peripherals.play_tone(660, 0.15)
+            except NameError:
+                pass
+            blink(CYAN, 0.4)
+            sound_toggle = True
         else:
-            light_pixels(BLACK)
-            pixel_toggle = False
-            time.sleep(0.35)  # debounce
+            blink(MAGENTA, 0.4)
+            sound_toggle = False
 
-    if not autoplay_button.value:
-        led.value = True
+    if magtag.peripherals.button_c_pressed:
         if not autoplay_toggle:
+            if sound_toggle:
+                try:  # dealing with an ocassional bug here
+                    magtag.peripherals.play_tone(440, 0.15)
+                except NameError:
+                    pass
+            blink(GREEN, 0.4)
             autoplay_toggle = True
-            light_pixels(GREEN)
-            print("Autoplay ON")
-            time.sleep(0.25)
-            light_pixels(BLACK)
-
+            slideshow.direction = PlayBackDirection.FORWARD
+            slideshow.auto_advance = True
         else:
+            if sound_toggle:
+                try:  # dealing with an ocassional bug here
+                    magtag.peripherals.play_tone(110, 0.15)
+                except NameError:
+                    pass
+            blink(RED, 0.4)
             autoplay_toggle = False
-            light_pixels(RED)
-            print("Autoplay off")
-            time.sleep(0.25)
-            light_pixels(BLACK)
+            slideshow.auto_advance = False
 
-    led.value = False
+    if magtag.peripherals.button_d_pressed:
+        if sound_toggle:
+            try:  # dealing with an ocassional bug here
+                magtag.peripherals.play_tone(880, 0.15)
+            except NameError:
+                pass
+        blink(BLUE, 0.4)
+        slideshow.direction = PlayBackDirection.FORWARD
+        time.sleep(5)
+        slideshow.advance()
+
+    time.sleep(0.01)
