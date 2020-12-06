acdbd25801c95117a03908a2cf2ec9a5420b4064
Phillip Burgess <paintyourdragon@dslextreme.com>
2020-12-06 13:30:30 -0800

    Sundry code changes and lighten background bitmap
    Initially based on Limor's refactor, plus further cleanup: several now-unused variables are removed, update comments to reflect new reality, use pylint-compliant variable names in places, consistent use of ' vs " quotes

--- MagTag_Seasonal_Produce/code.py
+++ MagTag_Seasonal_Produce/code.py
@@ -4,7 +4,7 @@ Lists in-season fruits and vegetables for a user's location and season.
 "Smart cursive" BDF font by Thomas A. Fine, helvB12.bdf from Xorg fonts.
 """
 
-# pylint: disable=import-error, no-name-in-module
+# pylint: disable=import-error, line-too-long
 import time
 from secrets import secrets
 import rtc
@@ -13,22 +13,23 @@ from adafruit_magtag.magtag import MagTag
 from produce import Produce
 
 
-# CONFIGURABLE SETTINGS ----------------------------------------------------
+# CONFIGURABLE SETTINGS and ONE-TIME INITIALIZATION ------------------------
 
-LOCATION = 'CA'       # 2-letter postal U.S. state code
-TWELVE_HOUR = True    # If set, show 12-hour vs 24-hour (e.g. 3:00 vs 15:00)
-DD_MM = False         # If set, show DD/MM instead of MM/DD dates
-DAILY_UPDATE_HOUR = 0 # Update around midnight
-DEEP_SLEEP = True     # True for max battery run time, False for testing
+TWELVE_HOUR = True # If set, show 12-hour vs 24-hour (e.g. 3:00 vs 15:00)
+DD_MM = False      # If set, show DD/MM instead of MM/DD dates
 # Location of produce data (file:// or http:// or https://):
 JSON_URL = 'https://raw.githubusercontent.com/adafruit/Adafruit_Learning_System_Guides/master/MagTag_Seasonal_Produce/produce.json'
 
-# Location and time zone can be configured in secrets.py. If location is not
-# contained there, value above will be used.
-LOCATION = secrets.get('location', "NY") # default to 'NY'
+# Location is configured in secrets.py. If location is not contained there,
+# default value below will be used.
+LOCATION = secrets.get('location', 'NY') # default to 'NY'
 
-magtag = MagTag(rotation=0)  # portrait!
+PRODUCE = Produce(JSON_URL, LOCATION)
+MAGTAG = MagTag(rotation=0) # Portrait (vertical) display
+
+
 # SOME UTILITY FUNCTIONS ---------------------------------------------------
+
 def hh_mm(time_struct, twelve_hour=True):
     """ Given a time.struct_time, return a string as H:MM or HH:MM, either
         12- or 24-hour style depending on twelve_hour flag.
@@ -45,83 +46,77 @@ def hh_mm(time_struct, twelve_hour=True):
     return hour_string + ':{mm:02d}'.format(mm=time_struct.tm_min)
 
 
-# ONE-TIME INITIALIZATION --------------------------------------------------
-
-PRODUCE = Produce(JSON_URL, LOCATION)
-
 # GRAPHICS INITIALIZATION --------------------------------------------------
-# Load background image, make it the bottom-most element
-magtag.graphics.set_background("bitmaps/produce.bmp")
+
+MAGTAG.graphics.set_background('bitmaps/produce.bmp')
 
 # Produce list is inserted at this position
-magtag.add_text(
-    text_font="/fonts/cursive-smart.bdf",
+MAGTAG.add_text(
+    text_font='/fonts/cursive-smart.bdf',
     text_position=(3, 2),
     line_spacing=1.0,
     text_anchor_point=(0, 0),  # top left
     is_data=False, # we'll set this text manually
 )
-#magtag.set_text("testing", auto_refresh=False)  # don't refresh
 
 # Add 14-pixel-tall black bar at bottom of display. It's a distinct layer
 # (not just background) to appear on top of produce list if it runs long.
-rect = Rect(0, magtag.graphics.display.height - 14,
-            magtag.graphics.display.width, magtag.graphics.display.height,
-            fill=0x0)
-magtag.graphics.splash.append(rect)
+MAGTAG.graphics.splash.append(Rect(0, MAGTAG.graphics.display.height - 14,
+                                   MAGTAG.graphics.display.width,
+                                   MAGTAG.graphics.display.height, fill=0x0))
 
 # Center white text label over black bar to show last update time
 # (Initially a placeholder, string is not assigned to label until later)
-magtag.add_text(
-    text_font="/fonts/helvB12.bdf",
-    text_position=(magtag.graphics.display.width // 2, magtag.graphics.display.height - 1),
+MAGTAG.add_text(
+    text_font='/fonts/helvB12.bdf',
+    text_position=(MAGTAG.graphics.display.width // 2,
+                   MAGTAG.graphics.display.height - 1),
     text_maxlen=19,
     text_color=0xFFFFFF,
     text_anchor_point=(0.5, 1),
-    is_data=False, # we'll set this text manually
+    is_data=False, # we'll set this text manually later
 )
-#magtag.set_text("date", 1, auto_refresh=False)
+
 
 # MAIN LOOP ----------------------------------------------------------------
 
-# Just FYI, the "main loop" doesn't really loop when using deep sleep...
-# it runs once and the whole system restarts on wake. It's written this
-# way so time.sleep() can be used during testing rather than deep sleep.
+# FYI: Not really a "loop" -- deep sleep makes the whole system restart on
+# wake, this only needs to run once.
 
 try:
-    magtag.network.connect() # Sneak this in last, as WiFi uses power=
+    MAGTAG.network.connect() # Sneak this in last, as WiFi uses power
     print('Updating time')
-    magtag.get_local_time()
-    now = rtc.RTC().datetime
-    print(now)
+    MAGTAG.get_local_time()
+    NOW = rtc.RTC().datetime
+    print(NOW)
 
     print('Updating produce')
-    PRODUCE.fetch(magtag)
+    PRODUCE.fetch(MAGTAG)
 
     # Set the "Updated" date and time label
     if DD_MM:
-        DATE = '%d/%d' % (now.tm_mday, now.tm_mon)
+        DATE = '%d/%d' % (NOW.tm_mday, NOW.tm_mon)
     else:
-        DATE = '%d/%d' % (now.tm_mon, now.tm_mday)
-    magtag.set_text('Updated %s %s' % (DATE, hh_mm(now, TWELVE_HOUR)), 1, auto_refresh=False)
+        DATE = '%d/%d' % (NOW.tm_mon, NOW.tm_mday)
+    MAGTAG.set_text('Updated %s %s' % (DATE, hh_mm(NOW, TWELVE_HOUR)), 1,
+                    auto_refresh=False)
 
     # Look up the matching produce data (returned as list of strings)
-    PRODUCE_LIST = PRODUCE.in_season(now.tm_mon)
+    PRODUCE_LIST = PRODUCE.in_season(NOW.tm_mon)
     NUM_ITEMS = len(PRODUCE_LIST)
-    print("Produce list: ", PRODUCE_LIST)
-    # Make the list nicely wrapped since some words may be long
-    veggie_list = ""
+    print('Produce list: ', PRODUCE_LIST)
+    # List one item per line since some may be long
+    VEGGIE_LIST = ''
     for item in PRODUCE_LIST:
-        print(item)
-        veggie_list += "\n".join(magtag.wrap_nicely(item, 15)) + "\n"
-    magtag.set_text(veggie_list)  # this finally updates the display!
+        VEGGIE_LIST += '\n'.join(MAGTAG.wrap_nicely(item, 15)) + '\n'
+    MAGTAG.set_text(VEGGIE_LIST) # Update list on the display
 
     time.sleep(2) # Allow refresh to finish before deep sleep
-    print("Zzzz time")
-    magtag.exit_and_deep_sleep(24 * 60 * 60)  # one day snooze
+    print('Zzzz time')
+    MAGTAG.exit_and_deep_sleep(24 * 60 * 60) # 24 hour snooze
 
-except RuntimeError as e:
+except RuntimeError as error:
     # If there's an error above, no harm, just try again in ~15 minutes.
     # Usually it's a common network issue or time server hiccup.
-    print("Retrying - ", e)
-    magtag.exit_and_deep_sleep(15 * 60) # 15 minute snooze
+    print('Retrying in 15 min - ', error)
+    MAGTAG.exit_and_deep_sleep(15 * 60) # 15 minute snooze
