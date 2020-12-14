35b9bfa4bf37e60c46ae65d9b47420440d08bc35
Collin Cunningham <me@collinmel.com>
2020-12-14 12:57:48 -0500

    Added current time clock, fonts

--- Pet_Feeder_Clock/code.py
+++ Pet_Feeder_Clock/code.py
@@ -5,29 +5,28 @@ import rtc
 PET_NAME = "Midi"
 
 magtag = MagTag()
-magtag.auto_refresh = False
-magtag.network.connect()
 
 mid_x = magtag.graphics.display.width // 2 - 1
 magtag.add_text(
     text_font="/Montserrat-Regular-78.bdf",
-    text_position=(mid_x,6),
+    text_position=(mid_x,10),
     text_anchor_point=(0.5,0),
     is_data=False,
 )
-magtag.set_text("00:00")
+magtag.set_text("00:00", auto_refresh = False)
 
 magtag.add_text(
-    text_font="/CourierNewPSMT-18.bdf",
-    text_position=(20,80),
+    text_font="/Arial-12.bdf",
+    text_position=(48  ,94),
     text_anchor_point=(0,0),
+    line_spacing=0.7,
     is_data=False,
 )
-magtag.set_text("test:", index = 1)
+magtag.set_text("last feeding:", index = 1, auto_refresh = False)
 
 magtag.add_text(
     text_font="/Montserrat-Regular-39.bdf",
-    text_position=(132,80),
+    text_position=(141,86),
     text_anchor_point=(0,0),
     is_data=False,
 )
@@ -38,16 +37,50 @@ def network_time():
     now = rtc.RTC().datetime
     return now
 
+def time_for_display(new_time):
+    hour = new_time.tm_hour if new_time.tm_hour <= 12 else new_time.tm_hour - 12
+    suffix = "a" if new_time.tm_hour < 12 else "p"
+    time_string = str(hour) + ":" + str(new_time.tm_min) + suffix
+    return time_string
+
+magtag.network.connect()
+
+# Set initial clock time
+# pylint: disable=bare-except
+try:
+    DATETIME = network_time()
+except:
+    DATETIME = time.localtime()
+LAST_SYNC = time.mktime(DATETIME)
+
+LAST_DISPLAY = 0
+
+
 while(True):
 
+    NOW = time.time() # Current epoch time in seconds
+
+    # Sync with time server every hour
+    if NOW - LAST_SYNC > 60 * 60:
+        try:
+            DATETIME = network_time()
+            LAST_SYNC = time.mktime(DATETIME)
+            continue # Time may have changed; refresh NOW value
+        except:
+            # if unable to sync time, try again in 30 min
+            LAST_SYNC += 30 * 60 # 30 minutes -> seconds
+
+    if NOW - LAST_DISPLAY > 60:
+        # display new time
+        magtag.set_text(time_for_display(DATETIME), index = 0)
+        # increment local time
+        diff_secs = NOW - LAST_DISPLAY
+        new_secs = time.mktime(DATETIME) + diff_secs
+        DATETIME = time.localtime(new_secs) # OverflowError: overflow converting long int to machine word
+        LAST_DISPLAY = NOW
+
     if magtag.peripherals.button_a_pressed:
-        now = network_time()
-        #set time
-        hour = now.tm_hour if now.tm_hour <= 12 else now.tm_hour - 12
-        suffix = "a" if now.tm_hour < 12 else "p"
-        time_string = str(hour) + ":" + str(now.tm_min) + suffix
-        print(time_string)
-        magtag.set_text(time_string, index = 2)
+        magtag.set_text(time_for_display(DATETIME), index = 2)
         #magtag.show()
         time.sleep(2)
 
