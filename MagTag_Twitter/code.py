212e8f3ae3434d38eb814b61a6551e7b0566c12b
brentru <robots199@me.com>
2020-12-24 16:51:55 -0500

    Black

--- MagTag_Twitter/code.py
+++ MagTag_Twitter/code.py
@@ -8,30 +8,32 @@ from adafruit_magtag.magtag import MagTag
 try:
     from secrets import secrets
 except ImportError:
-    print("""WiFi settings are kept in secrets.py, please add them there!
-the secrets dictionary must contain 'ssid' and 'password' at a minimum""")
+    print(
+        """WiFi settings are kept in secrets.py, please add them there!
+the secrets dictionary must contain 'ssid' and 'password' at a minimum"""
+    )
     raise
 
 # Set to the twitter username you'd like to fetch tweets from
-TWITTER_USERNAME = 'ap'
+TWITTER_USERNAME = "adafruit"
 
 # Set to the amount of time to deep sleep for, in minutes
 SLEEP_TIME = 15
 
 # Set up where we'll be fetching data from
-DATA_SOURCE="https://api.twitter.com/1.1/statuses/user_timeline.json? \
-                screen_name=%s&count=1&tweet_mode=extended"%TWITTER_USERNAME
-TWEET_TEXT = [0, 'full_text']
-TWEET_FULL_NAME = [0, 'user', 'name']
-TWEET_HANDLE = [0, 'user', 'screen_name']
-
-magtag = MagTag(
-    url=DATA_SOURCE,
-    json_path=(TWEET_FULL_NAME, TWEET_HANDLE, TWEET_TEXT)
+DATA_SOURCE = (
+    "https://api.twitter.com/1.1/statuses/user_timeline.json? \
+                screen_name=%s&count=1&tweet_mode=extended"
+    % TWITTER_USERNAME
 )
+TWEET_TEXT = [0, "full_text"]
+TWEET_FULL_NAME = [0, "user", "name"]
+TWEET_HANDLE = [0, "user", "screen_name"]
+
+magtag = MagTag(url=DATA_SOURCE, json_path=(TWEET_FULL_NAME, TWEET_HANDLE, TWEET_TEXT))
 # Set Twitter OAuth2.0 Bearer Token
-bearer_token = secrets['twitter_bearer_token']
-magtag.set_headers({'Authorization': 'Bearer ' + bearer_token})
+bearer_token = secrets["twitter_bearer_token"]
+magtag.set_headers({"Authorization": "Bearer " + bearer_token})
 
 # Display setup
 magtag.set_background("/images/background.bmp")
@@ -46,7 +48,7 @@ magtag.add_text(
 magtag.add_text(
     text_position=(70, 30),
     text_font="/fonts/Arial-12.bdf",
-    text_transform=lambda x: "@%s"%x,
+    text_transform=lambda x: "@%s" % x,
 )
 
 # Tweet text
@@ -56,7 +58,7 @@ magtag.add_text(
     text_maxlen=160,
     text_position=(
         5,
-        (magtag.graphics.display.height // 2)+20,
+        (magtag.graphics.display.height // 2) + 20,
     ),
     line_spacing=0.75,
 )
