5f6ab8eec78725dbd0aceec1c499460d5966adf8
Noe Ruiz <noe@adafruit.com>
2021-02-04 13:51:21 -0500

    Adding code for QTPy Heart
    code for QTPy heart

---
+++ QTPy_Heart/code.py
@@ -0,0 +1,16 @@
+import board
+import neopixel
+from adafruit_led_animation.animation.pulse import Pulse
+from adafruit_led_animation.color import RED
+
+# Update to match the pin connected to your NeoPixels
+pixel_pin = board.D1
+# Update to match the number of NeoPixels you have connected
+pixel_num = 6
+
+pixels = neopixel.NeoPixel(pixel_pin, pixel_num, brightness=0.5, auto_write=False)
+
+pulse = Pulse(pixels, speed=0.01, color=RED, period=1)
+
+while True:
+    pulse.animate()
