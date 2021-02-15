809c03187e6265ceb28aef90bf593f82da46c188
John Park <jp@jpixl.net>
2021-02-15 10:52:15 -0800

    pylint fix for line length and 'i' variable

--- Pico_RP2040_Solenoid/pico-solenoid.py
+++ Pico_RP2040_Solenoid/pico-solenoid.py
@@ -1,7 +1,8 @@
 # SPDX-FileCopyrightText: 2021 jedgarpark for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
-# This example uses an MOSFET transistor circuit to drive a solenoid from a Pico RP2040 digitalio pin
+# This example uses an MOSFET transistor circuit to drive a solenoid from a
+# Pico RP2040 digitalio pin
 # Hardware setup:
 #   Button on GP3 to gnd (uses internal pull up)
 #   MOSFET driving solenoid on GP14 w protection diode and 47uF capacitor across power rails
@@ -36,13 +37,13 @@ solenoid = DigitalInOut(board.GP14)  #  pin drives a MOSFET
 solenoid.direction = Direction.OUTPUT
 solenoid.value = False
 
-strike_time = 0.05  # adjust for coil on time  range ~0.05 - ? (beware heat/power drain beyond a few seconds)
+strike_time = 0.05  # coil on time range ~0.05 - ? beware heat/power drain beyond a few seconds)
 recover_time = 0.20  # adjust for coil off time/pause between strikes
 
 
 def solenoid_strike(loops):  # solenoid strike function
     print("solenoid test")
-    for i in range(loops):
+    for _ in range(loops):
         solenoid.value = True
         time.sleep(strike_time)
         solenoid.value = False
