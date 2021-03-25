5aab2b0bab6ed9323c137ca97437438033680e00
brentru <robots199@me.com>
2021-03-25 14:59:05 -0400

    fixup files, add pico wiring for uart echo script!

--- rp2040_ble_airlift/ble-bluefruit-color-picker-rp2040.py
+++ rp2040_ble_airlift/ble-bluefruit-color-picker-rp2040.py
@@ -1,11 +1,22 @@
 import board
+import neopixel
+
+from adafruit_bluefruit_connect.packet import Packet
+from adafruit_bluefruit_connect.color_packet import ColorPacket
+
+
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
+
 from adafruit_airlift.esp32 import ESP32
 
+
 # If you are using an AirLift FeatherWing or AirLift Bitsy Add-On,
-# use the pin settings below:
+# use the pin settings below.
+# If you are using an AirLift Breakout, check that these
+# choices match the wiring to your microcontroller board,
+# or change them as appropriate.
 esp32 = ESP32(
     reset=board.D12,
     gpio0=board.D10,
@@ -15,33 +26,23 @@ esp32 = ESP32(
     rx=board.RX,
 )
 
-# If you are using an AirLift Breakout, comment out the DEFAULT lines
-# above and uncomment the lines below:
-# esp32 = ESP32(
-#     reset=board.D12,
-#     gpio0=board.D10,
-#     busy=board.D11,
-#     chip_select=board.D13,
-#     tx=board.TX,
-#     rx=board.RX,
-# )
-
-
 adapter = esp32.start_bluetooth()
 
 ble = BLERadio(adapter)
-uart = UARTService()
-advertisement = ProvideServicesAdvertisement(uart)
+uart_service = UARTService()
+advertisement = ProvideServicesAdvertisement(uart_service)
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.1)
 
 while True:
+    # Advertise when not connected.
     ble.start_advertising(advertisement)
-    print("waiting to connect")
     while not ble.connected:
         pass
-    print("connected: trying to read input")
+
     while ble.connected:
-        # Returns b'' if nothing was read.
-        one_byte = uart.read(1)
-        if one_byte:
-            print(one_byte)
-            uart.write(one_byte)
+        if uart_service.in_waiting:
+            packet = Packet.from_stream(uart_service)
+            if isinstance(packet, ColorPacket):
+                print(packet.color)
+                pixels.fill(packet.color)
---
+++ rp2040_ble_airlift/ble-bluefruit-uart-echo-rp2040.py
@@ -0,0 +1,47 @@
+import board
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
+from adafruit_airlift.esp32 import ESP32
+
+# If you are using an AirLift FeatherWing or AirLift Bitsy Add-On,
+# use the pin settings below:
+esp32 = ESP32(
+    reset=board.D12,
+    gpio0=board.D10,
+    busy=board.D11,
+    chip_select=board.D13,
+    tx=board.TX,
+    rx=board.RX,
+)
+
+# If you are using an AirLift Breakout, comment out the DEFAULT lines
+# above and uncomment the lines below:
+# esp32 = ESP32(
+#    reset=board.GP16,
+#    gpio0=board.GP9,
+#    busy=board.GP14,
+#    chip_select=board.GP13,
+#    tx=board.GP0,
+#    rx=board.GP1,
+# )
+
+
+adapter = esp32.start_bluetooth()
+
+ble = BLERadio(adapter)
+uart = UARTService()
+advertisement = ProvideServicesAdvertisement(uart)
+
+while True:
+    ble.start_advertising(advertisement)
+    print("waiting to connect")
+    while not ble.connected:
+        pass
+    print("connected: trying to read input")
+    while ble.connected:
+        # Returns b'' if nothing was read.
+        one_byte = uart.read(1)
+        if one_byte:
+            print(one_byte)
+            uart.write(one_byte)
