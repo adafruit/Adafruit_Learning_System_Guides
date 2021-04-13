f51f0a4e7ad5e07de58989bf6579a9cc5182a344
Kattni Rembor <kattni@adafruit.com>
2021-04-13 19:02:54 -0400

    CircuitPython Neo Trinkey examples.

---
+++ Adafruit_Neo_Trinkey/cap_touch_hid.py
@@ -0,0 +1,25 @@
+"""CircuitPython Capacitive Touch HID Example for Neo Trinkey"""
+import time
+import board
+import touchio
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)
+
+touch1 = touchio.TouchIn(board.TOUCH1)
+touch2 = touchio.TouchIn(board.TOUCH2)
+
+while True:
+    if touch1.value:  # If touch pad 1 is touched...
+        while touch1.value:  # Wait for release...
+            time.sleep(0.1)
+        keyboard.send(Keycode.SHIFT, Keycode.A)  # Then send key press.
+
+    if touch2.value:  # If touch pad 2 is touched...
+        while touch2.value:  # Wait for release...
+            time.sleep(0.1)
+        keyboard_layout.write("Hello World!\n")  # Then send string.
