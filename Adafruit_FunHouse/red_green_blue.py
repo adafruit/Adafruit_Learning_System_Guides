9bdeefd0ab9b3d449fb966ff4b5fb2f5b6803819
Kattni Rembor <kattni@adafruit.com>
2021-04-28 10:44:11 -0400

    Template code and FunHouse examples.

---
+++ Adafruit_FunHouse/red_green_blue.py
@@ -0,0 +1,15 @@
+"""CircuitPython DotStar red, green, blue example for FunHouse"""
+import time
+import board
+import adafruit_dotstar
+
+dots = adafruit_dotstar.DotStar(board.DOTSTAR_CLOCK, board.DOTSTAR_DATA, 5)
+dots.brightness = 0.3
+
+while True:
+    dots.fill((255, 0, 0))
+    time.sleep(0.5)
+    dots.fill((0, 255, 0))
+    time.sleep(0.5)
+    dots.fill((0, 0, 255))
+    time.sleep(0.5)
