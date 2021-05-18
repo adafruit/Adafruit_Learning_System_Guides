b5c6c949ad5be4d85edc37064ae5e4a4e8449471
Anne Barela <1911920+TheKitty@users.noreply.github.com>
2021-05-18 15:00:02 -0400

    Create lowlight.py
    Code for guide https://learn.adafruit.com/low-light-long-exposure-photography/

---
+++ RasPi_Low_Light_Camera/lowlight.py
@@ -0,0 +1,22 @@
+from picamera import PiCamera
+import time
+from fractions import Fraction
+import datetime
+
+cur_time = datetime.datetime.now()
+stub = cur_time.strftime("%Y%m%d%H%M_low")
+
+camera = PiCamera(framerate=Fraction(1,6))
+
+# You can change these as needed. Six seconds (6000000)
+# is the max for shutter speed and 800 is the max for ISO.
+camera.shutter_speed = 1750000
+camera.iso = 800
+
+time.sleep(30)
+camera.exposure_mode = 'off'
+
+outfile = "%s.jpg" % (stub)
+camera.capture(outfile)
+
+camera.close()
