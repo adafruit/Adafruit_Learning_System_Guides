b011bab11366d5be9baa44daf06508ed5910198f
BlitzCityDIY <blitzcitydiy@gmail.com>
2021-06-02 15:22:23 -0400

    Adding examples for Arduino RP2040
    Adding example code for the Arduino Nano RP2040 Connect: blinking onboard LED, digital input, PDM mic and wifi

---
+++ Arduino_Nano_RP2040_Connect/arduino_nano_rp2040_connect_wifi.py
@@ -0,0 +1,65 @@
+'''Adapted from the Adafruit_CircuitPython_ESP32SPI
+library example esp32spi_simpletest.py:
+https://github.com/adafruit/Adafruit_CircuitPython_ESP32SPI/
+blob/master/examples/esp32spi_simpletest.py '''
+
+import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_requests as requests
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+print("Arduino Nano RP2040 Connect webclient test")
+
+TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
+
+#  ESP32 pins
+esp32_cs = DigitalInOut(board.CS1)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+
+#  uses the secondary SPI connected through the ESP32
+spi = busio.SPI(board.SCK1, board.MOSI1, board.MISO1)
+
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+requests.set_socket(socket, esp)
+
+if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
+    print("ESP32 found and in idle mode")
+print("Firmware vers.", esp.firmware_version)
+print("MAC addr:", [hex(i) for i in esp.MAC_address])
+
+for ap in esp.scan_networks():
+    print("\t%s\t\tRSSI: %d" % (str(ap['ssid'], 'utf-8'), ap['rssi']))
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+print("My IP address is", esp.pretty_ip(esp.ip_address))
+
+print(
+    "IP lookup adafruit.com: %s" % esp.pretty_ip(esp.get_host_by_name("adafruit.com"))
+)
+
+print("Fetching text from", TEXT_URL)
+r = requests.get(TEXT_URL)
+print("-" * 40)
+print(r.text)
+print("-" * 40)
+r.close()
+
+print("Done!")
