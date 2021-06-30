f3e726f17a51d1df19a0e2aa35c20e2c56c59a11
Kattni Rembor <kattni@adafruit.com>
2021-06-30 15:41:39 -0400

    Adding MacroPad code.

---
+++ Adafruit_MacroPad/Digital_Input/code.py
@@ -0,0 +1,15 @@
+"""CircuitPython Digital Input example for MacroPad"""
+import board
+import digitalio
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+button = digitalio.DigitalInOut(board.BUTTON)
+button.switch_to_input(pull=digitalio.Pull.UP)
+
+while True:
+    if not button.value:
+        led.value = True
+    else:
+        led.value = False
