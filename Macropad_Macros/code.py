e3b50fc0521270d155f9727a2a2957e191cfb444
Phillip Burgess <paintyourdragon@dslextreme.com>
2021-07-02 14:27:21 -0700

    Initial pylintage and move macro files into folder

--- Macropad_Macros/code.py
+++ Macropad_Macros/code.py
@@ -1,52 +1,43 @@
+"""
+Add description here
+"""
+
+# pylint: disable=import-error, unused-import, too-few-public-methods, eval-used
+
+import json
+import os
 import time
 import board
 import digitalio
-import rotaryio
-import neopixel
-import json
-import os
-
 import displayio
+import neopixel
+import rotaryio
 import terminalio
-from adafruit_display_text import label
-
 import usb_hid
+from adafruit_display_text import label
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-#from adafruit_hid.mouse import Mouse
-#from adafruit_hid.consumer_control import ConsumerControl
-#from adafruit_hid.consumer_control_code import ConsumerControlCode
+# CONFIGURABLES ------------------------
 
-MACRO_FOLDER = '/'
+MACRO_FOLDER = '/macros'
 
-DISPLAY = board.DISPLAY
-ENCODER = rotaryio.IncrementalEncoder(board.ENCODER_B, board.ENCODER_A)
-PIXELS = neopixel.NeoPixel(board.NEOPIXEL, 12, auto_write=False)
-KEYBOARD = Keyboard(usb_hid.devices)
+# CLASSES AND FUNCTIONS ----------------
 
-group = displayio.Group(max_size=13)
-text_area = label.Label(terminalio.FONT, text='M', color=0xFFFFFF, x=DISPLAY.width//2, y=0, anchor_point=(0.5, 0.0), max_glyphs=30)
-group.append(text_area)
-# Use baseline alignment for these!
-#labels = []
-#for i in range(13):
-#    labels.append = label.Label(terminalio.FONT, text='M', color=0xFFFFFF, x=0, y=0, max_glyphs=15)
-
-DISPLAY.show(group)
-
-class key:
+class Key:
+    """ Add class doccstring here"""
     DEBOUNCE_TIME = 1 / 50
 
     def __init__(self, keyname):
-        self.io = digitalio.DigitalInOut(keyname)
-        self.io.direction = digitalio.Direction.INPUT
-        self.io.pull = digitalio.Pull.UP
-        self.last_value = self.io.value # Initial state
+        self.pin = digitalio.DigitalInOut(keyname)
+        self.pin.direction = digitalio.Direction.INPUT
+        self.pin.pull = digitalio.Pull.UP
+        self.last_value = self.pin.value # Initial state
         self.last_time = time.monotonic()
 
     def debounce(self):
-        value = self.io.value
+        """ Add function docstring here """
+        value = self.pin.value
         if value != self.last_value:
             now = time.monotonic()
             elapsed = now - self.last_time
@@ -56,14 +47,8 @@ class key:
                 return value
         return None
 
-KEYS = []
-for keyname in (board.KEY1, board.KEY2, board.KEY3, board.KEY4, board.KEY5,
-                board.KEY6, board.KEY7, board.KEY8, board.KEY9, board.KEY10,
-                board.KEY11, board.KEY12, board.ENCODER_SWITCH):
-    KEYS.append(key(keyname))
-
-
-class macro:
+class Macro:
+    """ Add class doccstring here"""
     def __init__(self, desc, color, sequence):
         self.desc = desc
         self.color = eval(color)
@@ -74,69 +59,96 @@ class macro:
                 self.in_order = True
                 break
 
-class app:
+class App:
+    """ Add class doccstring here"""
     def __init__(self, filename):
-        with open(filename) as jsonfile:
+        with open(MACRO_FOLDER + '/' + filename) as jsonfile:
             json_data = json.load(jsonfile)
             self.name = json_data['name']
             default_color = json_data['color'] if 'color' in json_data else '0'
             self.macros = []
             for mac in json_data['macros']:
-                self.macros.append(macro(
+                self.macros.append(Macro(
                     mac['desc'] if 'desc' in mac else None,
                     mac['color'] if 'color' in mac else default_color,
                     mac['sequence'] if 'sequence' in mac else None))
 
     def switch(self):
-        # Set up LED colors
-        PIXELS.fill(0)
-        for i, mac in enumerate(self.macros):
-            PIXELS[i] = mac.color
+        """ Add function docstring here """
+        GROUP[12].text = self.name
+        for i in range(12):
+            if i < len(self.macros):
+                PIXELS[i] = self.macros[i].color
+                GROUP[i].text = self.macros[i].desc
+            else:
+                PIXELS[i] = 0
+                GROUP[i].text = ''
         PIXELS.show()
-        # Set up screen
-        text_area.text = self.name
-
-
-APPS = []
-files = os.listdir(MACRO_FOLDER)
-files.sort()
-for filename in files:
-    if filename.endswith('.json'):
-        APPS.append(app(filename))
-
-if not len(APPS):
-    print('No valid macro files found')
-    while True:
-        pass
 
 # Convert key code name (e.g. "COMMAND") to a numeric value for press/release
 def code(name):
+    """ Add function doccstring here"""
     return eval('Keycode.' + name.upper())
+
+# INITIALIZATION -----------------------
+
+DISPLAY = board.DISPLAY
+ENCODER = rotaryio.IncrementalEncoder(board.ENCODER_B, board.ENCODER_A)
+PIXELS = neopixel.NeoPixel(board.NEOPIXEL, 12, auto_write=False)
+KEYBOARD = Keyboard(usb_hid.devices)
+
+GROUP = displayio.Group(max_size=13)
+for KEY_INDEX in range(12):
+    x = KEY_INDEX % 3
+    y = KEY_INDEX // 3
+    GROUP.append(label.Label(terminalio.FONT, text='', color=0xFFFFFF,
+                             anchored_position=((DISPLAY.width - 1) * x / 2,
+                                                DISPLAY.height - 1 -
+                                                (3 - y) * 11),
+                             anchor_point=(x / 2, 1.0), max_glyphs=15))
+GROUP.append(label.Label(terminalio.FONT, text='', color=0xFFFFFF,
+                         anchored_position=(DISPLAY.width//2, 0),
+                         anchor_point=(0.5, 0.0), max_glyphs=30))
+DISPLAY.show(GROUP)
+
+KEYS = []
+for pin in (board.KEY1, board.KEY2, board.KEY3, board.KEY4, board.KEY5,
+            board.KEY6, board.KEY7, board.KEY8, board.KEY9, board.KEY10,
+            board.KEY11, board.KEY12, board.ENCODER_SWITCH):
+    KEYS.append(Key(pin))
+
+APPS = []
+FILES = os.listdir(MACRO_FOLDER)
+FILES.sort()
+for FILENAME in FILES:
+    if FILENAME.endswith('.json'):
+        APPS.append(App(FILENAME))
+
+if not APPS:
+    print('No valid macro files found')
+    while True:
+        pass
 
 LAST_POSITION = None
 APP_INDEX = 0
 APPS[APP_INDEX].switch()
 
+# MAIN LOOP ----------------------------
+
 while True:
-    position = ENCODER.position
-    if position != LAST_POSITION:
-        APP_INDEX = position % len(APPS)
+    POSITION = ENCODER.position
+    if POSITION != LAST_POSITION:
+        APP_INDEX = POSITION % len(APPS)
         APPS[APP_INDEX].switch()
-        LAST_POSITION = position
+        LAST_POSITION = POSITION
 
-    for i, key in enumerate(KEYS):
-        action = key.debounce()
+    for KEY_INDEX, KEY in enumerate(KEYS[0: len(APPS[APP_INDEX].macros)]):
+        action = KEY.debounce()
         if action is not None:
-            print(i, len(APPS[APP_INDEX].macros))
-
-            if i >= len(APPS[APP_INDEX].macros):
-                continue # Ignore if key # exceeds macro list length
-
-            keys = APPS[APP_INDEX].macros[i].sequence
+            keys = APPS[APP_INDEX].macros[KEY_INDEX].sequence
             if action is False: # Macro key pressed
-                print('Press', i)
-                if APPS[APP_INDEX].macros[i].in_order:
-                    for x in APPS[APP_INDEX].macros[i].sequence:
+                if APPS[APP_INDEX].macros[KEY_INDEX].in_order:
+                    for x in APPS[APP_INDEX].macros[KEY_INDEX].sequence:
                         if x.startswith('+'):   # Press and hold key
                             KEYBOARD.press(code(x[1:]))
                         elif x.startswith('-'): # Release key
@@ -145,27 +157,12 @@ while True:
                             KEYBOARD.press(code(x))
                             KEYBOARD.release(code(x))
                 else:
-                    for x in APPS[APP_INDEX].macros[i].sequence:
+                    for x in APPS[APP_INDEX].macros[KEY_INDEX].sequence:
                         KEYBOARD.press(code(x))
             elif action is True: # Macro key released
-                print('Release', i)
                 # Release all keys in reverse order
-                for x in reversed(APPS[APP_INDEX].macros[i].sequence):
+                for x in reversed(APPS[APP_INDEX].macros[KEY_INDEX].sequence):
                     if x.startswith('+') or x.startswith('-'):
                         KEYBOARD.release(code(x[1:]))
                     else:
                         KEYBOARD.release(code(x))
-
-
-META = ('LEFT_CONTROL', 'CONTROL', 'LEFT_SHIFT', 'SHIFT', 'LEFT_ALT', 'ALT',
-        'OPTION', 'LEFT_GUI', 'GUI', 'WINDOWS', 'COMMAND', 'RIGHT_CONTROL',
-        'RIGHT_SHIFT', 'RIGHT_ALT', 'RIGHT_GUI', 'modifier_bit')
-
-#>>> print(dir(board))
-#['__class__', 'BUTTON', 'DISPLAY', 'ENCODER_A', 'ENCODER_B', 'ENCODER_SWITCH', 'I2C', 'KEY1', 'KEY10', 'KEY11', 'KEY12', 'KEY2', 'KEY3', 'KEY4', 'KEY5', 'KEY6', 'KEY7', 'KEY8', 'KEY9', 'LED', 'MISO', 'MOSI', 'NEOPIXEL', 'OLED_CS', 'OLED_DC', 'OLED_RESET', 'ROTA', 'ROTB', 'SCK', 'SCL', 'SDA', 'SPEAKER', 'SPEAKER_SHUTDOWN', 'SPI']
-
-#>>> print(dir(Keycode))
-#['__class__', '__module__', '__name__', '__qualname__', '__bases__', '__dict__', 'C', 'M', 'A', 'B', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE', 'ZERO', 'ENTER', 'RETURN', 'ESCAPE', 'BACKSPACE', 'TAB', 'SPACEBAR', 'SPACE', 'MINUS', 'EQUALS', 'LEFT_BRACKET', 'RIGHT_BRACKET', 'BACKSLASH', 'POUND', 'SEMICOLON', 'QUOTE', 'GRAVE_ACCENT', 'COMMA', 'PERIOD', 'FORWARD_SLASH', 'CAPS_LOCK', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'PRINT_SCREEN', 'SCROLL_LOCK', 'PAUSE', 'INSERT', 'HOME', 'PAGE_UP', 'DELETE', 'END', 'PAGE_DOWN', 'RIGHT_ARROW', 'LEFT_ARROW', 'DOWN_ARROW', 'UP_ARROW', 'KEYPAD_NUMLOCK', 'KEYPAD_FORWARD_SLASH', 'KEYPAD_ASTERISK', 'KEYPAD_MINUS', 'KEYPAD_PLUS', 'KEYPAD_ENTER', 'KEYPAD_ONE', 'KEYPAD_TWO', 'KEYPAD_THREE', 'KEYPAD_FOUR', 'KEYPAD_FIVE', 'KEYPAD_SIX', 'KEYPAD_SEVEN', 'KEYPAD_EIGHT', 'KEYPAD_NINE', 'KEYPAD_ZERO', 'KEYPAD_PERIOD', 'KEYPAD_BACKSLASH', 'APPLICATION', 'POWER', 'KEYPAD_EQUALS', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'LEFT_CONTROL', 'CONTROL', 'LEFT_SHIFT', 'SHIFT', 'LEFT_ALT', 'ALT', 'OPTION', 'LEFT_GUI', 'GUI', 'WINDOWS', 'COMMAND', 'RIGHT_CONTROL', 'RIGHT_SHIFT', 'RIGHT_ALT', 'RIGHT_GUI', 'modifier_bit']
-
-#>>> print(eval('Keycode.' + 'COMMAND'))
-
