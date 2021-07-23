60ad89c3a378a7873fd5093ae5809a158f40e824
James Carr <lesamouraipourpre@gmail.com>
2021-07-23 10:49:53 +0100

    Ran black

--- CircuitPython_Sip_and_Puff/simple_hid.py
+++ CircuitPython_Sip_and_Puff/simple_hid.py
@@ -7,20 +7,23 @@ kbd = Keyboard(usb_hid.devices)
 
 detector = puff_detector.PuffDetector()
 
+
 @detector.on_sip
-def on_sip(strength, duration):#pylint:disable=unused-argument
+def on_sip(strength, duration):  # pylint:disable=unused-argument
     if strength == puff_detector.STRONG:
         kbd.send(Keycode.LEFT_ARROW)
         kbd.send(Keycode.LEFT_ARROW)
     if strength == puff_detector.SOFT:
         kbd.send(Keycode.LEFT_ARROW)
 
+
 @detector.on_puff
-def on_puff(strength, duration):#pylint:disable=unused-argument
+def on_puff(strength, duration):  # pylint:disable=unused-argument
     if strength == puff_detector.STRONG:
         kbd.send(Keycode.RIGHT_ARROW)
         kbd.send(Keycode.RIGHT_ARROW)
     if strength == puff_detector.SOFT:
         kbd.send(Keycode.RIGHT_ARROW)
+
 
 detector.run()
