b4218cb183a06262c57b6cc748f6d3fc49a0ffc1
Henry Gabryjelski <henrygab@users.noreply.github.com>
2021-08-19 10:08:27 -0700

    fix spacing from de-indentation

--- Macropad_Hotkeys/code.py
+++ Macropad_Hotkeys/code.py
@@ -59,14 +59,14 @@ for key_index in range(12):
     x = key_index % 3
     y = key_index // 3
     group.append(label.Label(terminalio.FONT, text='', color=0xFFFFFF,
-                            anchored_position=((macropad.display.width - 1) * x / 2,
+                             anchored_position=((macropad.display.width - 1) * x / 2,
                                                 macropad.display.height - 1 -
                                                 (3 - y) * 12),
-                            anchor_point=(x / 2, 1.0)))
+                             anchor_point=(x / 2, 1.0)))
 group.append(Rect(0, 0, macropad.display.width, 12, fill=0xFFFFFF))
 group.append(label.Label(terminalio.FONT, text='', color=0x000000,
-                        anchored_position=(macropad.display.width//2, -2),
-                        anchor_point=(0.5, 0.0)))
+                         anchored_position=(macropad.display.width//2, -2),
+                         anchor_point=(0.5, 0.0)))
 macropad.display.show(group)
 
 # Load all the macro key setups from .py files in MACRO_FOLDER
