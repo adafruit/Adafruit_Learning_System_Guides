fe3a2456603bff38a091da1e8a1f07cd90dfbe37
dherrada <dylan.herrada@adafruit.com>
2021-08-20 13:46:30 -0400

    Linted

--- CircuitPython_Made_Easy_On_CPX/cpx_acceleration_neopixels/code.py
+++ CircuitPython_Made_Easy_On_CPX/cpx_acceleration_neopixels/code.py
@@ -7,16 +7,15 @@ while True:
         print("Slide switch off!")
         cpx.pixels.fill((0, 0, 0))
         continue
-    else:
-        R = 0
-        G = 0
-        B = 0
-        x, y, z = cpx.acceleration
-        print((x, y, z))
-        if x:
-            R = R + abs(int(x))
-        if y:
-            G = G + abs(int(y))
-        if z:
-            B = B + abs(int(z))
-        cpx.pixels.fill((R, G, B))
+    R = 0
+    G = 0
+    B = 0
+    x, y, z = cpx.acceleration
+    print((x, y, z))
+    if x:
+        R = R + abs(int(x))
+    if y:
+        G = G + abs(int(y))
+    if z:
+        B = B + abs(int(z))
+    cpx.pixels.fill((R, G, B))
