f7bf9c0060e3f46009c1e8699dc132f05b0930a2
Kattni Rembor <kattni@adafruit.com>
2021-08-26 11:34:29 -0400

    Adding Pico MP3 files.

---
+++ MP3_Playback_RP2040/Pico_Single_File/code.py
@@ -0,0 +1,16 @@
+"""
+CircuitPython single MP3 playback example. Plays a single MP3 once.
+"""
+import board
+import audiomp3
+import audiopwmio
+
+audio = audiopwmio.PWMAudioOut(board.GP0)
+
+decoder = audiomp3.MP3Decoder(open("slow.mp3", "rb"))
+
+audio.play(decoder)
+while audio.playing:
+    pass
+
+print("Done playing!")
