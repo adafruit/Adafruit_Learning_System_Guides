add9186386ef0194933694993d8bb0c027c5428f
Jeff Epler <jepler@unpythonic.net>
2021-09-28 11:44:04 -0500

    Blacken & fix pylint

--- CircuitPython_BLEThermalPrinter/code.py
+++ CircuitPython_BLEThermalPrinter/code.py
@@ -1,16 +1,9 @@
-import seekablebitmap
-
-import gc
 import os
-import struct
-import time
 
 import board
-import digitalio
 import keypad
 import ulab.numpy as np
 
-import adafruit_ble
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising import Advertisement
 
@@ -21,41 +14,6 @@ ble = BLERadio()  # pylint: disable=no-member
 
 buttons = keypad.Keys([board.BUTTON_A, board.BUTTON_B], value_when_pressed=False)
 
-def pnmopen(filename):
-    """
-    Scan for netpbm format info, skip over comments, and read header data.
-
-    Return the format, header, and the opened file positioned at the start of
-    the bitmap data.
-    """
-    # pylint: disable=too-many-branches
-    image_file = open(filename, "rb")
-    magic_number = image_file.read(2)
-    image_file.seek(2)
-    pnm_header = []
-    next_value = bytearray()
-    while True:
-        # We have all we need at length 3 for formats P2, P3, P5, P6
-        if len(pnm_header) == 3:
-            return image_file, magic_number, pnm_header
-
-        if len(pnm_header) == 2 and magic_number in [b"P1", b"P4"]:
-            return image_file, magic_number, pnm_header
-
-        next_byte = image_file.read(1)
-        if next_byte == b"":
-            raise RuntimeError("Unsupported image format {}".format(magic_number))
-        if next_byte == b"#":  # comment found, seek until a newline or EOF is found
-            while image_file.read(1) not in [b"", b"\n"]:  # EOF or NL
-                pass
-        elif not next_byte.isdigit():  # boundary found in header data
-            if next_value:
-                # pull values until space is found
-                pnm_header.append(int("".join(["%c" % char for char in next_value])))
-                next_value = bytearray()  # reset the byte array
-        else:
-            next_value += next_byte  # push the digit into the byte array
-
 def wait_for_press(kbd):
     """
     Wait for a keypress and return the event
@@ -64,6 +22,7 @@ def wait_for_press(kbd):
         event = kbd.events.get()
         if event and event.pressed:
             return event
+
 
 def show(s):
     """
@@ -74,12 +33,14 @@ def show(s):
     print(s)
     board.DISPLAY.auto_refresh = True
 
+
 def show_error(s):
     """
     Display a message on the screen and wait for a button press
     """
     show(s + "\nPress a button to continue")
     wait_for_press(buttons)
+
 
 def find_cat_printer(radio):
     """
@@ -88,29 +49,38 @@ def find_cat_printer(radio):
     while True:
         show("Scanning for GB02 device...")
         for adv in radio.start_scan(Advertisement):
-            complete_name = getattr(adv, 'complete_name')
+            complete_name = getattr(adv, "complete_name")
             if complete_name is not None:
                 print(f"Saw {complete_name}")
-            if complete_name == 'GB02':
+            if complete_name == "GB02":
                 radio.stop_scan()
                 return radio.connect(adv, timeout=10)[CatPrinter]
 
-image_files = [i for i in os.listdir('/') if i.lower().endswith(".pbm") or i.lower().endswith(".bmp")]
+
+image_files = [
+    i
+    for i in os.listdir("/")
+    if i.lower().endswith(".pbm") or i.lower().endswith(".bmp")
+]
 image_files.sort(key=lambda filename: filename.lower())
+
 
 def select_image():
     i = 0
     while True:
-        show(f"Select image file\nA: next image\nB: print this image\n\n{image_files[i]}")
+        show(
+            f"Select image file\nA: next image\nB: print this image\n\n{image_files[i]}"
+        )
         event = wait_for_press(buttons)
-        if event.key_number == 0: # button "A"
+        if event.key_number == 0:  # button "A"
             i = (i + 1) % len(image_files)
-        if event.key_number == 1: # button "B"
+        if event.key_number == 1:  # button "B"
             return image_files[i]
-    
+
+
 printer = find_cat_printer(ble)
 
-while True:
+def main():
     try:
         filename = select_image()
 
@@ -134,9 +104,11 @@ while True:
 
         # Print blank lines until the paper can be torn off
         for i in range(80):
-            printer.print_bitmap_row(b'\0' * 48)
+            printer.print_bitmap_row(b"\0" * 48)
 
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-except
         show_error(str(e))
         image_files.remove(filename)
-        continue
+
+while True:
+    main()
--- CircuitPython_BLEThermalPrinter/seekablebitmap.py
+++ CircuitPython_BLEThermalPrinter/seekablebitmap.py
@@ -1,13 +1,29 @@
 import struct
-import ulab
+
+# pylint: disable=consider-using-with
 
 class SeekableBitmap:
-    def __init__(self, image_file, width, height, bits_per_pixel, *, bytes_per_row=None, data_start=None, stride=None, palette=None):
+    """Allow random access to an uncompressed bitmap file on disk"""
+    def __init__(
+        self,
+        image_file,
+        width,
+        height,
+        bits_per_pixel,
+        *,
+        bytes_per_row=None,
+        data_start=None,
+        stride=None,
+        palette=None,
+    ):
+        """Construct a SeekableBitmap"""
         self.image_file = image_file
         self.width = width
         self.height = height
         self.bits_per_pixel = bits_per_pixel
-        self.bytes_per_row = bytes_per_row if bytes_per_row else (bits_per_pixel * width + 7) // 8
+        self.bytes_per_row = (
+            bytes_per_row if bytes_per_row else (bits_per_pixel * width + 7) // 8
+        )
         self.stride = stride if stride else self.bytes_per_row
         self.palette = palette
         self.data_start = data_start if data_start else image_file.tell()
@@ -15,6 +31,7 @@ class SeekableBitmap:
     def get_row(self, row):
         self.image_file.seek(self.data_start + row * self.stride)
         return self.image_file.read(self.bytes_per_row)
+
 
 def _pnmopen(filename):
     """
@@ -51,40 +68,71 @@ def _pnmopen(filename):
         else:
             next_value += next_byte  # push the digit into the byte array
 
+
 def pnmopen(filename):
+    """
+    Interpret netpbm format info and construct a SeekableBitmap
+    """
     image_file, magic_number, pnm_header = _pnmopen(filename)
-    if magic_number == b'P4':
-        return SeekableBitmap(image_file, pnm_header[0], pnm_header[1], 1, palette=b'\xff\xff\xff\x00\x00\x00\x00\x00')
-    if magic_number == b'P5':
-        return SeekableBitmap(image_file, pnm_header[0], pnm_header[1], pnm_header[2].bit_length())
-    if magic_number == b'P6':
-        return SeekableBitmap(image_file, pnm_header[0], pnm_header[1], 3*pnm_header[2].bit_length())
+    if magic_number == b"P4":
+        return SeekableBitmap(
+            image_file,
+            pnm_header[0],
+            pnm_header[1],
+            1,
+            palette=b"\xff\xff\xff\x00\x00\x00\x00\x00",
+        )
+    if magic_number == b"P5":
+        return SeekableBitmap(
+            image_file, pnm_header[0], pnm_header[1], pnm_header[2].bit_length()
+        )
+    if magic_number == b"P6":
+        return SeekableBitmap(
+            image_file, pnm_header[0], pnm_header[1], 3 * pnm_header[2].bit_length()
+        )
     raise ValueError(f"Unknown or unsupported magic number {magic_number}")
 
+
 def bmpopen(filename):
+    """
+    Interpret bmp format info and construct a SeekableBitmap
+    """
     image_file = open(filename, "rb")
 
     header = image_file.read(34)
 
-    data_start, header_size, width, height, planes, bits_per_pixel, method = struct.unpack("<10x4l2hl", header)
+    data_start, header_size, width, height, _, bits_per_pixel, _ = struct.unpack(
+        "<10x4l2hl", header
+    )
 
     bits_per_pixel = bits_per_pixel if bits_per_pixel != 0 else 1
 
-    mask_size = (12 if header_size < 56 else 16) if method == 3 else 0
     palette_start = header_size + 14
     image_file.seek(palette_start)
     palette = image_file.read(4 << bits_per_pixel)
 
-    stride=(bits_per_pixel*width+31)//32*4
+    stride = (bits_per_pixel * width + 31) // 32 * 4
     if height < 0:
         height = -height
     else:
         data_start = data_start + stride * (height - 1)
         stride = -stride
 
-    return SeekableBitmap(image_file, width, height, bits_per_pixel, data_start=data_start, stride=stride, palette=palette)
+    return SeekableBitmap(
+        image_file,
+        width,
+        height,
+        bits_per_pixel,
+        data_start=data_start,
+        stride=stride,
+        palette=palette,
+    )
+
 
 def imageopen(filename):
+    """
+    Open a bmp or pnm file as a seekable bitmap
+    """
     if filename.lower().endswith(".bmp"):
         return bmpopen(filename)
     return pnmopen(filename)
