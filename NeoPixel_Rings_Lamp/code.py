86219c6bd4219ad6f5c620e5c04cbe81982bb71e
Noe Ruiz <noe@adafruit.com>
2021-11-15 15:37:39 -0500

    neopixel ring code update
    neopixel ring code update

--- NeoPixel_Rings_Lamp/code.py
+++ NeoPixel_Rings_Lamp/code.py
@@ -9,8 +9,7 @@ from adafruit_led_animation.animation.rainbow import Rainbow
 from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
 from adafruit_led_animation.animation.rainbowcomet import RainbowComet
 from adafruit_led_animation.sequence import AnimationSequence
-from adafruit_led_animation.color import RED, PURPLE, AMBER, JADE
-from _pixelbuf import colorwheel
+from adafruit_led_animation.color import PURPLE
 
 # Update this to match the number of NeoPixel LEDs connected to your board.
 num_pixels = 124
@@ -24,7 +23,13 @@ rainbow_comet = RainbowComet(pixels, speed=.01, tail_length=20, bounce=True)
 pulse = Pulse(pixels, speed=.05, color=PURPLE, period=3)
 
 animations = AnimationSequence(
-    pulse, rainbow_sparkle, rainbow_comet, rainbow, advance_interval=5, auto_clear=True, random_order=False
+    pulse,
+    rainbow_sparkle,
+    rainbow_comet,
+    rainbow,
+    advance_interval=5,
+    auto_clear=True,
+    random_order=False
 )
 
 while True:
