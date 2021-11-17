0b4ebde8d0d5b77fdbb7f29bd76da40188a9aeef
caternuson <caternuson@gmail.com>
2021-11-17 12:46:51 -0800

    add code for busio setup

--- CircuitPython_Essentials/CircuitPython_I2C_Scan/code.py
+++ CircuitPython_Essentials/CircuitPython_I2C_Scan/code.py
@@ -1,4 +1,4 @@
-"""CircuitPython Essentials I2C Scan example"""
+"""CircuitPython I2C Device Address Scan"""
 # If you run this and it seems to hang, try manually unlocking
 # your I2C bus from the REPL with
 #  >>> import board
@@ -7,15 +7,23 @@
 import time
 import board
 
+# To use default I2C bus (most boards)
 i2c = board.I2C()
+
+# To create I2C bus on specific pins
+# import busio
+# i2c = busio.I2C(board.SCL1, board.SDA1)  # QT Py RP2040 STEMMA connector
+# i2c = busio.I2C(board.GP1, board.GP0)    # Pi Pico RP2040
 
 while not i2c.try_lock():
     pass
 
 try:
     while True:
-        print("I2C addresses found:", [hex(device_address)
-              for device_address in i2c.scan()])
+        print(
+            "I2C addresses found:",
+            [hex(device_address) for device_address in i2c.scan()],
+        )
         time.sleep(2)
 
 finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
--- CircuitPython_Templates/i2c_scan/code.py
+++ CircuitPython_Templates/i2c_scan/code.py
@@ -1,15 +1,29 @@
 """CircuitPython I2C Device Address Scan"""
+# If you run this and it seems to hang, try manually unlocking
+# your I2C bus from the REPL with
+#  >>> import board
+#  >>> board.I2C().unlock()
+
 import time
 import board
 
+# To use default I2C bus (most boards)
 i2c = board.I2C()
+
+# To create I2C bus on specific pins
+# import busio
+# i2c = busio.I2C(board.SCL1, board.SDA1)  # QT Py RP2040 STEMMA connector
+# i2c = busio.I2C(board.GP1, board.GP0)    # Pi Pico RP2040
 
 while not i2c.try_lock():
     pass
 
 try:
     while True:
-        print("I2C addresses found:", [hex(device_address) for device_address in i2c.scan()])
+        print(
+            "I2C addresses found:",
+            [hex(device_address) for device_address in i2c.scan()],
+        )
         time.sleep(2)
 
 finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
