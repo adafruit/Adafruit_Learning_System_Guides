d960ec116517b976286d1e14d995cdc39534ece4
lady ada <limor@ladyada.net>
2022-01-18 20:04:19 -0500

    ci fix

--- ESP32_S2_WiFi_Tests/JSONdemo/JSONdemo.ino
+++ ESP32_S2_WiFi_Tests/JSONdemo/JSONdemo.ino
@@ -81,13 +81,13 @@ void setup() {
   #endif
 
   printWifiStatus();
-  client.setInsecure(); // don't use a root cert
 }
 
 uint32_t bytes = 0;
 
 void loop() {
   WiFiClientSecure client;
+  client.setInsecure(); // don't use a root cert
 
   Serial.println("\nStarting connection to server...");
   #if defined(USE_OLED)
