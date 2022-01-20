126515017ddbd1ff10ea0c13c0a939f4fdc1a4cf
Kattni Rembor <kattni@adafruit.com>
2022-01-20 13:43:46 -0500

    Code for QT Py ESP, and template fixes.

---
+++ Adafruit_QT_Py_ESP32-S2/Storage/boot.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+# SPDX-License-Identifier: Unlicense
+"""
+CircuitPython Essentials Storage CP Filesystem boot.py file
+"""
+import time
+import board
+import digitalio
+import storage
+import neopixel
+
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+button = digitalio.DigitalInOut(board.BUTTON)
+button.switch_to_input(pull=digitalio.Pull.UP)
+
+# Turn the NeoPixel blue for one second to indicate when to press the boot button.
+pixel.fill((255, 255, 255))
+time.sleep(1)
+
+# If the button is connected to ground, the filesystem is writable by CircuitPython
+storage.remount("/", readonly=button.value)
