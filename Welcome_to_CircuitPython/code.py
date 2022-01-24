555b5f815bf98a79a3d4661a4a238b0cf06cb975
Kattni <kattni@adafruit.com>
2022-01-24 10:42:55 -0500

    Remove trailing whitespace.

--- Welcome_to_CircuitPython/code.py
+++ Welcome_to_CircuitPython/code.py
@@ -6,13 +6,13 @@
 Blink example for boards with ONLY a NeoPixel LED (e.g. without a built-in red LED).
 Includes QT Py and various Trinkeys.
 
-Requires two libraries from the Adafruit CircuitPython Library Bundle. 
-Download the bundle from circuitpython.org/libraries and copy the 
+Requires two libraries from the Adafruit CircuitPython Library Bundle.
+Download the bundle from circuitpython.org/libraries and copy the
 following files to your CIRCUITPY/lib folder:
 * neopixel.mpy
 * adafruit_pypixelbuf.mpy
 
-Once the libraries are copied, save this file as code.py to your CIRCUITPY 
+Once the libraries are copied, save this file as code.py to your CIRCUITPY
 drive to run it.
 """
 import time
