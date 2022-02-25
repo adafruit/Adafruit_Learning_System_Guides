7542a81f5cfa8eaa06c248d7779a702ccb32ed52
Kattni Rembor <kattni@adafruit.com>
2022-02-25 18:08:08 -0500

    Adafruit IO and LC709203 code.

---
+++ CircuitPython_Templates/adafruit_io_cpu_temp_neopixel_color/code.py
@@ -0,0 +1,84 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+import time
+import ssl
+import socketpool
+import wifi
+import board
+import neopixel
+import microcontroller
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
+from adafruit_io.adafruit_io import IO_MQTT
+
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi and Adafruit IO credentials are kept in secrets.py - please add them there!")
+    raise
+
+# Set your Adafruit IO Username and Key in secrets.py
+# (visit io.adafruit.com if you need to create an account,
+# or if you need your Adafruit IO key.)
+aio_username = secrets["aio_username"]
+aio_key = secrets["aio_key"]
+
+# WiFi
+print("Connecting to %s" % secrets["ssid"])
+wifi.radio.connect(secrets["ssid"], secrets["password"])
+print("Connected to %s!" % secrets["ssid"])
+
+# Initialize NeoPixel
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.3)
+
+
+# Define callback functions which will be called when certain events happen.
+# pylint: disable=unused-argument
+def connected(client):
+    print("Connected to Adafruit IO!  Listening for NeoPixel changes...")
+    # Subscribe to Adafruit IO feed called "neopixel"
+    client.subscribe("neopixel")
+
+
+# pylint: disable=unused-argument
+def message(client, feed_id, payload):
+    print("Feed {0} received new value: {1}".format(feed_id, payload))
+    if feed_id == "neopixel":
+        pixel.fill(int(payload[1:], 16))
+
+
+# Create a socket pool
+pool = socketpool.SocketPool(wifi.radio)
+
+# Initialize a new MQTT Client object
+mqtt_client = MQTT.MQTT(
+    broker="io.adafruit.com",
+    username=secrets["aio_username"],
+    password=secrets["aio_key"],
+    socket_pool=pool,
+    ssl_context=ssl.create_default_context(),
+)
+
+# Initialize Adafruit IO MQTT "helper"
+io = IO_MQTT(mqtt_client)
+
+# Set up the callback methods above
+io.on_connect = connected
+io.on_message = message
+
+# Connect the client to the MQTT broker.
+print("Connecting to Adafruit IO...")
+io.connect()
+
+last = 0
+while True:
+    # Explicitly pump the message loop.
+    io.loop()
+
+    # Obtain the CPU temperature.
+    temperature = "{:.2f}".format(microcontroller.cpu.temperature)
+
+    # Print and publish the CPU temperature every 10 seconds.
+    if (time.monotonic() - last) >= 10:
+        print("Current CPU temperature: {0} C".format(temperature))
+        io.publish("cpu-temperature", temperature)
+        last = time.monotonic()
