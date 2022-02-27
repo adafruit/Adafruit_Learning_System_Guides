5d1d6fa8cac43ac7011f118d7631af4f7f54a3bc
lady ada <limor@ladyada.net>
2022-02-26 22:04:58 -0500

    fix compile

--- Adafruit_ESP32_Arduino_Demos/ESP32_sleeptest/ESP32_sleeptest.ino
+++ Adafruit_ESP32_Arduino_Demos/ESP32_sleeptest/ESP32_sleeptest.ino
@@ -1,20 +1,18 @@
 #include <Adafruit_NeoPixel.h>
-Adafruit_NeoPixel pixel(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 
+#if defined(PIN_NEOPIXEL)
+  Adafruit_NeoPixel pixel(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+#endif
 
 void setup() {
   Serial.begin(115200);
 
   // Turn on any internal power switches for TFT, NeoPixels, I2C, etc!
   enableInternalPower();
-
-  pixel.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixel.setBrightness(20); // not so bright
 }
 
 void loop() {
-  pixel.setPixelColor(0, 0xFFFFFF);
-  pixel.show();
+  LEDon();
   delay(1000);
   
   disableInternalPower();
@@ -26,6 +24,22 @@ void loop() {
   esp_sleep_enable_timer_wakeup(1000000); // 1 sec
   esp_deep_sleep_start(); 
   // we never reach here
+}
+
+void LEDon() {
+#if defined(PIN_NEOPIXEL)
+  pixel.begin(); // INITIALIZE NeoPixel
+  pixel.setBrightness(20); // not so bright
+  pixel.setPixelColor(0, 0xFFFFFF);
+  pixel.show();
+#endif
+}
+
+void LEDoff() {
+#if defined(PIN_NEOPIXEL)
+  pixel.setPixelColor(0, 0x0);
+  pixel.show();
+#endif
 }
 
 void enableInternalPower() {
