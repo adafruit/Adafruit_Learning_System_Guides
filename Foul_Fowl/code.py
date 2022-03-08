049e0aa31b9e50a8728e7ea31a710b2003eff09c
dherrada <dylan.herrada@adafruit.com>
2022-03-08 14:21:52 -0500

    Added files to break/test SPDX check

--- Foul_Fowl/code.py
+++ Foul_Fowl/code.py
@@ -1,6 +1,6 @@
-# SPDX-FileCopyrightText: 2018 John Edgar Park for Adafruit Industries
+# SPDX-FileCopyrightText:2018 John Edgar Park for Adafruit Industries
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier MIT
 
 # Foul Fowl
 # Keystroke Injection Payload for Adafruit Gemma M0
