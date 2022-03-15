3e3b0d0f13ab7766e4e37cc8273391701709c237
Melissa LeBlanc-Williams <melissa@adafruit.com>
2022-03-15 14:47:55 -0700

    Updated PyPortal Google Calendar example to make better use of PyPortal library

--- PyPortal_Google_Calendar/authenticator/code.py
+++ PyPortal_Google_Calendar/authenticator/code.py
@@ -1,18 +1,10 @@
 # SPDX-FileCopyrightText: 2021 Brent Rubell, written for Adafruit Industries
 #
 # SPDX-License-Identifier: Unlicense
-import board
-import busio
-from digitalio import DigitalInOut
-import adafruit_esp32spi.adafruit_esp32spi_socket as socket
-from adafruit_esp32spi import adafruit_esp32spi
-import adafruit_requests as requests
-import displayio
 from adafruit_display_text.label import Label
 from adafruit_bitmap_font import bitmap_font
-import adafruit_miniqr
 from adafruit_oauth2 import OAuth2
-
+from adafruit_pyportal import Network, Graphics
 
 # Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
 # "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
@@ -24,25 +16,10 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-esp32_cs = DigitalInOut(board.ESP_CS)
-esp32_ready = DigitalInOut(board.ESP_BUSY)
-esp32_reset = DigitalInOut(board.ESP_RESET)
+network = Network()
+network.connect()
 
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
-
-print("Connecting to AP...")
-while not esp.is_connected:
-    try:
-        esp.connect_AP(secrets["ssid"], secrets["password"])
-    except RuntimeError as e:
-        print("could not connect to AP, retrying: ", e)
-        continue
-print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
-
-# Initialize a requests object with a socket and esp32spi interface
-socket.set_interface(esp)
-requests.set_socket(socket, esp)
+graphics = Graphics()
 
 # DisplayIO Setup
 # Set up fonts
@@ -53,49 +30,30 @@ glyphs = b"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-,.: "
 font_small.load_glyphs(glyphs)
 font_large.load_glyphs(glyphs)
 
-group_verification = displayio.Group()
 label_overview_text = Label(
     font_large, x=0, y=45, text="To authorize this device with Google:"
 )
-group_verification.append(label_overview_text)
+graphics.splash.append(label_overview_text)
 
 label_verification_url = Label(font_small, x=0, y=100, line_spacing=1)
-group_verification.append(label_verification_url)
+graphics.splash.append(label_verification_url)
 
 label_user_code = Label(font_small, x=0, y=150)
-group_verification.append(label_user_code)
+graphics.splash.append(label_user_code)
 
 label_qr_code = Label(font_small, x=0, y=190, text="Or scan the QR code:")
-group_verification.append(label_qr_code)
-
-
-### helper methods ###
-def bitmap_QR(matrix):
-    # monochome (2 color) palette
-    BORDER_PIXELS = 2
-
-    # bitmap the size of the screen, monochrome (2 colors)
-    bitmap = displayio.Bitmap(
-        matrix.width + 2 * BORDER_PIXELS, matrix.height + 2 * BORDER_PIXELS, 2
-    )
-    # raster the QR code
-    for y in range(matrix.height):  # each scanline in the height
-        for x in range(matrix.width):
-            if matrix[x, y]:
-                bitmap[x + BORDER_PIXELS, y + BORDER_PIXELS] = 1
-            else:
-                bitmap[x + BORDER_PIXELS, y + BORDER_PIXELS] = 0
-    return bitmap
-
+graphics.splash.append(label_qr_code)
 
 # Set scope(s) of access required by the API you're using
 scopes = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 # Initialize an oauth2 object
 google_auth = OAuth2(
-    requests, secrets["google_client_id"], secrets["google_client_secret"], scopes
+    network.requests,
+    secrets["google_client_id"],
+    secrets["google_client_secret"],
+    scopes,
 )
-
 
 # Request device and user codes
 # https://developers.google.com/identity/protocols/oauth2/limited-input-device#step-1:-request-device-and-user-codes
@@ -119,24 +77,8 @@ label_verification_url.text = (
 label_user_code.text = "2. Enter code: %s" % google_auth.user_code
 
 # Create a QR code
-qr = adafruit_miniqr.QRCode(qr_type=3, error_correct=adafruit_miniqr.L)
-qr.add_data(google_auth.verification_url.encode())
-qr.make()
-
-# generate the 1-pixel-per-bit bitmap
-qr_bitmap = bitmap_QR(qr.matrix)
-# we'll draw with a classic black/white palette
-palette = displayio.Palette(2)
-palette[0] = 0xFFFFFF
-palette[1] = 0x000000
-# we'll scale the QR code as big as the display can handle
-scale = 15
-# then center it!
-qr_img = displayio.TileGrid(qr_bitmap, pixel_shader=palette, x=170, y=165)
-group_verification.append(qr_img)
-# show the group
-board.DISPLAY.show(group_verification)
-
+graphics.qrcode(google_auth.verification_url.encode(), qr_size=2, x=170, y=165)
+graphics.display.show(graphics.splash)
 
 # Poll Google's authorization server
 print("Waiting for browser authorization...")
@@ -150,9 +92,9 @@ print("Add the following lines to your secrets.py file:")
 print("\t'google_access_token' " + ":" + " '%s'," % google_auth.access_token)
 print("\t'google_refresh_token' " + ":" + " '%s'" % google_auth.refresh_token)
 # Remove QR code and code/verification labels
-group_verification.pop()
-group_verification.pop()
-group_verification.pop()
+graphics.splash.pop()
+graphics.splash.pop()
+graphics.splash.pop()
 
 label_overview_text.text = "Successfully Authenticated!"
 label_verification_url.text = (
