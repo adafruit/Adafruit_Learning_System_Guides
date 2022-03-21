3e906a1080cb220f5a9771a4b23ba32be4b7c4db
ladyada <support@adafruit.com>
2022-03-21 11:34:29 -0400

    fixi license

--- ESP32_S2_WiFi_Tests/CPy_Native_WiFi_Test/secrets.py
+++ ESP32_S2_WiFi_Tests/CPy_Native_WiFi_Test/secrets.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2020 Adafruit Industries
 #
-# SPDX-License-Identifier: Public Domain
+# SPDX-License-Identifier: Unlicense
 
 # This file is where you keep secret settings, passwords, and tokens!
 # If you put them in the code you risk committing that info or sharing it
