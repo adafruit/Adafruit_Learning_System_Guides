6639121bd5e9a65f5a6f5ec5c6b16690c0cc9466
Dan Halbert <halbert@halwitz.org>
2022-04-19 12:44:39 -0400

    remove remaining pypixelbuf reference

--- Welcome_to_CircuitPython/code.py
+++ Welcome_to_CircuitPython/code.py
@@ -10,7 +10,7 @@ Requires two libraries from the Adafruit CircuitPython Library Bundle.
 Download the bundle from circuitpython.org/libraries and copy the
 following files to your CIRCUITPY/lib folder:
 * neopixel.mpy
-* adafruit_pypixelbuf.mpy
+* adafruit_pixelbuf.mpy
 
 Once the libraries are copied, save this file as code.py to your CIRCUITPY
 drive to run it.
