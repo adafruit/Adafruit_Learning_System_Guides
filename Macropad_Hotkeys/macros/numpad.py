76fa095ade8de34fffa82b1f28c251d8e6272240
Ben Bleything <ben@bleything.net>
2022-05-25 14:06:30 -0700

    change numpad example to emulate keyboard numpad

--- Macropad_Hotkeys/macros/numpad.py
+++ Macropad_Hotkeys/macros/numpad.py
@@ -11,21 +11,21 @@ app = {                # REQUIRED dict, must be named 'app'
     'macros' : [       # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x202000, '7', ['7']),
-        (0x202000, '8', ['8']),
-        (0x202000, '9', ['9']),
+        (0x200020, '7', [Keycode.KEYPAD_SEVEN]),
+        (0x200020, '8', [Keycode.KEYPAD_EIGHT]),
+        (0x200020, '9', [Keycode.KEYPAD_NINE]),
         # 2nd row ----------
-        (0x202000, '4', ['4']),
-        (0x202000, '5', ['5']),
-        (0x202000, '6', ['6']),
+        (0x200020, '4', [Keycode.KEYPAD_FOUR]),
+        (0x200020, '5', [Keycode.KEYPAD_FIVE]),
+        (0x200020, '6', [Keycode.KEYPAD_SIX]),
         # 3rd row ----------
-        (0x202000, '1', ['1']),
-        (0x202000, '2', ['2']),
-        (0x202000, '3', ['3']),
+        (0x200020, '1', [Keycode.KEYPAD_ONE]),
+        (0x200020, '2', [Keycode.KEYPAD_TWO]),
+        (0x200020, '3', [Keycode.KEYPAD_THREE]),
         # 4th row ----------
-        (0x101010, '*', ['*']),
-        (0x800000, '0', ['0']),
-        (0x101010, '#', ['#']),
+        (0x200010, '0', [Keycode.KEYPAD_ZERO]),
+        (0x200010, '', [Keycode.KEYPAD_ZERO]),
+        (0x000020, '.', [Keycode.KEYPAD_PERIOD]),
         # Encoder button ---
         (0x000000, '', [Keycode.BACKSPACE])
     ]
