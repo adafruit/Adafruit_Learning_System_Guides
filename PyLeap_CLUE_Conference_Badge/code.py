a34dd6aa33948a6e8d31f32632a0931e6c0554f0
BlitzCityDIY <blitzcitydiy@gmail.com>
2022-10-05 15:16:29 -0400

    Adding code for PyLeap CLUE badge
    Adding code for the PyLeap CLUE conference badge. Display is flipped for use with a lanyard.

---
+++ PyLeap_CLUE_Conference_Badge/code.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-FileCopyrightText: 2022 Liz Clark for Adafruit Industries (Adapted for CLUE)
+
+# SPDX-License-Identifier: MIT
+
+import board
+from adafruit_pybadger import pybadger
+
+pybadger.badge_background(
+    background_color=pybadger.WHITE,
+    rectangle_color=pybadger.PURPLE,
+    rectangle_drop=0.2,
+    rectangle_height=0.6,
+)
+
+pybadger.badge_line(
+    text="@circuitpython", color=pybadger.BLINKA_PURPLE, scale=2, padding_above=2
+)
+pybadger.badge_line(text="Blinka", color=pybadger.WHITE, scale=5, padding_above=6)
+pybadger.badge_line(
+    text="CircuitPythonista", color=pybadger.WHITE, scale=2, padding_above=2
+)
+pybadger.badge_line(
+    text="she/her", color=pybadger.BLINKA_PINK, scale=4, padding_above=7
+)
+
+display = board.DISPLAY
+display.rotation = 180
+
+pybadger.show_custom_badge()
+
+while True:
+    if pybadger.button.a:
+        pybadger.show_qr_code("https://circuitpython.org")
+
+    if pybadger.button.b:
+        pybadger.show_custom_badge()
