b3a993d90e1fe37e784d89f9d0ea069278017683
Mikey Sklar <mikeysklar@gmail.com>
2023-01-25 12:22:57 -0800

    16x2 LCD example updated with Bullseye compat init script and now works with no IP address

--- Drive_a_16x2_LCD_with_the_Raspberry_Pi/Drive_a_16x2_LCD_with_the_Raspberry_Pi.py
+++ Drive_a_16x2_LCD_with_the_Raspberry_Pi/Drive_a_16x2_LCD_with_the_Raspberry_Pi.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: 2018 Mikey Sklar for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
-
+# Modified by Jonathan Seyfert, 2022-01-22, to keep code from crashing when WiFi or IP is unavailable
 from subprocess import Popen, PIPE
-from time import sleep
+from time import sleep, perf_counter
 from datetime import datetime
 import board
 import digitalio
@@ -29,23 +29,28 @@ lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
 
 # looking for an active Ethernet or WiFi device
 def find_interface():
+#    dev_name = 0 # sets dev_name so that function does not return Null and crash code
     find_device = "ip addr show"
     interface_parse = run_cmd(find_device)
     for line in interface_parse.splitlines():
         if "state UP" in line:
             dev_name = line.split(':')[1]
-    return dev_name
+            return dev_name
+    return 1 # avoids returning Null if "state UP" doesn't exist
 
 # find an active IP on the first LIVE network device
 def parse_ip():
-    find_ip = "ip addr show %s" % interface
+    if interface == 1: # if true, no device is in "state UP", skip IP check
+        return "not assigned " # display "IP not assigned" 
+    ip = "0"
     find_ip = "ip addr show %s" % interface
     ip_parse = run_cmd(find_ip)
     for line in ip_parse.splitlines():
         if "inet " in line:
             ip = line.split(' ')[5]
             ip = ip.split('/')[0]
-    return ip
+            return ip # returns IP address, if found
+    return "pending      " # display "IP pending" when "state UP", but no IPv4 address yet
 
 # run unix shell command, return as ASCII
 def run_cmd(cmd):
@@ -56,12 +61,19 @@ def run_cmd(cmd):
 # wipe LCD screen before we start
 lcd.clear()
 
+
 # before we start the main loop - detect active network device and ip address
-sleep(2)
+# set timer to = perf_counter(), for later use in IP update check
 interface = find_interface()
 ip_address = parse_ip()
+timer = perf_counter()
 
 while True:
+    # check for new IP addresses, at a slower rate than updating the clock
+    if perf_counter() - timer >= 15:
+        interface = find_interface()
+        ip_address = parse_ip()
+        timer = perf_counter()
 
     # date and time
     lcd_line_1 = datetime.now().strftime('%b %d  %H:%M:%S\n')
@@ -72,4 +84,4 @@ while True:
     # combine both lines into one update to the display
     lcd.message = lcd_line_1 + lcd_line_2
 
-    sleep(2)
+    sleep(1)
