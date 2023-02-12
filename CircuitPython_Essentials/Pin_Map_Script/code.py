436240f539f3bd6a4708dcc48a0bfe22ed08bd8c
anecdata <16617689+anecdata@users.noreply.github.com>
2023-02-12 11:47:35 -0600

    Update CircuitPython Essentials Pin Map Script for Pico W
    Also add a commented line to show `microcontroller` pins.

--- CircuitPython_Essentials/Pin_Map_Script/code.py
+++ CircuitPython_Essentials/Pin_Map_Script/code.py
@@ -1,15 +1,22 @@
-# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+# SPDX-FileCopyrightText: 2023 anecdata, Neradoc, Kattni Rembor for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
 
 """CircuitPython Essentials Pin Map Script"""
 import microcontroller
 import board
+try:
+    import cyw43  # raspberrypi
+except ImportError:
+    cyw43 = None
 
 board_pins = []
 for pin in dir(microcontroller.pin):
-    if isinstance(getattr(microcontroller.pin, pin), microcontroller.Pin):
+    if isinstance(getattr(microcontroller.pin, pin), microcontroller.Pin) or \
+       (cyw43 and isinstance(getattr(microcontroller.pin, pin), cyw43.CywPin)):
         pins = []
+        # uncomment to show microcontroller pins
+        # pins.append("microcontroller.{}\t".format(pin))
         for alias in dir(board):
             if getattr(board, alias) is getattr(microcontroller.pin, pin):
                 pins.append("board.{}".format(alias))
