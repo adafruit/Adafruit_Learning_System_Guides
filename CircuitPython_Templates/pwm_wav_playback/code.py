f658ced40f75372b8119a7e175caf095afda469a
Liz <liz@adafruit.com>
2023-03-30 09:51:38 -0400

    Adding PWM audio code
    Adding PWM audio example code for the new PWM audio template page

---
+++ CircuitPython_Templates/pwm_wav_playback/code.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+CircuitPython PWM Audio Out WAV example
+Play a WAV file once.
+
+Remove this line and all of the following docstring content before submitting to the Learn repo.
+
+Update the audio out pin to match the wiring chosen for the microcontroller. 
+Update the following pin name to a viable pin:
+* AUDIO_OUTPUT_PIN
+"""
+import board
+from audiocore import WaveFile
+
+try:
+    from audioio import AudioOut
+    print("Using AudioOut")
+except ImportError:
+    try:
+        from audiopwmio import PWMAudioOut as AudioOut
+        print("Using PWMAudioOut")
+    except ImportError:
+        pass  # not always supported by every board!
+
+audio = AudioOut(board.AUDIO_OUTPUT_PIN)
+
+with open("StreetChicken.wav", "rb") as wave_file:
+    wave = WaveFile(wave_file)
+    print("Playing wav file!")
+    audio.play(wave)
+    while audio.playing:
+        pass
+
+print("Done!")
