5cabdec92661d6d71012393b3340a8c24ca2f718
Anne Barela <1911920+TheKitty@users.noreply.github.com>
2023-04-05 11:45:03 -0400

    Add files via upload

---
+++ CircuitPython_gifio/PyPortal/code-multi-pyportal-direct.py
@@ -0,0 +1,69 @@
+# SPDX-FileCopyrightText: 2023 Anne Barela for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+#
+# Play Multiple GIF files on a PyPortal
+# Requires CircuitPython 8.1.0-beta.1 or later
+# Updated 4/4/2023
+import os
+import struct
+import board
+import gifio
+import time
+import adafruit_touchscreen
+import gc
+
+# Get a dictionary of GIF filenames at the passed base directory
+def get_files(base):
+    files = os.listdir(base)
+    file_names = []
+    for j, filetext in enumerate(files):
+        if not filetext.startswith("."):
+            if filetext not in ('boot_out.txt', 'System Volume Information'):
+                if filetext.endswith(".gif"):
+                    file_names.append(filetext)
+    return file_names
+
+display = board.DISPLAY
+
+# Take over display to drive directly
+display.auto_refresh = False
+display_bus = display.bus
+
+# Pyportal has a touchscreen, a touch advances to next GIF file
+WIDTH = board.DISPLAY.width
+HEIGHT = board.DISPLAY.height
+ts = adafruit_touchscreen.Touchscreen(board.TOUCH_XL, board.TOUCH_XR,
+                                      board.TOUCH_YD, board.TOUCH_YU,
+                                      calibration=((5200, 59000), (5800, 57000)),
+                                      size=(WIDTH, HEIGHT))
+files = get_files("/")
+for i in range(len(files)):
+
+    odg = gifio.OnDiskGif(files[i])
+    # Skip Feather GIFs if put on PyPortal
+    if odg.width != WIDTH:
+        print("File "+files[i]+" not right width, skipping\n")
+        pass
+
+    start = time.monotonic()
+    next_delay = odg.next_frame()  # Load the first frame
+    end = time.monotonic()
+    call_delay = end - start
+
+    # Display GIF file frames until screen touched (for PyPortal)
+    while True:
+        sleeptime = max(0, next_delay - call_delay)
+        time.sleep(sleeptime)
+        if ts.touch_point is not None:
+            break
+        next_delay = odg.next_frame()
+        display_bus.send(42, struct.pack(">hh", 0, odg.bitmap.width - 1))
+        display_bus.send(43, struct.pack(">hh", 0, odg.bitmap.height - 1))
+        display_bus.send(44, odg.bitmap)
+    # End while
+    # Clean up memory
+    odg.deinit()
+    odg = None
+    gc.collect()
+# End for
---
+++ CircuitPython_gifio/PyPortal/code-single-pyportal-displayio.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: 2023 Anne Barela for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+#
+# gifio demo for the Adafruit PyPortal - single file
+#
+# Documentation:
+#   https://docs.circuitpython.org/en/latest/shared-bindings/gifio/
+# Updated 4/5/2023
+#
+import board
+import gifio
+import displayio
+import time
+import adafruit_touchscreen
+import gc
+
+display = board.DISPLAY
+splash = displayio.Group()
+display.root_group = splash
+
+# Pyportal has a touchscreen, a touch stops the display
+WIDTH = board.DISPLAY.width
+HEIGHT = board.DISPLAY.height
+ts = adafruit_touchscreen.Touchscreen(board.TOUCH_XL, board.TOUCH_XR,
+                                      board.TOUCH_YD, board.TOUCH_YU,
+                                      calibration=((5200, 59000), (5800, 57000)),
+                                      size=(WIDTH, HEIGHT))
+
+odg = gifio.OnDiskGif('/sample.gif')
+
+start = time.monotonic()
+next_delay = odg.next_frame()  # Load the first frame
+end = time.monotonic()
+call_delay = end - start
+
+# Depending on your display the next line may need Colorspace.RGB565
+#   instead of Colorspace.RGB565_SWAPPED
+face = displayio.TileGrid(odg.bitmap,
+                          pixel_shader=displayio.ColorConverter
+                          (input_colorspace=displayio.Colorspace.RGB565_SWAPPED))
+splash.append(face)
+board.DISPLAY.refresh()
+
+# Play the GIF file until screen is touched
+while True:
+    time.sleep(max(0, next_delay - call_delay))
+    next_delay = odg.next_frame()
+    if ts.touch_point is not None:
+        break
+# End while
+# Clean up memory
+odg.deinit()
+odg = None
+gc.collect()
