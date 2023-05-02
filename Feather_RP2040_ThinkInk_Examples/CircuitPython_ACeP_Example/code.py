306dfc877a5299f4fad215668761a586200ce6ab
Liz <liz@adafruit.com>
2023-05-02 11:20:20 -0400

    Adding CP examples for ThinkInk
    Adding CircuitPython examples for tri-color and ACeP EInk displays

---
+++ Feather_RP2040_ThinkInk_Examples/CircuitPython_ACeP_Example/code.py
@@ -0,0 +1,44 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 Scott Shawcroft for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 Melissa LeBlanc-Williams for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+
+'''Simple test for a 5.65" ACeP eInk Display 
+with the Feather RP2040 ThinkInk'''
+
+import board
+import displayio
+import busio
+import adafruit_spd1656
+
+displayio.release_displays()
+
+# this pinout is for the Feather RP2040 ThinkInk
+spi = busio.SPI(board.EPD_SCK, MOSI=board.EPD_MOSI, MISO=None)
+epd_cs = board.EPD_CS
+epd_dc = board.EPD_DC
+epd_reset = board.EPD_RESET
+epd_busy = board.EPD_BUSY
+display_bus = displayio.FourWire(
+    spi, command=epd_dc, chip_select=epd_cs, reset=epd_reset, baudrate=1000000
+)
+
+display = adafruit_spd1656.SPD1656(
+    display_bus, width=600, height=448, busy_pin=epd_busy
+)
+
+g = displayio.Group()
+
+fn = "/display-ruler-720p.bmp"
+
+with open(fn, "rb") as f:
+    pic = displayio.OnDiskBitmap(f)
+    t = displayio.TileGrid(pic, pixel_shader=pic.pixel_shader)
+    g.append(t)
+
+    display.show(g)
+
+    display.refresh()
+
+while True:
+    pass
---
+++ Feather_RP2040_ThinkInk_Examples/CircuitPython_Tri-Color_Example/code.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2023 Jose D. Montoya
+#
+# SPDX-License-Identifier: Unlicense
+
+'''Simple test for the Adafruit 2.13" Tri-Color eInk Display 
+with the Feather RP2040 ThinkInk'''
+
+import time
+import board
+import displayio
+import busio
+import adafruit_ssd1680
+
+displayio.release_displays()
+
+# this pinout is for the Feather RP2040 ThinkInk
+spi = busio.SPI(board.EPD_SCK, MOSI=board.EPD_MOSI, MISO=None)
+epd_cs = board.EPD_CS
+epd_dc = board.EPD_DC
+epd_reset = board.EPD_RESET
+epd_busy = board.EPD_BUSY
+display_bus = displayio.FourWire(
+    spi, command=epd_dc, chip_select=epd_cs, reset=epd_reset, baudrate=1000000
+)
+time.sleep(1)
+
+display = adafruit_ssd1680.SSD1680(
+    display_bus,
+    colstart=8,
+    width=250,
+    height=122,
+    highlight_color=0xFF0000,
+    rotation=270,
+)
+
+g = displayio.Group()
+
+with open("/display-ruler.bmp", "rb") as f:
+    pic = displayio.OnDiskBitmap(f)
+
+    t = displayio.TileGrid(
+        pic, pixel_shader=getattr(pic, "pixel_shader", displayio.ColorConverter())
+    )
+
+    g.append(t)
+
+    display.show(g)
+
+    display.refresh()
+
+    print("refreshed")
+
+while True:
+    pass
