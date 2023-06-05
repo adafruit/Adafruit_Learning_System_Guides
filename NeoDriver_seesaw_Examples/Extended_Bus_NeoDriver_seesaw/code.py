ff5a1724002da4c8146da30a4f3abb1728508f92
Liz <liz@adafruit.com>
2023-06-05 13:30:36 -0400

    adding NeoDriver seesaw examples
    Adding CircuitPython, Arduino and CircuitPython with extended bus examples for NeoDriver guide

---
+++ NeoDriver_seesaw_Examples/Extended_Bus_NeoDriver_seesaw/code.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2023 Liz Clark for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+'''This example should only be used with Linux single board computers
+that require creating an I2C busio object by passing in the Bus ID'''
+
+import time
+from rainbowio import colorwheel
+from adafruit_extended_bus import ExtendedI2C as I2C
+from adafruit_seesaw import seesaw, neopixel
+
+i2c = I2C(1)
+ss = seesaw.Seesaw(i2c, addr=0x60)
+neo_pin = 15
+num_pixels = 64
+
+pixels = neopixel.NeoPixel(ss, neo_pin, num_pixels, brightness = 0.1)
+
+color_offset = 0
+
+while True:
+    for i in range(num_pixels):
+        rc_index = (i * 256 // num_pixels) + color_offset
+        pixels[i] = colorwheel(rc_index & 255)
+    pixels.show()
+    color_offset += 1
+    time.sleep(0.01)
