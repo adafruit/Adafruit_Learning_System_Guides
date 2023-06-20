38e6c13866ddcc8042a8a25dcfa1854418d59007
Liz <liz@adafruit.com>
2023-06-20 12:40:32 -0400

    adding arduino license

--- RP2040_Prop-Maker_Feather_Examples/Arduino_Prop-Maker_Feather_Example/Arduino_Prop-Maker_Feather_Example.ino
+++ RP2040_Prop-Maker_Feather_Examples/Arduino_Prop-Maker_Feather_Example/Arduino_Prop-Maker_Feather_Example.ino
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Limor Fried for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
