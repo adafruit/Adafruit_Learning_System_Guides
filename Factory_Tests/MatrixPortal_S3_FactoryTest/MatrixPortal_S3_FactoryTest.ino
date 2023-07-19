c858d1915470ea9239df147a0debc434d86c6e40
Melissa LeBlanc-Williams <melissa@adafruit.com>
2023-07-18 20:03:39 -0700

    Added SPDX and fixed name

--- Factory_Tests/MatrixPortal_S3_FactoryTest/MatrixPortal_S3_FactoryTest.ino
+++ Factory_Tests/MatrixPortal_S3_FactoryTest/MatrixPortal_S3_FactoryTest.ino
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Limor Fried for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_TestBed.h>
 #include <WiFi.h>
