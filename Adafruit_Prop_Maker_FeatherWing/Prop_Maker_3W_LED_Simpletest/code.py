2c5449756e04bd8d85aa58ad84e728a0f2d85317
Kattni Rembor <kattni@kattni.com>
2023-08-14 11:15:24 -0400

    Update PropMaker FeatherWing examples.

--- Adafruit_Prop_Maker_FeatherWing/Prop_Maker_3W_LED_Simpletest/code.py
+++ Adafruit_Prop_Maker_FeatherWing/Prop_Maker_3W_LED_Simpletest/code.py
@@ -1,16 +1,29 @@
-# SPDX-FileCopyrightText: 2019 Kattni Rembor for Adafruit Industries
+# SPDX-FileCopyrightText: 2019, 2023 Kattni Rembor for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
 
 """Simple rainbow swirl example for 3W LED"""
+import time
 import pwmio
 import board
-from rainbowio import colorwheel
 import digitalio
 
 enable = digitalio.DigitalInOut(board.D10)
 enable.direction = digitalio.Direction.OUTPUT
 enable.value = True
+
+
+def colorwheel(pos):
+    if pos < 0 or pos > 255:
+        return 0, 0, 0
+    if pos < 85:
+        return int(255 - pos * 3), int(pos * 3), 0
+    if pos < 170:
+        pos -= 85
+        return 0, int(255 - pos * 3), int(pos * 3)
+    pos -= 170
+    return int(pos * 3), 0, int(255 - pos * 3)
+
 
 red = pwmio.PWMOut(board.D11, duty_cycle=0, frequency=20000)
 green = pwmio.PWMOut(board.D12, duty_cycle=0, frequency=20000)
@@ -22,3 +35,4 @@ while True:
         red.duty_cycle = int(r * 65536 / 256)
         green.duty_cycle = int(g * 65536 / 256)
         blue.duty_cycle = int(b * 65536 / 256)
+        time.sleep(0.05)
