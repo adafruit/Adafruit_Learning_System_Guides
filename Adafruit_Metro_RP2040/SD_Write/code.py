e098dfcb4c8c4c5f9908a914000aaa9ea7bd541a
Kattni Rembor <kattni@kattni.com>
2023-09-15 14:08:30 -0400

    Adding SD card demo code.

---
+++ Adafruit_Metro_RP2040/SD_Write/code.py
@@ -0,0 +1,35 @@
+#
+# SPDX-License-Identifier: MIT
+
+"""
+CircuitPython Essentials SD Card Write Demo
+"""
+
+import time
+import adafruit_sdcard
+import board
+import digitalio
+import microcontroller
+import storage
+
+# The SD_CS pin is the chip select line.
+SD_CS = board.SD_CS
+
+# Connect to the card and mount the filesystem.
+cs = digitalio.DigitalInOut(SD_CS)
+sdcard = adafruit_sdcard.SDCard(board.SPI(), cs)
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd")
+
+# Use the filesystem as normal! Our files are under /sd
+
+print("Logging temperature to filesystem")
+# append to the file!
+while True:
+    # open file for append
+    with open("/sd/temperature.txt", "a") as f:
+        t = microcontroller.cpu.temperature
+        print("Temperature = %0.1f" % t)
+        f.write("%0.1f\n" % t)
+    # file is saved
+    time.sleep(1)
