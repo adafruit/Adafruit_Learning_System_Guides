5d40c9d5685dad75f509e39357c7d1a1893107da
ladyada <limor@ladyada.net>
2023-10-07 17:02:10 -0400

    generalize for any size display

--- Factory_Tests/Qualia_ESP32S3_RGB666_FactoryTest/Qualia_ESP32S3_RGB666_FactoryTest.ino
+++ Factory_Tests/Qualia_ESP32S3_RGB666_FactoryTest/Qualia_ESP32S3_RGB666_FactoryTest.ino
@@ -34,6 +34,10 @@ Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
 //    480 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */,
 //    expander, GFX_NOT_DEFINED /* RST */, tl034wvs05_b1477a_init_operations, sizeof(tl034wvs05_b1477a_init_operations));
 
+// 3.2" 320x820 rectangle bar display
+//    320 /* width */, 820 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */,
+//    expander, GFX_NOT_DEFINED /* RST */, tl032fwv01_init_operations, sizeof(tl032fwv01_init_operations));
+
 uint16_t *colorWheel;
   
 void setup(void)
@@ -61,7 +65,7 @@ void setup(void)
   expander->digitalWrite(PCA_TFT_BACKLIGHT, HIGH);
 
   TB.begin();
-  colorWheel = (uint16_t *) ps_malloc(480 * 480 * sizeof(uint16_t));
+  colorWheel = (uint16_t *) ps_malloc(gfx->width() * gfx->height() * sizeof(uint16_t));
   if (colorWheel) generateColorWheel(colorWheel);
 }
 
@@ -70,29 +74,34 @@ uint8_t allpins[] = {SS, SCK, MOSI, MISO, A1, A0};
 
 void loop()
 {  
-  gfx->draw16bitRGBBitmap(0, 0, colorWheel, 480, 480);
+  gfx->draw16bitRGBBitmap(0, 0, colorWheel, gfx->width(), gfx->height());
   delay(100);
   return;
 }
 
 // https://chat.openai.com/share/8edee522-7875-444f-9fea-ae93a8dfa4ec
 void generateColorWheel(uint16_t *colorWheel) {
+  int width = gfx->width();
+  int height = gfx->height();
+  int half_width = width / 2;
+  int half_height = height / 2;
   float angle;
   uint8_t r, g, b;
-  int scaled_index;
+  int index, scaled_index;
 
-  for(int y = 0; y < 240; y++) {
-    for(int x = 0; x < 240; x++) {
-      angle = atan2(y - 120, x - 120);
+  for(int y = 0; y < half_height; y++) {
+    for(int x = 0; x < half_width; x++) {
+      index = y * half_width + x;
+      angle = atan2(y - half_height / 2, x - half_width / 2);
       r = uint8_t(127.5 * (cos(angle) + 1));
       g = uint8_t(127.5 * (sin(angle) + 1));
       b = uint8_t(255 - (r + g) / 2);
       uint16_t color = RGB565(r, g, b);
 
-      // Scale this pixel into 4 pixels in the 480x480 buffer
+      // Scale this pixel into 4 pixels in the full buffer
       for(int dy = 0; dy < 2; dy++) {
         for(int dx = 0; dx < 2; dx++) {
-          scaled_index = (y * 2 + dy) * 480 + (x * 2 + dx);
+          scaled_index = (y * 2 + dy) * width + (x * 2 + dx);
           colorWheel[scaled_index] = color;
         }
       }
