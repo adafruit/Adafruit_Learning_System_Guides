8e6490df4abaaa292c437b0da39fe6067c87ca96
Liz <liz@adafruit.com>
2023-11-27 10:42:15 -0500

    updating space clock code
    changing some variable names, adding more comments

--- Qualia_S3_Space_Clock/code.py
+++ Qualia_S3_Space_Clock/code.py
@@ -26,9 +26,10 @@ from adafruit_display_text import label
 from adafruit_bitmap_font import bitmap_font
 from adafruit_qualia.graphics import Graphics, Displays
 
-key = keypad.Keys((board.A0,), value_when_pressed=False, pull=True)
-
+# timezone offset for calculating mars time
 timezone = -5
+
+key = keypad.Keys((board.A0,), value_when_pressed=False, pull=True)
 
 wifi.radio.connect(os.getenv("CIRCUITPY_WIFI_SSID"), os.getenv("CIRCUITPY_WIFI_PASSWORD"))
 print(f"Connected to {os.getenv('CIRCUITPY_WIFI_SSID')}")
@@ -37,11 +38,8 @@ aio_username = os.getenv('ADAFRUIT_AIO_USERNAME')
 aio_key = os.getenv('ADAFRUIT_AIO_KEY')
 
 context = ssl.create_default_context()
-print("socketpool")
 pool = socketpool.SocketPool(wifi.radio)
-print("requests")
 requests = adafruit_requests.Session(pool, context)
-print("io")
 io = IO_HTTP(aio_username, aio_key, requests)
 
 earth_bitmap = displayio.OnDiskBitmap("/earth.bmp")
@@ -49,54 +47,46 @@ mars_bitmap = displayio.OnDiskBitmap("/mars.bmp")
 
 graphics = Graphics(Displays.ROUND40, default_bg=None, auto_refresh=True)
 
-# Create a TileGrid to hold the bitmap
 earth_grid = displayio.TileGrid(earth_bitmap, pixel_shader=earth_bitmap.pixel_shader)
-
-# Create a Group to hold the TileGrid
 earth_group = displayio.Group()
-
-# Add the TileGrid to the Group
 earth_group.append(earth_grid)
 
-# Create a TileGrid to hold the bitmap
 mars_grid = displayio.TileGrid(mars_bitmap, pixel_shader=mars_bitmap.pixel_shader)
+mars_group = displayio.Group()
+mars_group.append(mars_grid)
 
 def center(grid, bitmap):
-    # Center the image
+    # center the image
     grid.x -= (bitmap.width - graphics.display.width) // 2
     grid.y -= (bitmap.height - graphics.display.height) // 2
 
 center(mars_grid, mars_bitmap)
 center(earth_grid, earth_bitmap)
-# Create a Group to hold the TileGrid
-mars_group = displayio.Group()
-
-# Add the TileGrid to the Group
-mars_group.append(mars_grid)
 
 graphics.display.root_group = mars_group
 
-# Pointer using vectorio, first the hub
+# pointer using vectorio, first the hub
 pointer_pal = displayio.Palette(4)
 pointer_pal[0] = 0xff0000
 pointer_pal[1] = 0x000000
 pointer_pal[2] = 0x0000ff
 pointer_pal[3] = 0xffffff
-pointer_hub = vectorio.Circle(pixel_shader=pointer_pal, radius=16, x=0, y=0)
+pointer_hub = vectorio.Circle(pixel_shader=pointer_pal, radius=26, x=0, y=0)
 pointer_hub.x = graphics.display.width // 2
 pointer_hub.y = graphics.display.height // 2
 
-# Pointer itself
-pw = 15    # pointer width
-ph = 200   # pointer height
-pointer_points = [(pw,0), (pw,-ph), (-pw,-ph), (-pw,0)]
-pointer = vectorio.Polygon(pixel_shader=pointer_pal, points=pointer_points, x=0,y=0)
-pointer.x = graphics.display.width // 2
-pointer.y = graphics.display.height // 2
-mars_group.append(pointer)
-earth_group.append(pointer)
-hw = 13    # pointer width
-hh = 150   # pointer height
+# minute hand
+mw = 23
+mh = 225
+min_points = [(mw,0), (mw,-mh), (-mw,-mh), (-mw,0)]
+min_hand = vectorio.Polygon(pixel_shader=pointer_pal, points=min_points, x=0,y=0)
+min_hand.x = graphics.display.width // 2
+min_hand.y = graphics.display.height // 2
+mars_group.append(min_hand)
+earth_group.append(min_hand)
+# hour hand
+hw = 25
+hh = 175
 hour_points = [(hw,0), (hw,-hh), (-hw,-hh), (-hw,0)]
 hour_hand = vectorio.Polygon(pixel_shader=pointer_pal, points=hour_points,
                              x=0, y=0, color_index=1)
@@ -105,6 +95,7 @@ hour_hand.y = graphics.display.height // 2
 mars_group.append(hour_hand)
 earth_group.append(hour_hand)
 
+# add numbers to the clock face
 def calculate_number_position(number, center_x, center_y, radius):
     angle = (360 / 12) * (number - 3)  # -3 adjusts the angle to start at 12 o'clock
     rad_angle = pi * angle / 180
@@ -144,6 +135,8 @@ for i in range(1, 13):
 mars_group.append(pointer_hub)
 earth_group.append(pointer_hub)
 
+# get time from adafruit io
+# called once an hour in the loop
 def update_time():
     print("time")
     now = io.receive_time()
@@ -160,10 +153,11 @@ def convert_time(the_time):
         h = 12
     return h, a
 
+# get mars time
 def mars_time():
     dt = io.receive_time()
     print(dt)
-    utc_offset = 3600 * abs(timezone)
+    utc_offset = 3600 * -timezone
     tai_offset = 37
     millis = time.mktime(dt)
     unix_timestamp = millis + utc_offset
@@ -181,20 +175,20 @@ def mars_time():
     return mtc_minutes, mtc_hours
 
 def time_angle(m, the_hour):
-    if m == 15:
-        m = 16
-    elif m == 45:
-        m = 46
+    m_offset = 25 if 12 <= m < 18 or 42 <= m < 48 else 5
+    h_offset = 25 if 2 <= the_hour % 12 < 4 or 8 <= the_hour % 12 < 10 else 5
     # Adjusted angle calculation for minute hand
     theta_minute = 360 - (m / 60) * 360
     theta_hour = ((the_hour / 12) + (m / (12 * 60))) * 360
     # Calculate coordinates for minute hand (mirrored)
-    minute_x = -int(cos(pi * (theta_minute - 90) / 180) * ph)
-    minute_y = int(sin(pi * (theta_minute + 90) / 180) * ph)
+    minute_x = -int(cos(pi * (theta_minute - 90) / 180) * mh)
+    minute_y = int(sin(pi * (theta_minute + 90) / 180) * mh)
     hour_x = int(cos(pi * (theta_hour - 90) / 180) * hh)
     hour_y = int(sin(pi * (theta_hour + 90) / 180) * hh)
-    pointer.points = [(pw, 0), (minute_x + 2, -minute_y), (minute_x - 2, -minute_y), (-pw, 0)]
-    hour_hand.points = [(hw, 0), (hour_x + 2, -hour_y), (hour_x - 2, -hour_y), (-hw, 0)]
+    min_hand.points = [(mw, 0), (minute_x + m_offset, -minute_y),
+                       (minute_x - m_offset, -minute_y), (-mw, 0)]
+    hour_hand.points = [(hw, 0), (hour_x + h_offset, -hour_y),
+                        (hour_x - h_offset, -hour_y), (-hw, 0)]
 
 clock_timer = 1 * 1000
 clock_clock = ticks_ms()
@@ -213,22 +207,28 @@ time_angle(minute, hour)
 
 while True:
     event = key.events.get()
+    # swap between earth or mars time
     if event:
         if event.pressed:
             print("updating display")
             show_earth = not show_earth
+    # update background image
+    # change minute hand color depending on background
     if show_earth:
-        if pointer.color_index != 2:
+        if min_hand.color_index != 2:
             time_angle(minute, hour)
             graphics.display.root_group = earth_group
-            pointer.color_index = 2
+            min_hand.color_index = 2
             pointer_hub.color_index = 2
     else:
-        if pointer.color_index != 0:
+        if min_hand.color_index != 0:
             time_angle(mars_min, mars_hour)
             graphics.display.root_group = mars_group
-            pointer.color_index = 0
+            min_hand.color_index = 0
             pointer_hub.color_index = 0
+    # use ticks for timekeeping
+    # every minute update clock hands
+    # recheck IO time every hour
     if ticks_diff(ticks_ms(), clock_clock) >= clock_timer:
         tick += 1
         if tick > 59:
