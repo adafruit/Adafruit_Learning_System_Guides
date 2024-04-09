cf8764a1d7c09fcb085f8cebe5b34d8ac549d53d
Dan Halbert <halbert@halwitz.org>
2024-04-09 16:02:09 -0400

    black

--- BLE_Client_Server/client/code.py
+++ BLE_Client_Server/client/code.py
@@ -23,24 +23,23 @@ switch = Debouncer(pin)
 
 pixels = NeoPixel(NEOPIXEL, 1)  # Set up built-in NeoPixel
 
-AQUA = const(0x00FFFF)    # (0, 255, 255)
-GREEN = const(0x00FF00)   # (0, 255, 0)
+AQUA = const(0x00FFFF)  # (0, 255, 255)
+GREEN = const(0x00FF00)  # (0, 255, 0)
 ORANGE = const(0xFF8000)  # (255, 128, 0)
-RED = const(0xFF0000)     # (255, 0, 0)
-BLUE = const(0x0000FF)    # (0, 0, 255)
+RED = const(0xFF0000)  # (255, 0, 0)
+BLUE = const(0x0000FF)  # (0, 0, 255)
 
-gradients = {'Off': [(0.0, RED), (0.75, ORANGE)],
-             'On':  [(0.0, GREEN), (1.0, AQUA)]}
-palette = fancy.expand_gradient(gradients['Off'], 30)
+gradients = {"Off": [(0.0, RED), (0.75, ORANGE)], "On": [(0.0, GREEN), (1.0, AQUA)]}
+palette = fancy.expand_gradient(gradients["Off"], 30)
 
 gamma_levels = (0.25, 0.3, 0.15)
 color_index = 1
 fade_direction = 1
 
-TARGET = 'f0:74:72:60:87:d2'  # CHANGE TO YOUR BLE ADDRESS
+TARGET = "f0:74:72:60:87:d2"  # CHANGE TO YOUR BLE ADDRESS
 target_address = TARGET.split(":")  # Convert address string to list of bytes
 target_address.reverse()  # Reverse bytes to match Address class little-endian
-target_address = unhexlify(''.join(target_address))  # Convert list to bytes
+target_address = unhexlify("".join(target_address))  # Convert list to bytes
 
 button_packet = ButtonPacket("1", True)  # Transmits pressed button 1
 
@@ -80,10 +79,10 @@ while True:
                     # Color match
                     if fancy.CRGB(*packet.color).pack() == GREEN:
                         # Green indicates on state
-                        palette = fancy.expand_gradient(gradients['On'], 30)
+                        palette = fancy.expand_gradient(gradients["On"], 30)
                     else:
                         # Otherwise red indicates off
-                        palette = fancy.expand_gradient(gradients['Off'], 30)
+                        palette = fancy.expand_gradient(gradients["Off"], 30)
 
             # NeoPixel color fading routing
             color = fancy.palette_lookup(palette, color_index / 29)
