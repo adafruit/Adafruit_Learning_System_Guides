297910abbf57095c845b8f70c0d86eaceaae07b7
Liz <liz@adafruit.com>
2024-04-15 11:22:00 -0400

    adding Arduino and CP examples for ADG72x

---
+++ ADG72x_Examples/CircuitPython/code.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 Liz Clark for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_adg72x
+from analogio import AnalogIn
+
+analog_in = AnalogIn(board.A0)
+
+i2c = board.I2C()
+switch = adafruit_adg72x.ADG72x(i2c)
+
+c = 0
+switch_time = 2
+clock = time.monotonic()
+while True:
+    if (time.monotonic() - clock) > switch_time:
+        print(f"Selecting channel {c + 1}")
+        switch.channel = c
+        c = (c + 1) % 8
+        clock = time.monotonic()
+    print((analog_in.value,))
+    time.sleep(0.1)
