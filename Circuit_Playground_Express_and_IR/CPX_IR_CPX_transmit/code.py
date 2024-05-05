f43b59dc42ae0e4b51b3fb5f5acb268b12c6b44a
bradanlane <bradanlane@users.noreply.github.com>
2024-05-05 10:00:19 -0400

    correct one and zero list values
    the list values for ONE and ZERO are swapped from what is described in the Adafruit_irremote.py library. This error causes the receiver to get the one-s compliment of the expected data.

--- Circuit_Playground_Express_and_IR/CPX_IR_CPX_transmit/code.py
+++ Circuit_Playground_Express_and_IR/CPX_IR_CPX_transmit/code.py
@@ -11,8 +11,8 @@ import board
 # Create a 'pulseio' output, to send infrared signals on the IR transmitter @ 38KHz
 pulseout = pulseio.PulseOut(board.IR_TX, frequency=38000, duty_cycle=2 ** 15)
 # Create an encoder that will take numbers and turn them into NEC IR pulses
-encoder = adafruit_irremote.GenericTransmit(header=[9500, 4500], one=[550, 550],
-                                            zero=[550, 1700], trail=0)
+encoder = adafruit_irremote.GenericTransmit(header=[9500, 4500], one=[550, 1700],
+                                            zero=[550, 550], trail=0)
 
 while True:
     if cpx.button_a:
