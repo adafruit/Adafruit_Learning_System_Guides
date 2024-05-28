87d83ca0bf7f8c8d6d1be3eb41c114b09471266e
Liz <liz@adafruit.com>
2024-05-28 15:05:36 -0400

    adding ATMakers TRRS Trinkey demos
    Adding two demos from ATMakers for the TRRS Trinkey guide

---
+++ TRRS_Trinkey_Demos/CircuitPython_Chording_Joystick/XACsettings.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2024 Bill Binko
+# SPDX-License-Identifier: MIT
+
+#Change this to True to swap horizonatal and vertical axes
+swapAxes = False
+
+#Change this to True to invert (flip) the horizontal axis
+invertHor = False
+
+#Change this to True to invert (flip) the vertical axis
+invertVert = True
+
+#Increase this to make the motion smoother (with more lag)
+#Decrease to make more responsive (Min=1 Default=3 Max=Any but>20 is unreasonable)
+smoothingFactor = 2
