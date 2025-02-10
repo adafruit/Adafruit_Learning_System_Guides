7779de188f49cff9eb0aad0c271441f150712286
Liz <23021834+BlitzCityDIY@users.noreply.github.com>
2025-02-10 14:54:30 -0500

    Update code.py

--- GC9A01A_Demo_Code/Python_GC9A01A/code.py
+++ GC9A01A_Demo_Code/Python_GC9A01A/code.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: MIT
 
-'''Raspberry Pi Graphics example for the 240x240 Round TFT'''
+'''Raspberry Pi Graphics example for the Vertical Newxie TFT'''
 
 import time
 import digitalio
@@ -18,13 +18,14 @@ cs_pin = digitalio.DigitalInOut(board.CE0)
 dc_pin = digitalio.DigitalInOut(board.D25)
 reset_pin = digitalio.DigitalInOut(board.D27)
 
-BAUDRATE = 24000000
+BAUDRATE=24000000
+
 
 spi = board.SPI()
 
-disp = gc9a01a.GC9A01A(spi, rotation=180,
-    width=135, height=240,
-    x_offset=53, y_offset=40,
+disp = gc9a01a.GC9A01A(spi, rotation=0,
+    width=240, height=240,
+    x_offset=0, y_offset=0,
     cs=cs_pin,
     dc=dc_pin,
     rst=reset_pin,
@@ -37,22 +38,22 @@ height = disp.height
 # -------TEXT AND SHAPES---------
 image1 = Image.new("RGB", (width, height))
 draw1 = ImageDraw.Draw(image1)
-draw1.rectangle((0, 0, width, height), fill=(0, 255, 0))  # Green background
+draw1.ellipse((0, 0, width, height), fill=(0, 255, 0))  # Green background
 
-draw1.rectangle(
+draw1.ellipse(
     (BORDER, BORDER, width - BORDER - 1, height - BORDER - 1), fill=(170, 0, 136)
 )
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
-text = "Hello\nWorld!"
+text = "Hello World!"
 (font_width, font_height) = font.getsize(text)
 draw1.text(
-    (30, height // 2 - font_height // 2-12),
+    (width // 2 - font_width // 2, height // 2 - font_height // 2),
     text,
     font=font,
     fill=(255, 255, 0),
 )
 
-# ------BLINKA JPEG DISPLAY----------
+# ------ADABOT JPEG DISPLAY----------
 image2 = Image.open("blinka_round.jpg")
 image_ratio = image2.width / image2.height
 screen_ratio = width / height
@@ -66,5 +67,5 @@ image2 = image2.crop((x, y, x + width, y + height))
 while True:
     disp.image(image1)  # show text
     time.sleep(2)
-    disp.image(image2)  # show blinka
+    disp.image(image2)  # show adabot
     time.sleep(2)
