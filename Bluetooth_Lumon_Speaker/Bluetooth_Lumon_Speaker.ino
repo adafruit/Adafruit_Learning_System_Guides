1e54149f1aa1c8f71a44faf72a51243aafe4cc8b
Liz <liz@adafruit.com>
2025-03-11 15:53:43 -0400

    adding moon phase clock & bluetooth speaker
    CircuitPython code for moon phase clock and arduino code for bluetooth lumon speaker

---
+++ Bluetooth_Lumon_Speaker/Bluetooth_Lumon_Speaker.ino
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: 2020 Phil Schatzmann
+//
+// SPDX-License-Identifier: GNU General Public License
+
+/*
+  Streaming Music from Bluetooth
+  
+  Copyright (C) 2020 Phil Schatzmann
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// ==> Example which shows how to use the built in ESP32 I2S >= 3.0.0
+
+#include "ESP_I2S.h"
+#include "BluetoothA2DPSink.h"
+
+const uint8_t I2S_SCK = 8;       /* Audio data bit clock */
+const uint8_t I2S_WS = 7;       /* Audio data left and right clock */
+const uint8_t I2S_SDOUT = 14;    /* ESP32 audio data output (to speakers) */
+I2SClass i2s;
+
+BluetoothA2DPSink a2dp_sink(i2s);
+
+void setup() {
+    i2s.setPins(I2S_SCK, I2S_WS, I2S_SDOUT);
+    if (!i2s.begin(I2S_MODE_STD, 44100, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO, I2S_STD_SLOT_BOTH)) {
+      Serial.println("Failed to initialize I2S!");
+      while (1); // do nothing
+    }
+
+    a2dp_sink.start("Lumon Industries Speaker");
+}
+
+void loop() {
+}
