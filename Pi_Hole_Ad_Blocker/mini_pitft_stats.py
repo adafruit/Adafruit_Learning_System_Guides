8937ea355e13f494b04e4aa556dd916a0cc88c48
Mikey Sklar <mikeysklar@gmail.com>
2025-06-02 12:51:41 -0700

    linting long line.

--- Pi_Hole_Ad_Blocker/mini_pitft_stats.py
+++ Pi_Hole_Ad_Blocker/mini_pitft_stats.py
@@ -76,7 +76,10 @@ while True:
     MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
     cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}'"
     Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}'"
+    cmd = (
+    "cat /sys/class/thermal/thermal_zone0/temp | "
+    "awk '{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}'"
+    )
     Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
     # Pi Hole data!
