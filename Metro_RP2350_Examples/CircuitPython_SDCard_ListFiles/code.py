748f988a520f19d10a26f7154cdd9b9224345ae4
foamyguy <foamyguy@gmail.com>
2025-06-13 11:54:11 -0500

    expand SD comments, remove leftover comment section, remove 10x specific files

--- Metro_RP2350_Examples/CircuitPython_SDCard_ListFiles/code.py
+++ Metro_RP2350_Examples/CircuitPython_SDCard_ListFiles/code.py
@@ -3,7 +3,6 @@
 
 """
 CircuitPython Essentials SD Card Read Demo
-* Not compatible with CircuitPython 10.x
 """
 
 import os
@@ -16,16 +15,23 @@ import adafruit_sdcard
 # The SD_CS pin is the chip select line.
 SD_CS = board.SD_CS
 
+# For CircuitPython 9.x we must initialize and mount the SD card manually.
+# On CircuitPython 10.x the core will automatically initialize and mount the SD.
+# This try/except block can be removed with 10.x has a stable release.
 try:
-    # Connect to the card and mount the filesystem.
+    # Initialize the Chip Select pin for the SD card
     cs = digitalio.DigitalInOut(SD_CS)
+    # Initialize the SD card
     sdcard = adafruit_sdcard.SDCard(
         busio.SPI(board.SD_SCK, board.SD_MOSI, board.SD_MISO), cs
     )
+    # Mount the SD card
     vfs = storage.VfsFat(sdcard)
     storage.mount(vfs, "/sd")
 except ValueError:
-    # SD_CS in use error happens if CircuitPython core initialized the SD automatically
+    # "ValueError SD_CS in use" error happen on CircuitPython 10.x
+    # because the core initialized the SD automatically. The error
+    # can be ignored on 10.x.
     pass
 
 # Use the filesystem as normal! Our files are under /sd
