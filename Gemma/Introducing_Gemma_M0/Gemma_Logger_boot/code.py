a4d7e1afd0a6e36ff7b7cd63b21e51d4873691f3
Anne Barela <1911920+TheKitty@users.noreply.github.com>
2025-12-15 15:42:34 -0600

    Clone Introducing Gemma to Gemma/
    For guide changes

---
+++ Gemma/Introducing_Gemma_M0/Gemma_Logger_boot/code.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2017 Limor Fried for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import board
+import digitalio
+import storage
+
+switch = digitalio.DigitalInOut(board.D0)
+switch.direction = digitalio.Direction.INPUT
+switch.pull = digitalio.Pull.UP
+
+# If the D0 is connected to ground with a wire
+# CircuitPython can write to the drive
+storage.remount("/", readonly=switch.value)
---
+++ Gemma/Introducing_Gemma_M0/README.md
@@ -0,0 +1,4 @@
+# Introducing_Gemma_M0
+
+Code to accompany this tutorial:
+https://learn.adafruit.com/adafruit-gemma-m0
